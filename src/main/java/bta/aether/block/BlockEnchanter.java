fa5fd44ae933c76c36131dbcba3ca107c6b3388a
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2024-01-09 19:35:14 +0100

    Enchanter/Freezer should now drop their blocks. Enchanter now accepts damaged items.

--- src/main/java/bta/aether/block/BlockEnchanter.java
+++ src/main/java/bta/aether/block/BlockEnchanter.java
@@ -2,16 +2,54 @@ package bta.aether.block;
 
 import bta.aether.gui.GuiEnchanter;
 import bta.aether.tile.TileEntityEnchanter;
+import bta.aether.tile.TileEntityFreezer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.block.BlockTileEntity;
 import net.minecraft.core.block.entity.TileEntity;
 import net.minecraft.core.block.material.Material;
+import net.minecraft.core.entity.EntityItem;
 import net.minecraft.core.entity.player.EntityPlayer;
+import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.world.World;
+
+import java.util.Random;
 
 public class BlockEnchanter extends BlockTileEntity {
     public BlockEnchanter(String key, int id, Material material) {
         super(key, id, material);
+    }
+
+    @Override
+    public void onBlockRemoved(World world, int i, int j, int k, int data) {
+        TileEntityEnchanter tile = (TileEntityEnchanter) world.getBlockTileEntity(i, j, k);
+        if (tile != null) {
+            Random random = new Random();
+            for (int l = 0; l < tile.getSizeInventory(); ++l) {
+                ItemStack itemstack = tile.getStackInSlot(l);
+                if (itemstack != null) {
+                    float f = random.nextFloat() * 0.8F + 0.1F;
+                    float f1 = random.nextFloat() * 0.8F + 0.1F;
+                    float f2 = random.nextFloat() * 0.8F + 0.1F;
+
+                    while (itemstack.stackSize > 0) {
+                        int i1 = random.nextInt(21) + 10;
+                        if (i1 > itemstack.stackSize) {
+                            i1 = itemstack.stackSize;
+                        }
+
+                        itemstack.stackSize -= i1;
+                        EntityItem entityitem = new EntityItem(world, (float) i + f, (float) j + f1, (float) k + f2, new ItemStack(itemstack.itemID, i1, itemstack.getMetadata()));
+                        float f3 = 0.05F;
+                        entityitem.xd = (float) random.nextGaussian() * f3;
+                        entityitem.yd = (float) random.nextGaussian() * f3 + 0.2F;
+                        entityitem.zd = (float) random.nextGaussian() * f3;
+                        world.entityJoinedWorld(entityitem);
+                    }
+                }
+            }
+        }
+
+        super.onBlockRemoved(world, i, j, k, data);
     }
 
     @Override
