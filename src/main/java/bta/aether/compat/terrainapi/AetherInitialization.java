bea414f509c067122008b160230f950ea3e17b7b
FatherCheese <ythojim@gmail.com>
2024-01-08 17:36:03 -0700

    Initial TerrainAPI world gen

---
+++ src/main/java/bta/aether/compat/terrainapi/AetherInitialization.java
@@ -0,0 +1,55 @@
+package bta.aether.compat.terrainapi;
+
+import bta.aether.Aether;
+import bta.aether.block.AetherBlocks;
+import bta.aether.world.generate.feature.WorldFeatureTreeSkyroot;
+import net.minecraft.core.world.generate.feature.WorldFeatureFlowers;
+import useless.terrainapi.generation.StructureFeatures;
+import useless.terrainapi.generation.overworld.OverworldOreFeatures;
+import useless.terrainapi.generation.overworld.OverworldRandomFeatures;
+import useless.terrainapi.initialization.BaseInitialization;
+
+import static bta.aether.Aether.MOD_ID;
+
+public class AetherInitialization extends BaseInitialization {
+    private static final TerrainAetherConfig aetherConfig = ChunkDecoratorAetherAPI.aetherConfig;
+    @Override
+    protected void initValues() {
+        aetherConfig.addTreeDensity(Aether.biomeAether, 16);
+    }
+
+    @Override
+    protected void initStructure() {
+        StructureFeatures structureFeatures = ChunkDecoratorAetherAPI.structureFeatures;
+
+        structureFeatures.addFeature(
+            AetherFunctions::generateQuickSoil, null
+        );
+
+        structureFeatures.addFeature(
+                AetherFunctions::generateClouds, null
+        );
+    }
+
+    @Override
+    protected void initOre() {
+        OverworldOreFeatures oreFeatures = ChunkDecoratorAetherAPI.oreFeatures;
+        oreFeatures.addManagedOreFeature(MOD_ID, AetherBlocks.oreAmbrosiumHolystone, 16, 24, 1f, false);
+        oreFeatures.addManagedOreFeature(MOD_ID, AetherBlocks.oreZaniteHolystone, 8, 16, 1f, false);
+        oreFeatures.addManagedOreFeature(MOD_ID, AetherBlocks.oreZaniteHolystone, 2, 2, 1/3f, false);
+    }
+
+    @Override
+    protected void initRandom() {
+        OverworldRandomFeatures randomFeatures = ChunkDecoratorAetherAPI.randomFeatures;
+
+        randomFeatures.addFeatureSurface(new WorldFeatureTreeSkyroot(5), 2);
+        randomFeatures.addFeature(new WorldFeatureFlowers(AetherBlocks.flowerPurple.id), 2, 1);
+        randomFeatures.addFeature(new WorldFeatureFlowers(AetherBlocks.flowerWhite.id), 2, 1);
+    }
+
+    @Override
+    protected void initBiome() {
+
+    }
+}
