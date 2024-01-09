afc1c768cf665e92550dc4d00bcdc11d500631c7
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2024-01-08 20:29:30 -0600

    Fixing up old existing code

--- src/main/java/bta/aether/block/BlockSaplingSkyroot.java
+++ src/main/java/bta/aether/block/BlockSaplingSkyroot.java
@@ -1,7 +1,5 @@
 package bta.aether.block;
 
-import bta.aether.Aether;
-import net.minecraft.core.block.Block;
 import net.minecraft.core.block.BlockSaplingBase;
 import net.minecraft.core.world.World;
 import net.minecraft.core.world.generate.feature.WorldFeature;
@@ -13,17 +11,16 @@ public class BlockSaplingSkyroot extends BlockSaplingBase {
     public BlockSaplingSkyroot(String key, int id) {
         super(key, id);
     }
-
+    @Override
     public boolean canThisPlantGrowOnThisBlockID(int i) {
         return i == AetherBlocks.grassAether.id;
     }
-
+    @Override
     public void growTree(World world, int i, int j, int k, Random random) {
-        Object treeSmall = new WorldFeatureTree(AetherBlocks.leavesSkyroot.id, AetherBlocks.logSkyroot.id, 4);
+        WorldFeature treeSmall = new WorldFeatureTree(AetherBlocks.leavesSkyroot.id, AetherBlocks.logSkyroot.id, 4);
         world.setBlock(i, j, k, 0);
-        if (!((WorldFeature)treeSmall).generate(world, random, i, j, k)){
+        if (!treeSmall.generate(world, random, i, j, k)){
             world.setBlock(i, j, k, this.id);
         }
-
     }
 }
