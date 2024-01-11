ad5c697771dff83be3f7581c731248cfb7853ffe
Colin <colinjswee@gmail.com>
2024-01-11 00:22:04 -0500

    Add classes for each accessory type, making them equippable.

--- src/main/java/bta/aether/item/ItemToolAccessory.java
+++ src/main/java/bta/aether/item/ItemToolAccessory.java
@@ -1,10 +1,21 @@
 package bta.aether.item;
 
+import csweetla.accessoryapi.API.Accessory;
+import net.minecraft.core.entity.player.EntityPlayer;
 import net.minecraft.core.item.Item;
+import net.minecraft.core.item.ItemStack;
 
-public class ItemToolAccessory extends Item {
+public abstract class ItemToolAccessory extends Item implements Accessory {
     public ItemToolAccessory(int id){
         super(id);
         this.maxStackSize = 1;
+    }
+
+    @Override
+    public void onAccessoryAdded(EntityPlayer player, ItemStack accessory) {
+    }
+
+    @Override
+    public void onAccessoryRemoved(EntityPlayer player, ItemStack accessory) {
     }
 }
