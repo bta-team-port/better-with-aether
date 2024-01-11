a0589f86e41d36af83cb05f1d51ac4ecaf113281
Oly <33404595+Olypolyu@users.noreply.github.com>
2024-01-11 08:09:33 -0300

    added dungeon blocks

---
+++ src/main/java/bta/aether/entity/EntityAetherBossBase.java
@@ -0,0 +1,10 @@
+package bta.aether.entity;
+
+import net.minecraft.core.entity.monster.EntityMonster;
+import net.minecraft.core.world.World;
+
+public class EntityAetherBossBase extends EntityMonster {
+    public EntityAetherBossBase(World world) {
+        super(world);
+    }
+}
