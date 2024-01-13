4c24d8f99f7e759ce092365b88e738811b33d541
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2024-01-13 15:00:45 -0600

    moving clientside stuffs to its own class

--- src/main/java/bta/aether/AetherClient.java
+++ src/main/java/bta/aether/AetherClient.java
@@ -1,10 +1,18 @@
 package bta.aether;
 
+import bta.aether.entity.AetherEntities;
+import bta.aether.entity.ArrowFlamingRenderer;
+import bta.aether.entity.EntityArrowFlaming;
+import bta.aether.entity.EntityFallingGravitite;
+import bta.aether.entity.EntityGoldenDart;
+import bta.aether.entity.GoldenDartRenderer;
 import bta.aether.gui.components.ComponentBossBar;
 import net.minecraft.client.gui.hud.AbsoluteLayout;
 import net.minecraft.client.gui.hud.ComponentAnchor;
 import net.minecraft.client.gui.hud.HudComponent;
 import net.minecraft.client.gui.hud.HudComponents;
+import net.minecraft.client.render.entity.FallingSandRenderer;
+import turniplabs.halplibe.helper.EntityHelper;
 import turniplabs.halplibe.util.ClientStartEntrypoint;
 
 public class AetherClient implements ClientStartEntrypoint {
@@ -12,11 +20,13 @@ public class AetherClient implements ClientStartEntrypoint {
 
     @Override
     public void beforeClientStart() {
-
+        EntityHelper.Client.assignEntityRenderer(EntityFallingGravitite.class, new FallingSandRenderer());
+        EntityHelper.Client.assignEntityRenderer(EntityArrowFlaming.class, new ArrowFlamingRenderer());
+        EntityHelper.Client.assignEntityRenderer(EntityGoldenDart.class, new GoldenDartRenderer());
+        new AetherEntities().initializeModels();
     }
 
     @Override
     public void afterClientStart() {
-
     }
 }
