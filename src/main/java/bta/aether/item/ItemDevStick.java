c56342f505dce634a339f6bc977b6fb603f67f42
Oly <Oly@urmums.house>
2024-01-13 07:09:23 -0300

    dungeon tooling, clouds refactor.

--- src/main/java/bta/aether/item/ItemDevStick.java
+++ src/main/java/bta/aether/item/ItemDevStick.java
@@ -1,19 +1,9 @@
 package bta.aether.item;
 
-import bta.aether.block.AetherBlocks;
-import bta.aether.entity.EntityDevBoss;
-import bta.aether.entity.tileEntity.TileEntityChestLocked;
-import bta.aether.world.AetherDimension;
-import bta.aether.world.LootTable;
-import net.minecraft.core.entity.player.EntityPlayer;
 import net.minecraft.core.item.Item;
-import net.minecraft.core.item.ItemStack;
-import net.minecraft.core.util.helper.Side;
-import net.minecraft.core.world.World;
 
 public class ItemDevStick extends Item {
     public ItemDevStick(int id) {
         super(id);
     }
-
 }
