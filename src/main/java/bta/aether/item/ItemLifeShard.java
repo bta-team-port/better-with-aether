8ced23b34f0f8f9f99c0f55d7461ad1961ae3a73
Colin <colinjswee@gmail.com>
2024-01-14 00:42:46 -0500

    Made Lifeshard work

---
+++ src/main/java/bta/aether/item/ItemLifeShard.java
@@ -0,0 +1,26 @@
+package bta.aether.item;
+
+
+import net.minecraft.core.entity.player.EntityPlayer;
+import net.minecraft.core.item.Item;
+import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.world.World;
+
+import csweetla.accessoryapi.API.HealthHelper;
+
+public class ItemLifeShard extends Item {
+
+    public ItemLifeShard(int id) {
+        super(id);
+        setMaxStackSize(1);
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack itemstack, World world, EntityPlayer entityplayer) {
+        if (HealthHelper.getExtraHealth(entityplayer) < 20 && itemstack.consumeItem(entityplayer)) {
+            HealthHelper.addExtraHealth(entityplayer, 2);
+            entityplayer.heal(2);
+        }
+        return itemstack;
+    }
+}
