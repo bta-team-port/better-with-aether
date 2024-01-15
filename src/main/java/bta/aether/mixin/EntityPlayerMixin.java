dd7ff8b96ae9f8e43935d49974f21d52f9f5d9d3
oly <33404595+Olypolyu@users.noreply.github.com>
2024-01-15 09:50:58 -0300

    the garbo moa (#77)
    * made the jump icon 8x8
    
    * rename texture to aetherIcons.png. made jump icons overlap.
    
    * the garbo moa implementation
    
    ---------
    
    Co-authored-by: Oly <Oly@urmums.house>

--- src/main/java/bta/aether/mixin/EntityPlayerMixin.java
+++ src/main/java/bta/aether/mixin/EntityPlayerMixin.java
@@ -1,25 +1,51 @@
 package bta.aether.mixin;
 
+import bta.aether.entity.IPlayerJumpAmount;
 import bta.aether.gui.IAetherGuis;
 import bta.aether.tile.TileEntityEnchanter;
 import bta.aether.tile.TileEntityFreezer;
 import net.minecraft.core.entity.player.EntityPlayer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(value = EntityPlayer.class, remap = false)
-public class EntityPlayerMixin implements IAetherGuis {
+public class EntityPlayerMixin implements IAetherGuis, IPlayerJumpAmount {
+    @Unique
+    private int jumpMaxAmount;
+    @Unique
+    private int jumpAmount;
+
     @Override
     public void aether$displayGUIEnchanter(TileEntityEnchanter tile) {
-
     }
 
     @Override
     public void aether$displayGUIFreeze(TileEntityFreezer tile) {
-
     }
 
     @Override
     public void aether$displayGUILoreBook(String loreId) {
-
     }
+
+    @Unique
+    public int aether$getJumpMaxAmount() {
+        return this.jumpMaxAmount;
+    }
+
+    @Unique
+    public int aether$getJumpAmount() {
+        return this.jumpAmount;
+    }
+
+    @Unique
+    public void aether$setJumpMaxAmount(int jumpMaxAmount) {
+        this.jumpMaxAmount = jumpMaxAmount;
+        if (this.jumpMaxAmount > this.jumpAmount) this.jumpAmount = this.jumpMaxAmount;
+    }
+
+    @Unique
+    public void aether$setJumpAmount(int jumpAmount) {
+        this.jumpAmount = jumpAmount;
+    }
+
 }
