73df171cef453b08cecb1e97ff590127be2c63e6
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2024-01-15 12:17:05 -0600

    Dimension Swapping when falling

--- src/main/java/bta/aether/mixin/EntityPlayerMixin.java
+++ src/main/java/bta/aether/mixin/EntityPlayerMixin.java
@@ -4,16 +4,29 @@ import bta.aether.entity.IPlayerJumpAmount;
 import bta.aether.gui.IAetherGuis;
 import bta.aether.tile.TileEntityEnchanter;
 import bta.aether.tile.TileEntityFreezer;
+import bta.aether.world.AetherDimension;
+import net.minecraft.core.entity.EntityLiving;
 import net.minecraft.core.entity.player.EntityPlayer;
+import net.minecraft.core.world.Dimension;
+import net.minecraft.core.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = EntityPlayer.class, remap = false)
-public class EntityPlayerMixin implements IAetherGuis, IPlayerJumpAmount {
+public abstract class EntityPlayerMixin extends EntityLiving implements IAetherGuis, IPlayerJumpAmount {
+    @Shadow public int dimension;
     @Unique
     private int jumpMaxAmount;
     @Unique
     private int jumpAmount;
+
+    public EntityPlayerMixin(World world) {
+        super(world);
+    }
 
     @Override
     public void aether$displayGUIEnchanter(TileEntityEnchanter tile) {
@@ -48,4 +61,16 @@ public class EntityPlayerMixin implements IAetherGuis, IPlayerJumpAmount {
         this.jumpAmount = jumpAmount;
     }
 
+    @Unique
+    public int teleportDelay = 0;
+    @Inject(method = "tick()V", at = @At("HEAD"))
+    private void fallIntoOverworldFromAether(CallbackInfo ci){
+        teleportDelay--;
+        if (teleportDelay < 0 && Dimension.getDimensionList().get(dimension) == AetherDimension.dimensionAether){
+            if (this.y < world.worldType.getMinY() - 10){
+                teleportDelay = 20;
+                AetherDimension.dimensionShift((EntityPlayer) (Object)this, Dimension.overworld.id);
+            }
+        }
+    }
 }
