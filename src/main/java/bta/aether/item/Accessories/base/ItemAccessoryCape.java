7a9e9cb85ffeb069912141e7d466aa73b3073a8a
ToufouMaster <owen.kala@gmx.fr>
2024-01-16 22:41:54 +0100

    Testing aether cape rendering.
    it's a mess

--- src/main/java/bta/aether/item/Accessories/base/ItemAccessoryCape.java
+++ src/main/java/bta/aether/item/Accessories/base/ItemAccessoryCape.java
@@ -13,4 +13,14 @@ public class ItemAccessoryCape extends ItemToolAccessory {
     public String[] getAccessoryTypes(ItemStack itemStack) {
         return new String[]{"cape"};
     }
+
+    public String getTexturePath() {
+        return "assets/aether/other/AetherCape.png";
+        //return "aether/other/AgilityCape.png";
+        //return "aether/other/BlueCape.png";
+        //return "aether/other/RedCape.png";
+        //return "aether/other/WhiteCape.png";
+        //return "aether/other/YellowCape.png";
+        //return null;
+    }
 }
