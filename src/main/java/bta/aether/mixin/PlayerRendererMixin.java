c977b57937da58af85e7bd1463ecf1f088b3079d
ToufouMaster <owen.kala@gmx.fr>
2024-01-16 22:41:54 +0100

    Testing aether cape rendering.
    it's a mess

--- src/main/java/bta/aether/mixin/PlayerRendererMixin.java
+++ src/main/java/bta/aether/mixin/PlayerRendererMixin.java
@@ -2,10 +2,12 @@ package bta.aether.mixin;
 
 
 import bta.aether.entity.IAetherAccessories;
+import bta.aether.item.Accessories.base.ItemAccessoryCape;
 import bta.aether.item.Accessories.base.ItemAccessoryGloves;
 import bta.aether.item.Accessories.base.ItemAccessoryPendant;
 import com.llamalad7.mixinextras.sugar.Local;
 import csweetla.accessoryapi.API.Accessory;
+import net.minecraft.client.render.ImageParser;
 import net.minecraft.client.render.entity.LivingRenderer;
 import net.minecraft.client.render.entity.PlayerRenderer;
 import net.minecraft.client.render.model.ModelBase;
@@ -17,11 +19,14 @@ import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+@Debug(export = true)
 @Mixin(value = PlayerRenderer.class, remap = false)
 public class PlayerRendererMixin extends LivingRenderer<EntityPlayer> {
+    @Shadow private ModelBiped modelBipedMain;
     @Unique
     private final ModelBiped modelAccessories = new ModelBiped(1.0F);
 
@@ -71,6 +76,7 @@ public class PlayerRendererMixin extends LivingRenderer<EntityPlayer> {
         this.modelAccessories.bipedBody.showModel = renderPass == 4;
         this.modelAccessories.bipedLeftArm.showModel = renderPass == 10;
         this.modelAccessories.bipedRightArm.showModel = renderPass == 10;
+        this.modelBipedMain.bipedCloak.showModel = renderPass == 5;
         String path = null;
         ItemStack itemStack = player.inventory.armorInventory[renderPass];
         if (itemStack == null) return;
@@ -80,9 +86,18 @@ public class PlayerRendererMixin extends LivingRenderer<EntityPlayer> {
             path = ((ItemAccessoryGloves)item).getTexturePath();
         } else if (item instanceof ItemAccessoryPendant) {
             path = ((ItemAccessoryPendant)item).getTexturePath();
+        } else if (item instanceof ItemAccessoryCape) {
+            path = ((ItemAccessoryCape)item).getTexturePath();
         }
         this.loadTexture(path);
         this.setRenderPassModel(this.modelAccessories);
         info.setReturnValue(true);
     }
+
+    @Redirect(method = "renderSpecials(Lnet/minecraft/core/entity/player/EntityPlayer;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/PlayerRenderer;loadDownloadableTexture(Ljava/lang/String;Ljava/lang/String;Lnet/minecraft/client/render/ImageParser;)Z"))
+    public boolean renderSpecials(PlayerRenderer instance, String s1, String s2, ImageParser imageParser)  {
+        if (!modelBipedMain.bipedCloak.showModel) return instance.loadDownloadableTexture(s1, s2, imageParser);
+        return true;
+    }
+
 }
