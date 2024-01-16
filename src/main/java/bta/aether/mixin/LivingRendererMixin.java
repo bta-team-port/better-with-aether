f2b57b6f958740f369a848959ce1181ddef99a89
ToufouMaster <owen.kala@gmx.fr>
2024-01-16 20:52:51 +0100

    Removed the @Debug mixin lines

--- src/main/java/bta/aether/mixin/LivingRendererMixin.java
+++ src/main/java/bta/aether/mixin/LivingRendererMixin.java
@@ -2,14 +2,12 @@ package bta.aether.mixin;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.render.entity.LivingRenderer;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Debug(export = true)
 @Mixin(value = LivingRenderer.class, remap = false)
 public class LivingRendererMixin {
 
