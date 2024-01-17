9944e8ce38147916b43acb278ef32ca47d255f1d
oly <33404595+Olypolyu@users.noreply.github.com>
2024-01-17 02:11:14 -0300

    garbage slider work (#91)
    * slider work
    
    ---------
    
    Co-authored-by: Oly <Oly@urmums.house>

--- src/main/java/bta/aether/entity/EntityBossSlider.java
+++ src/main/java/bta/aether/entity/EntityBossSlider.java
@@ -1,28 +1,52 @@
 package bta.aether.entity;
 
+import bta.aether.block.BlockDungeon;
 import com.mojang.nbt.CompoundTag;
+import net.minecraft.core.block.Block;
 import net.minecraft.core.entity.Entity;
 import net.minecraft.core.entity.player.EntityPlayer;
+import net.minecraft.core.enums.EnumDropCause;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.item.tool.ItemToolPickaxe;
 import net.minecraft.core.lang.I18n;
+import net.minecraft.core.sound.SoundType;
 import net.minecraft.core.util.helper.DamageType;
+import net.minecraft.core.util.helper.Direction;
 import net.minecraft.core.world.World;
 
 public class EntityBossSlider extends EntityAetherBossBase{
 
-    boolean awake = false;
-    int angerThreshold = 50;
-    int attackCoolDown = 30;
+    public boolean awake = false;
+    public int angerThreshold = 50;
+    public float baseDamage = 10F;
+    public int attackCoolDown = 0;
+    private boolean moving = false;
+    public int maxAttackCoolDown = 45;
+    public float baseSpeed = 1.5F;
+
+    private float momentumX = 0;
+    private float momentumZ = 0;
 
     public EntityBossSlider(World world) {
         super(world, 100, "aether.slider.name");
         this.setSize(2f,2f);
         this.viewScale = 2f;
+        this.setSize(2.5F, 2.5F);
     }
 
     @Override
     protected void roamRandomPath() {
+    }
+
+    @Override
+    public boolean collidesWith(Entity entity) {
+        if (Math.abs(this.momentumZ) > 0F || Math.abs(this.momentumX) > 0F) {
+            entity.hurt(this, (int) (baseDamage * getAngerModifier()), DamageType.FALL);
+            entity.hurt(this, (int) ((baseDamage * .50F) * getAngerModifier()), DamageType.COMBAT);
+            doExplosionEffect(entity.world, entity.x, entity.y, entity.z);
+        }
+
+        return super.collidesWith(entity);
     }
 
     public String getEntityTexture() {
@@ -45,29 +69,109 @@ public class EntityBossSlider extends EntityAetherBossBase{
         return "step.stone";
     }
 
-
-    @Override
-    public void tick() {
-        this.xRot = 0f;
-        this.yRot = 0f;
-
-        if ( awake && this.world.loadedEntityList.stream().noneMatch(entity -> entity instanceof EntityPlayer && getDistanceFrom(this.x, this.y, this.z, entity.x, entity.y, entity.z) < 100)) {
-            this.awake = false;
-            returnToPedestal(this.world, this.x, this.y, this.z);
-        }
-
-        super.tick();
-    }
-
+    // TODO: add return to pedestal method.
     public void returnToPedestal(World world, double x, double y, double z) {
     }
 
-    public void doExplosionEffect(World world, double x, double y, double z){
-        double XParticle = x + 0.5F + ((double) world.rand.nextFloat()) - ((double) world.rand.nextFloat() * 0.375F);
-        double YParticle = y + 0.5F + ((double) world.rand.nextFloat()) - ((double) world.rand.nextFloat() * 0.375F);
-        double ZParticle = z + 0.5F + ((double) world.rand.nextFloat()) - ((double) world.rand.nextFloat() * 0.375F);
+    @Override
+    public void tick() {
+        super.baseTick();
 
-        world.spawnParticle("explode", XParticle, YParticle, ZParticle, 0,0,0);
+        if (awake) {
+            if (this.world.loadedEntityList.stream().noneMatch(entity -> entity instanceof EntityPlayer && getDistanceFrom(this.x, this.y, this.z, entity.x, entity.y, entity.z) < 100)) {
+                this.awake = false;
+                returnToPedestal(this.world, this.x, this.y, this.z);
+                return;
+            }
+
+            EntityPlayer target = (EntityPlayer) findPlayerToAttack();
+            if (target != null && !moving && (Math.abs(this.momentumZ) <= 0.05F && Math.abs(this.momentumX) <= 0.05F)) {
+                this.speed = this.baseSpeed * getSpeedModifier(target);
+                this.attackCoolDown = this.maxAttackCoolDown * this.health/this.maxHealth;
+
+                switch (calculateDirection((int) target.x, (int) target.z)){
+                    case NORTH:
+                        this.moving = true;
+                        this.momentumZ -= speed;
+                        break;
+
+                    case SOUTH:
+                        this.moving = true;
+                        this.momentumZ += speed;
+                        break;
+
+                    case EAST:
+                        this.moving = true;
+                        this.momentumX += speed;
+                        break;
+
+                    case WEST:
+                        this.moving = true;
+                        this.momentumX -= speed;
+                        break;
+                }
+            }
+
+            this.momentumZ *= 0.75F;
+            this.momentumX *= 0.75F;
+            move(momentumX, 0, momentumZ);
+
+            attackCoolDown--;
+            if (attackCoolDown <= 0) moving = false;
+        }
+    }
+
+    public float getSpeedModifier(Entity target){
+        int distance = (int) getDistanceFrom(this.x, this.y, this.z, target.x, target.y, target.z);
+        if (distance > 3) {
+            return getAngerModifier();
+        }
+
+        return (float) distance / 10;
+    }
+
+    public float getAngerModifier() {
+        return 1.0F + ( (float) (this.maxHealth - this.health) / this.maxHealth );
+    }
+
+    public Direction calculateDirection(int entityX, int entityZ) {
+        int deltaX = (int) (this.x - entityX);
+        int deltaZ = (int) (this.z - entityZ);
+
+        if (Math.abs(deltaX) > Math.abs(deltaZ)) {
+            if (deltaX < 0) {
+                return Direction.EAST;
+            } else {
+                return Direction.WEST;
+            }
+        } else {
+            if (deltaZ < 0) {
+                return Direction.SOUTH;
+            } else {
+                return Direction.NORTH;
+            }
+        }
+    }
+
+    public void doBlockSmash(World world, int x, int y, int z) {
+        Block block = world.getBlock(x, y, z);
+        if (block != null && !(block instanceof BlockDungeon)) {
+            this.moving = false;
+            block.dropBlockWithCause(world, EnumDropCause.EXPLOSION, x, y, z, world.getBlockMetadata(x, y,z), world.getBlockTileEntity(x, y, z));
+            doExplosionEffect(world, x, y, z);
+        }
+    }
+
+    public void doExplosionEffect(World world, double x, double y, double z){
+        for (int particle = 0; particle < 16; particle++) {
+            double XParticle = x + 0.5F + ((double) world.rand.nextFloat()) - ((double) world.rand.nextFloat() * 0.375F);
+            double YParticle = y + 0.5F + ((double) world.rand.nextFloat()) - ((double) world.rand.nextFloat() * 0.375F);
+            double ZParticle = z + 0.5F + ((double) world.rand.nextFloat()) - ((double) world.rand.nextFloat() * 0.375F);
+
+            world.spawnParticle("explode", XParticle, YParticle, ZParticle, 0,0,0);
+        }
+
+        world.playSoundEffect(SoundType.WORLD_SOUNDS, x, y, z, "random.explode", 1.5F, (1.0F + (world.rand.nextFloat() - world.rand.nextFloat()) * 0.2F) * 0.7F);
     }
 
     @Override
@@ -76,12 +180,14 @@ public class EntityBossSlider extends EntityAetherBossBase{
     }
 
     @Override
-    public boolean hurt(Entity attacker, int i, DamageType type) {
+    public boolean hurt(Entity attacker, int damage, DamageType type) {
+        if(this.awake && type == DamageType.BLAST) return super.hurt(attacker, damage/4, type);
+
         if (attacker instanceof EntityPlayer){
             ItemStack item = ((EntityPlayer)attacker).inventory.mainInventory[((EntityPlayer)attacker).inventory.currentItem];
             if (item != null && item.getItem() instanceof ItemToolPickaxe) {
                 awake = true;
-                return super.hurt(attacker, i, type);
+                return super.hurt(attacker, damage, type);
             } else if (!this.awake) {
                 String message = "<"+((EntityPlayer)attacker).getDisplayName()+"> "+ I18n.getInstance().translateKey("aether.slider.hit_fail");
                 ((EntityPlayer)attacker).addChatMessage(message);
