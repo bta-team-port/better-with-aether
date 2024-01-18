2259c69a41094fe17a871ec48adcd2399d9c2724
azurelmao <de_arcane@protonmail.com>
2024-01-18 21:44:37 +0100

    Made pendants and capes display properly

--- src/main/java/bta/aether/item/Accessories/ItemAccessoryInvisibilityCloak.java
+++ src/main/java/bta/aether/item/Accessories/ItemAccessoryInvisibilityCloak.java
@@ -2,12 +2,18 @@ package bta.aether.item.Accessories;
 
 import bta.aether.entity.IAetherAccessories;
 import bta.aether.item.Accessories.base.ItemAccessoryCape;
+import bta.aether.item.ItemToolAccessory;
 import net.minecraft.core.entity.player.EntityPlayer;
 import net.minecraft.core.item.ItemStack;
 
-public class ItemAccessoryInvisibilityCape extends ItemAccessoryCape {
-    public ItemAccessoryInvisibilityCape(int id) {
+public class ItemAccessoryInvisibilityCloak extends ItemToolAccessory {
+    public ItemAccessoryInvisibilityCloak(int id) {
         super(id);
+    }
+
+    @Override
+    public String[] getAccessoryTypes(ItemStack itemStack) {
+        return new String[]{"cape"};
     }
 
     @Override
