1867917ca17589029bcc75fc652e97c8a47a5c51
azurelmao <de_arcane@protonmail.com>
2024-01-20 19:12:45 +0100

    Ice pendant and ring now freeze stuff

---
+++ src/main/java/bta/aether/item/ItemPendantIce.java
@@ -0,0 +1,10 @@
+package bta.aether.item;
+
+import bta.aether.item.Accessories.base.ItemAccessoryPendant;
+
+public class ItemPendantIce extends ItemAccessoryPendant {
+    public ItemPendantIce(String name, int id, String texturePath) {
+        super(name, id, texturePath);
+        setMaxDamage(2999);
+    }
+}
