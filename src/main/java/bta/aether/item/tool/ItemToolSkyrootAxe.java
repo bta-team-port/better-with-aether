9285ba2dee7d7ed2e1ae482910378dd59ee33834
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2024-01-21 20:30:52 -0600

    Skyroot doubling *should* work properly now

---
+++ src/main/java/bta/aether/item/tool/ItemToolSkyrootAxe.java
@@ -0,0 +1,10 @@
+package bta.aether.item.tool;
+
+import bta.aether.item.tool.base.ItemToolAetherAxe;
+import net.minecraft.core.item.material.ToolMaterial;
+
+public class ItemToolSkyrootAxe extends ItemToolAetherAxe {
+    public ItemToolSkyrootAxe(String name, int id, ToolMaterial enumtoolmaterial) {
+        super(name, id, enumtoolmaterial);
+    }
+}
---
+++ src/main/java/bta/aether/item/tool/ItemToolSkyrootShovel.java
@@ -0,0 +1,10 @@
+package bta.aether.item.tool;
+
+import bta.aether.item.tool.base.ItemToolAetherShovel;
+import net.minecraft.core.item.material.ToolMaterial;
+
+public class ItemToolSkyrootShovel extends ItemToolAetherShovel {
+    public ItemToolSkyrootShovel(String name, int id, ToolMaterial enumtoolmaterial) {
+        super(name, id, enumtoolmaterial);
+    }
+}
