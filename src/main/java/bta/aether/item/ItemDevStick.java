85e2fdc2c0baf90bf5381511855ab180b7d1915c
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2024-02-24 08:55:16 -0600

    basic silver dungeon stuff

--- src/main/java/bta/aether/item/ItemDevStick.java
+++ src/main/java/bta/aether/item/ItemDevStick.java
@@ -1,17 +1,9 @@
 package bta.aether.item;
 
-import bta.aether.block.AetherBlocks;
-import bta.aether.entity.EntityBossSlider;
-import bta.aether.entity.projectiles.EntityHammerHead;
 import bta.aether.entity.projectiles.EntityZephyrSnowball;
-import bta.aether.util.StructureLoader;
-import bta.aether.world.AetherDimension;
-import bta.aether.util.LootTable;
-import bta.aether.world.generate.WorldFeatureAetherDungeonBase;
 import net.minecraft.core.entity.player.EntityPlayer;
 import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemStack;
-import net.minecraft.core.util.helper.Side;
 import net.minecraft.core.world.World;
 
 public class ItemDevStick extends Item {
