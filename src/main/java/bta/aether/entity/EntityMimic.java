bd7890854e8d046b67bd05d876da9a37c5957c2f
Oly <Oly@yamums.house>
2024-04-06 02:50:48 -0300

    unshat boss code again (mimic edition)

--- src/main/java/bta/aether/entity/EntityMimic.java
+++ src/main/java/bta/aether/entity/EntityMimic.java
@@ -8,11 +8,12 @@ public class EntityMimic extends EntityMonster {
     public EntityMimic(World world) {
         super(world);
     }
-
+    private int tickCounter = 0;
     @Override
     public void tick() {
         super.tick();
-        if (world.players.stream().noneMatch(entityPlayer -> this.distanceToSqr(entityPlayer) < 10000) && world.getBlockId((int) this.x, (int) this.y, (int) this.z) == 0) {
+        tickCounter = (tickCounter+1)%100;
+        if (tickCounter == 0 && world.players.stream().noneMatch(entityPlayer -> this.distanceToSqr(entityPlayer) < 10000) && world.getBlockId((int) this.x, (int) this.y, (int) this.z) == 0) {
             world.setBlockWithNotify((int) this.x, (int) this.y, (int) this.z, AetherBlocks.chestMimic.id);
             this.remove();
         }
