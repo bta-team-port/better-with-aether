74f1bd1638734f0ed74754e9f6f414e5574ad209
LukeisStuff <lukeisawesome03@gmail.com>
2024-04-06 22:55:47 -0400

    Broken Pig thing Wings wont work for some reason

---
+++ src/main/java/bta/aether/entity/model/ModelPhyg1.java
@@ -0,0 +1,13 @@
+package bta.aether.entity.model;
+
+import net.minecraft.client.render.model.ModelQuadruped;
+
+public class ModelPhyg1 extends ModelQuadruped {
+    public ModelPhyg1() {
+        super(6, 0.0F);
+    }
+
+    public ModelPhyg1(float f) {
+        super(6, f);
+    }
+}
