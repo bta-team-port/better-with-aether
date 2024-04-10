5f9e2f365ac34d4dfabc26800bbdd61e8cec0c53
LukeisStuff <lukeisawesome03@gmail.com>
2024-04-10 16:01:22 -0400

    Broken Phow

---
+++ src/main/java/bta/aether/entity/renderer/PhowRenderer.java
@@ -0,0 +1,33 @@
+package bta.aether.entity.renderer;
+
+import bta.aether.entity.EntityPhow;
+import bta.aether.entity.model.ModelPhow2;
+import net.minecraft.client.render.entity.LivingRenderer;
+import net.minecraft.client.render.model.ModelBase;
+import net.minecraft.core.entity.EntityLiving;
+
+public class PhowRenderer extends LivingRenderer<EntityPhow> {
+    public PhowRenderer(ModelBase modelbase, ModelBase modelbase1, float f) {
+        super(modelbase, f);
+        this.setRenderPassModel(modelbase1);
+    }
+
+    protected boolean setWoolColorAndRender(EntityPhow flyingcow, int i, float f) {
+        if (i == 0) {
+            this.loadTexture("/assets/aether/mobs/Wings.png");
+            ModelPhow2.flyingcow = flyingcow;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    protected boolean render(EntityLiving entityliving, int i, float f) {
+        return this.setWoolColorAndRender((EntityPhow) entityliving, i, f);
+    }
+
+    @Override
+    public void doRender(EntityPhow entity, double d, double e, double f, float g, float h) {
+
+    }
+}
