e8f079290849dcf67cfbb607ad521deb0105267b
LukeisStuff <lukeisawesome03@gmail.com>
2024-04-11 10:40:26 -0400

    Implemented more guidebook entity pages, fix for sheepuff lang, First progress on texture rework with new/old textures and names

--- src/main/java/bta/aether/Aether.java
+++ src/main/java/bta/aether/Aether.java
@@ -101,7 +101,7 @@ public class Aether implements GameStartEntrypoint, ClientStartEntrypoint, ModIn
         EntityHelper.Client.assignEntityRenderer(EntityGoldenDart.class, new aetherArrowRenderer("/assets/aether/mobs/entitygoldendart.png"));
         EntityHelper.Client.assignEntityRenderer(EntityPoisonDart.class, new aetherArrowRenderer("/assets/aether/mobs/entitypoisondart.png"));
         EntityHelper.Client.assignEntityRenderer(EntityEnchantedDart.class, new aetherArrowRenderer("/assets/aether/mobs/entityenchanteddart.png"));
-        EntityHelper.Client.assignEntityRenderer(EntityHammerHead.class, new SnowballRenderer(TextureHelper.getOrCreateItemTextureIndex(Aether.MOD_ID, "../other/NotchWave.png")));
+        EntityHelper.Client.assignEntityRenderer(EntityHammerHead.class, new SnowballRenderer(TextureHelper.getOrCreateItemTextureIndex(Aether.MOD_ID, "/assets/aether/other/NotchWave.png")));
         EntityHelper.Client.assignEntityRenderer(EntityZephyrSnowball.class, new SnowballRenderer(Item.ammoSnowball.getIconIndex(new ItemStack(Item.ammoSnowball))));
 
         new AetherEntities().initializeModels();
