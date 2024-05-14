05f65d5953c692e62f18fb7fbae62cb38467aaca
LukeisStuff <lukeisawesome03@gmail.com>
2024-05-14 18:48:15 -0400

    aerwhale stuff

--- src/main/java/bta/aether/entity/renderer/AerwhaleRenderer.java
+++ src/main/java/bta/aether/entity/renderer/AerwhaleRenderer.java
@@ -18,8 +18,8 @@ public class AerwhaleRenderer extends LivingRenderer<EntityAerwhale> {
         GL11.glPushMatrix();
         this.loadTexture("/assets/aether/mobs/aerwhale/0.png");
         GL11.glTranslatef((float) d, (float) e, (float) f);
-        GL11.glRotatef(90.0F - entity.yRotO, 0.0F, 1.0F, 0.0F);
-        GL11.glRotatef(180.0F - entity.xRotO, 1.0F, 0.0F, 0.0F);
+        GL11.glRotatef(90.0F - entity.yRot, 0.0F, 1.0F, 0.0F);
+        GL11.glRotatef(180.0F - entity.xRot, 1.0F, 0.0F, 0.0F);
         GL11.glScalef(10.0F, 10.0F, 10.0F);
         this.model.render(0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
         GL11.glPopMatrix();
