8fa245d05af43c054a3a97d1f07d9ceed9c7e444
LukeisStuff <lukeisawesome03@gmail.com>
2024-05-14 16:30:27 -0400

    bronze dungeon redone

--- src/main/java/bta/aether/item/ItemDevStick.java
+++ src/main/java/bta/aether/item/ItemDevStick.java
@@ -1,13 +1,8 @@
 package bta.aether.item;
 
-import bta.aether.world.generate.feature.WorldFeatureAetherDungeonBronzeCursed;
 import net.minecraft.core.entity.Entity;
 import net.minecraft.core.entity.EntityLiving;
-import net.minecraft.core.entity.player.EntityPlayer;
 import net.minecraft.core.item.Item;
-import net.minecraft.core.item.ItemStack;
-import net.minecraft.core.util.helper.Side;
-import net.minecraft.core.world.World;
 
 public class ItemDevStick extends Item {
     public ItemDevStick(String name, int id) {
@@ -21,9 +16,9 @@ public class ItemDevStick extends Item {
         return Integer.MAX_VALUE;
     }
 
-    @Override
-    public boolean onItemUse(ItemStack itemstack, EntityPlayer entityplayer, World world, int blockX, int blockY, int blockZ, Side side, double xPlaced, double yPlaced) {
-        new WorldFeatureAetherDungeonBronzeCursed().generate(world, world.rand, blockX, blockY, blockZ);
-        return true;
-    }
+//    @Override
+//    public boolean onItemUse(ItemStack itemstack, EntityPlayer entityplayer, World world, int blockX, int blockY, int blockZ, Side side, double xPlaced, double yPlaced) {
+//        new WorldFeatureAetherDungeonBronze().generate(world, world.rand, blockX, blockY, blockZ);
+//        return true;
+//    }
 }
