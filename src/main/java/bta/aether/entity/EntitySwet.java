e2160594514412aede9af44d20056d2cca57cc13
Oly <Oly@yamums.house>
2024-07-27 00:21:38 -0300

    swet work

--- src/main/java/bta/aether/entity/EntitySwet.java
+++ src/main/java/bta/aether/entity/EntitySwet.java
@@ -1,66 +1,204 @@
 package bta.aether.entity;
 
+import bta.aether.Aether;
 import bta.aether.block.AetherBlocks;
 import bta.aether.item.AetherItems;
+import com.mojang.nbt.CompoundTag;
+import net.minecraft.core.block.Block;
+import net.minecraft.core.block.material.Material;
+import net.minecraft.core.entity.Entity;
+import net.minecraft.core.entity.EntityLiving;
 import net.minecraft.core.entity.player.EntityPlayer;
+import net.minecraft.core.enums.EnumBlockSoundEffectType;
+import net.minecraft.core.util.helper.DamageType;
+import net.minecraft.core.util.helper.MathHelper;
+import net.minecraft.core.util.phys.Vec3d;
 import net.minecraft.core.world.World;
+import net.minecraft.core.world.pathfinder.Path;
+import org.lwjgl.Sys;
+
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
 
 public class EntitySwet extends EntityAetherAnimal {
+    private final Random rand = new Random();
+
     private int jumpDelay;
     private int cooldownInactive;
-    public boolean activated;
+    private int jumpsCounter = 0;
+    public boolean isGold;
+    private boolean isfriendly;
+    private boolean targetPlayer = false;
 
     public EntitySwet(World world) {
         super(world);
         this.cooldownInactive = 0;
-        this.activated = false;
+        this.isGold = rand.nextBoolean();
         this.heightOffset = 0.0F;
         this.scoreValue = 100;
+        this.setHealthRaw(this.getMaxHealth());
         this.setSize(1f, 1f);
+
+        if (this.isGold) this.speed = 3.0F;
+        else this.speed = 1.5f;
     }
 
     public String getEntityTexture() {
-        return "/assets/aether/mobs/swet/swet_blue.png";
+        if (isGold) return "/assets/aether/mobs/swet/swet_blue.png";
+        else return "/assets/aether/mobs/swet/swet_gold.png";
     }
 
     @Override
     public void tick() {
-        boolean flag = this.onGround;
+        boolean onGround_last = this.onGround;
+
+        if (this.isUnderLiquid(Material.water)) this.damageEntity(999999999, DamageType.DROWN);
+        if (!this.onGround && this.yd < 0.0) this.yd *= 0.9;
+
+        if (this.yd > 0.05F) {
+            for (int i = 0; i < 3; ++i) {
+                double d = (float) this.x + (this.rand.nextFloat() - this.rand.nextFloat()) * 0.3F;
+                double d1 = (float) this.y + this.bbHeight;
+                double d2 = (float) this.z + (this.rand.nextFloat() - this.rand.nextFloat()) * 0.3F;
+                this.world.spawnParticle("splash", d, d1 - 0.25, d2, 0.0, 0.0, 0.0);
+            }
+        }
+
         super.tick();
-        if (this.onGround && !flag) {
-            this.world.playSoundAtEntity(null, this, "mob.slime", this.getSoundVolume(), ((this.random.nextFloat() - this.random.nextFloat()) * 0.2f + 1.0f) / 0.8f);
+        if (this.onGround && !onGround_last) this.world.playSoundAtEntity(null, this, "mob.slime", this.getSoundVolume(), ((this.random.nextFloat() - this.random.nextFloat()) * 0.2f + 1.0f) / 0.8f);
+        if (!this.world.isClientSide && this.world.difficultySetting == 0) this.remove();
+
+
+//        if (this.passenger != null && this.onGround && rand.nextInt(60) == 0) {
+//            this.yd += 2.2;
+//        }
+
+        if (this.passenger == null) {
+            List<Entity> list = this.world.getEntitiesWithinAABB(EntityLiving.class, this.bb.copy().expand(0.5, 0.75, 0.5));
+            if (!list.isEmpty()) {
+                Entity entity = list.get(0);
+                if (!(entity instanceof EntitySwet) && rand.nextBoolean()) {
+                    if (!(entity instanceof EntityPlayer)) this.capturePrey(entity);
+                    else if (((EntityPlayer) entity).gamemode.areMobsHostile()) this.capturePrey(entity);
+                }
+            }
         }
-        if (!this.world.isClientSide && this.world.difficultySetting == 0) {
-            this.remove();
+    }
+
+    @Override
+    public double getRideHeight() {
+        return this.bbHeight/4;
+    }
+
+    public void capturePrey(Entity entity) {
+        this.splorch();
+        this.setPos(this.x, this.y, this.z);
+        this.setSize(entity.bbWidth + rand.nextFloat(), entity.bbHeight + rand.nextFloat());
+        entity.startRiding(this);
+        this.roamRandomPath();
+    }
+
+    public void dissolve() {
+        for (int i = 0; i < 50; ++i) {
+            float f = this.rand.nextFloat() * 3.141593F * 2.0F;
+            float f1 = this.rand.nextFloat() * 0.5F + 0.25F;
+            float f2 = MathHelper.sin(f) * f1;
+            float f3 = MathHelper.cos(f) * f1;
+            this.world.spawnParticle("splash", this.x + (double) f2, this.bb.minY + 1.25, this.z + (double) f3, (double) f2 * 1.5 + this.xd, 4.0, (double) f3 * 1.5 + this.zd);
         }
+
+        if (this.passenger != null) {
+            this.passenger.y += (double) (this.passenger.bbHeight - 0.3F);
+            this.passenger.startRiding(this);
+        }
+
+        this.remove();
+    }
+
+    private Path goSomewhere() {
+        final int distance = 8;
+        double angleRad = Math.toRadians(world.rand.nextInt(360));
+
+        Path result = null;
+
+        int tries = 60;
+        while (tries --> 0) {
+            int x, y, z;
+            x = (int) (distance * Math.cos(angleRad) + this.x);
+            z = (int) (distance * Math.sin(angleRad) + this.z);
+            y = world.getHeightValue(x, z);
+
+            if (world.getBlockId(x, y, z) != 0) continue;
+            Block block = world.getBlock(x, y - 1, z);
+            if (block==null) continue;
+            if (!block.blockMaterial.isSolid()) continue;
+            if (block.blockMaterial == Material.lava) continue;
+            if (block.blockMaterial == Material.water) continue;
+
+            double d3 = x - this.x; double d4 = y - this.y; double d5 = z - this.z;
+            if ((d3 * d3 + d4 * d4 + d5 * d5) >= (distance * distance * 1.25F)) continue;
+            result = world.getEntityPathToXYZ(this, x, y, z, 10.0F);
+
+            world.setBlockWithNotify(x, y-1, z, Block.blockDiamond.id);
+            world.setBlockWithNotify((int) this.x, (int) (this.y -1), (int) this.z, Block.blockGold.id);
+            break;
+        }
+
+        return result;
     }
 
     @Override
     protected void updatePlayerActionState() {
         this.tryToDespawn();
-        EntityPlayer entityplayer = this.world.getClosestPlayerToEntity(this, 16.0);
-        boolean targetPlayer = entityplayer != null &&
-                entityplayer.getGamemode().areMobsHostile() &&
-                canEntityBeSeen(entityplayer) &&
-                (entityplayer.inventory.armorItemInSlot(5) == null || entityplayer.inventory.armorItemInSlot(5).itemID != AetherItems.armorCapeSwet.id);
 
-        if (cooldownInactive > 0) {
-            cooldownInactive--;
+        if (this.passenger == null) {
+            if (this.entityToAttack != null && this.distanceTo(this.entityToAttack) > 32 ) {
+                EntityPlayer entityplayer = this.world.getClosestPlayerToEntity(this, 16.0);
+                targetPlayer =
+                    entityplayer != null &&
+                    entityplayer.getGamemode().areMobsHostile() &&
+                    canEntityBeSeen(entityplayer) &&
+                    (entityplayer.inventory.armorItemInSlot(5) == null || entityplayer.inventory.armorItemInSlot(5).itemID != AetherItems.armorCapeSwet.id);
+
+                if (targetPlayer) this.entityToAttack = entityplayer;
+            }
         }
 
-        if (targetPlayer) {
-            entityToAttack = entityplayer;
-            faceEntity(entityToAttack, 10.0f, 20.0f);
-            activated = true;
+        if (this.pathToEntity == null) this.pathToEntity = goSomewhere();
+
+        if (cooldownInactive > 0) cooldownInactive--;
+        if (this.pathToEntity != null || (targetPlayer && this.entityToAttack != null)) {
+            double zNext = 0;
+            double xNext = 0;
+
+            if (targetPlayer) {
+                xNext = this.entityToAttack.x;
+                zNext = this.entityToAttack.z;
+            }
+
+            else {
+                if (!this.pathToEntity.isDone()) {
+                    Vec3d pos = this.pathToEntity.getPos(this);
+                    xNext = pos.xCoord;
+                    zNext = pos.yCoord;
+                    this.pathToEntity.next();
+                }
+                else {
+                    this.pathToEntity = goSomewhere();
+                }
+            }
+
+            if (zNext != 0 && xNext != 0) {
+                this.yRot = (float) (180 - Math.atan2(this.x - xNext, this.z - zNext) * 180 / Math.PI);
+            }
+
             cooldownInactive = 100;
-        } else {
-            entityToAttack = null;
-            activated = false;
         }
 
         if (this.onGround && this.jumpDelay-- <= 0 && cooldownInactive > 0) {
             this.jumpDelay = this.random.nextInt(20) + 10;
-            if (targetPlayer) {
+            if (this.passenger != null) {
                 this.jumpDelay /= 3;
             } else {
                 float rotation = (this.world.rand.nextFloat() - 0.5f) * 90.0f;
@@ -70,6 +208,8 @@ public class EntitySwet extends EntityAetherAnimal {
             this.world.playSoundAtEntity(null, this, "mob.slime", this.getSoundVolume(), ((this.random.nextFloat() - this.random.nextFloat()) * 0.2f + 1.0f) * 0.8f);
             this.moveStrafing = 1.0f - this.random.nextFloat() * 2.0f;
             this.moveForward = 2;
+            //if (this.pathToEntity != null) this.moveForward *= ((float) this.pathToEntity.length / 100);
+
         } else {
             this.isJumping = false;
             if (this.onGround) {
@@ -77,6 +217,58 @@ public class EntitySwet extends EntityAetherAnimal {
                 this.moveStrafing = 0.0f;
             }
         }
+    }
+
+    @Override
+    protected Entity findPlayerToAttack() {
+        return super.findPlayerToAttack();
+    }
+
+    @Override
+    protected void causeFallDamage(float f) {
+        int i = (int)Math.ceil((double)(f - 3.0F));
+        if (i > 0) {
+            this.hurt(null, i, DamageType.FALL);
+            if (this.passenger != null) {
+                this.passenger.hurt(this, i, DamageType.FALL);
+                this.passenger.hurt(this, (int)(i*0.25F), DamageType.COMBAT);
+            }
+
+            int j = this.world.getBlockId((int)(this.x), (int)(this.y - 0.2 - this.heightOffset), (int)(this.z));
+            if (j > 0) this.world.playBlockSoundEffect(this, this.x, this.y - (double)this.heightOffset, this.z, Block.blocksList[j], EnumBlockSoundEffectType.ENTITY_LAND);
+        }
+    }
+
+    @Override
+    protected void checkFallDamage(double d, boolean flag) {
+        if (!this.isfriendly) super.checkFallDamage(d, flag);
+        if (this.jumpsCounter >= 3 && this.getHealth() > 0) this.dissolve();
+    }
+
+    @Override
+    public void addAdditionalSaveData(CompoundTag tag) {
+        tag.putBoolean("isGold", this.isGold);
+        super.addAdditionalSaveData(tag);
+    }
+
+    @Override
+    public void readAdditionalSaveData(CompoundTag tag) {
+        this.isGold = tag.getBoolean("isGold");
+        super.readAdditionalSaveData(tag);
+    }
+
+    public void splorch() {
+        this.world.playSoundAtEntity(null, this, "mob.slimeattack", 1.0F, (this.rand.nextFloat() - this.rand.nextFloat()) * 0.2F + 1.0F);
+    }
+
+    public void splotch() {
+        this.world.playSoundAtEntity(null, this, "mob.slime", 0.5F, (this.rand.nextFloat() - this.rand.nextFloat()) * 0.2F + 1.0F);
+    }
+
+    @Override
+    public int getMaxHealth() {
+        if (this.isGold) return 25;
+        else return 20;
     }
 
     public String getDefaultEntityTexture() {
--- src/main/java/bta/aether/item/ItemDevStick.java
+++ src/main/java/bta/aether/item/ItemDevStick.java
@@ -1,13 +1,20 @@
 package bta.aether.item;
 
+import bta.aether.entity.EntityBossSlider;
 import bta.aether.world.generate.feature.WorldFeatureAetherDungeonBronze;
+import net.minecraft.core.block.Block;
 import net.minecraft.core.entity.Entity;
 import net.minecraft.core.entity.EntityLiving;
 import net.minecraft.core.entity.player.EntityPlayer;
 import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.util.helper.Side;
+import net.minecraft.core.util.phys.AABB;
 import net.minecraft.core.world.World;
+import org.lwjgl.Sys;
+
+import java.util.List;
+import java.util.concurrent.Callable;
 
 public class ItemDevStick extends Item {
     public ItemDevStick(String name, int id) {
@@ -23,7 +30,28 @@ public class ItemDevStick extends Item {
 
     @Override
     public boolean onItemUse(ItemStack itemstack, EntityPlayer entityplayer, World world, int blockX, int blockY, int blockZ, Side side, double xPlaced, double yPlaced) {
-        new WorldFeatureAetherDungeonBronze().generate(world, world.rand, blockX, blockY, blockZ);
+        int distance = 5;
+
+        AABB bb = entityplayer.bb.expand(32, 32, 32);
+
+        EntityLiving entityLiving = null;
+        List<Entity> entities = world.getEntitiesWithinAABB(EntityBossSlider.class, bb);
+
+        for (Entity entity : entities) {
+            double d3 = entityplayer.x - entity.x; double d4 = entityplayer.y - entity.y; double d5 = entityplayer.z - entity.z;
+            if ((d3 * d3 + d4 * d4 + d5 * d5) >= (distance * distance)) {
+                entityLiving = (EntityLiving) entity;
+                System.out.println(entity);
+                break;
+            }
+        }
+
+        if (entityLiving == null) return false;
+
+        double value = 180 - Math.atan2(entityplayer.x - entityLiving.x, entityplayer.z - entityLiving.z) * 180 / Math.PI;
+        entityplayer.yRot = (float) value;
+        entityplayer.addChatMessage(String.valueOf(value));
+
         return true;
     }
 }
