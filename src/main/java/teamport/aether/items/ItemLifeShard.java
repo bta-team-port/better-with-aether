aa4d1c599c09257ddccb4b81345aa2e25d91ea19
Redart15 <redart15@users.noreply.github.com>
2025-07-07 23:48:36 +0200

    life shard works I think, in client for sure

--- src/main/java/teamport/aether/items/ItemLifeShard.java
+++ src/main/java/teamport/aether/items/ItemLifeShard.java
@@ -12,8 +12,21 @@ public class ItemLifeShard extends Item {
     }
 
     @Override
-    public ItemStack onUseItem(ItemStack stack, World world, Player player) {
-        return stack;
+    public ItemStack onUseItem(ItemStack itemstack, World world, Player player) {
+        int playerHealth = player.getHealth();
+        if (playerHealth < 20 && itemstack.consumeItem(player)) {
+            // min to make damn sure we don't increase pitch and volume more than expected, because that's a recipe for earsplitting sound
+            int extra_heart_amount = Math.min(playerHealth, 20);
+
+            if (extra_heart_amount == 18)
+                world.playSoundAtEntity(player,player, "aether.life_shard_chime_final",0.65F,1.0F);
+            else
+                world.playSoundAtEntity(player,player,"aether.life_shard_chime", 0.45F + 0.02F * extra_heart_amount,0.9F + 0.015F * extra_heart_amount);
+
+//            HealthHelper.addExtraHealth(player, 2);
+            player.heal(2);
+        }
+        return itemstack;
     }
 
 }
