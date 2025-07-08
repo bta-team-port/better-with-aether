19cdb1b997db9f0bce54b422a56e41a5e778087c
LukeisStuff <lukeisawesome03@gmail.com>
2025-07-08 03:08:47 -0400

    Added Sentry

--- src/main/java/teamport/aether/entity/sentry/MobRendererSentry.java
+++ src/main/java/teamport/aether/entity/sentry/MobRendererSentry.java
@@ -9,27 +9,29 @@ public class MobRendererSentry extends MobRenderer<MobSentry> {
 
     public MobRendererSentry(ModelBase model, float shadowSize) {
         super(model, shadowSize);
-    }
-
-    public void scaleSentry(MobSentry entitysentry, float f) {
         GL11.glScalef(1.75F, 1.75F, 1.75F);
+        this.setArmorModel(model);
+        this.overlayTexture = "/assets/aether/textures/entity/sentry/sentry_eye.png/";
     }
 
     public boolean setEyeBrightness(MobSentry sentry, int renderPass) {
         if (renderPass == 0 && sentry.activated) {
-            this.bindTexture("/assets/aether/textures/entity/sentry/sentry_eye.png/" + sentry.getTextureReference() + ".png");
-            float brightness = sentry.getBrightness(1.0F);
+            this.bindTexture("/assets/aether/textures/entity/sentry/sentry_eye.png/");
+            this.overlayTexture = "/assets/aether/textures/entity/sentry/sentry_eye.png/";
+            float brightness = sentry.getBrightness(2.0F);
             if (LightmapHelper.isLightmapEnabled()) {
                 LightmapHelper.setLightmapCoord(LightmapHelper.getLightmapCoord(15, 15));
             }
-            float f1 = (1.0f - brightness) * 0.5f;
+
+            float f1 = (1.0F - brightness) * 0.5F;
             GL11.glEnable(3042);
             GL11.glDisable(3008);
             GL11.glBlendFunc(770, 771);
-            GL11.glColor4f(1.0f, 1.0f, 1.0f, f1);
+            GL11.glColor4f(1.0F, 1.0F, 1.0F, f1);
             return true;
+        } else {
+            return false;
         }
-        return false;
     }
 
     @Override
