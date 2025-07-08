14eca6a9b3956caa2a817fa8ecc6792aa3128487
LukeisStuff <lukeisawesome03@gmail.com>
2025-07-08 02:09:57 -0400

    sentry test stuff

---
+++ src/main/java/teamport/aether/entity/sentry/MobRendererSentry.java
@@ -0,0 +1,39 @@
+package teamport.aether.entity.sentry;
+
+import net.minecraft.client.render.LightmapHelper;
+import net.minecraft.client.render.entity.MobRenderer;
+import net.minecraft.client.render.model.ModelBase;
+import org.lwjgl.opengl.GL11;
+
+public class MobRendererSentry extends MobRenderer<MobSentry> {
+
+    public MobRendererSentry(ModelBase model, float shadowSize) {
+        super(model, shadowSize);
+    }
+
+    public void scaleSentry(MobSentry entitysentry, float f) {
+        GL11.glScalef(1.75F, 1.75F, 1.75F);
+    }
+
+    public boolean setEyeBrightness(MobSentry sentry, int renderPass) {
+        if (renderPass == 0 && sentry.activated) {
+            this.bindTexture("/assets/aether/textures/entity/sentry/sentry_eye.png/" + sentry.getTextureReference() + ".png");
+            float brightness = sentry.getBrightness(1.0F);
+            if (LightmapHelper.isLightmapEnabled()) {
+                LightmapHelper.setLightmapCoord(LightmapHelper.getLightmapCoord(15, 15));
+            }
+            float f1 = (1.0f - brightness) * 0.5f;
+            GL11.glEnable(3042);
+            GL11.glDisable(3008);
+            GL11.glBlendFunc(770, 771);
+            GL11.glColor4f(1.0f, 1.0f, 1.0f, f1);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean prepareArmor(MobSentry sentry, int renderPass, float partialTick) {
+        return this.setEyeBrightness(sentry, renderPass);
+    }
+}
