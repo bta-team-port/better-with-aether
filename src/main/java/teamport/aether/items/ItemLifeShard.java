c4339b61d8d271344ce4a8139f7baa3f2655173f
Artem <Artem.Betz@gmx.de>
2025-07-09 23:25:48 +0200

    so the logic of lifeshard is fixed

--- src/main/java/teamport/aether/items/ItemLifeShard.java
+++ src/main/java/teamport/aether/items/ItemLifeShard.java
@@ -11,27 +11,37 @@ public class ItemLifeShard extends Item {
     public ItemLifeShard(String translationKey, String namespaceId, int id) {
         super(translationKey, namespaceId, id);
     }
+//
 
     @Override
     public ItemStack onUseItem(ItemStack itemstack, World world, Player player) {
         int extraHealth = HealthHelper.getExtraHealth(player);
-        if (!itemstack.consumeItem(player)) {
+
+        // to save on calculation further down
+        boolean canHeal = player.getHealth() < player.getMaxHealth();
+        boolean canGainExtraHealth = extraHealth < 20;
+
+        // only processed if player can be healed or gain extra health
+        if (!canHeal && !canGainExtraHealth) {
             return itemstack;
         }
-        if (player.getMaxHealth() < 40 && extraHealth < 20) {
-            // gives Player extra health
-            HealthHelper.addExtraHealth(player, 2);
+        // consume the item if possible
+        if (!itemstack.consumeItem(player)) return itemstack;
+        if (!canGainExtraHealth) {
+            player.heal(40);
+            world.playSoundAtEntity(player, player, "aether:life.shard.chime", 0.45F, 0.6F);
+            return itemstack;
         }
+        // gives Player extra health
+        HealthHelper.addExtraHealth(player, 2);
         // min to make damn sure we don't increase pitch and volume more than expected, because that's a recipe for earsplitting sound
         int extra_heart_amount = Math.min(extraHealth, 20);
-        if (extra_heart_amount == 20) {
-            world.playSoundAtEntity(player, player, "aether:life.shard.chime", 0.45F, 0.6F);
-        } else if(extra_heart_amount == 18) {
+        if (extra_heart_amount == 18) {
             world.playSoundAtEntity(player, player, "aether:life.shard.chime.final", 0.65F, 1.0F);
-        } else{
+        } else {
             world.playSoundAtEntity(player, player, "aether:life.shard.chime", 0.45F + 0.02F * extra_heart_amount, 0.9F + 0.015F * extra_heart_amount);
         }
-        player.heal(40);
+        player.heal(2);
         return itemstack;
     }
 }
