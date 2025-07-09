f91b12cef3c02624ff51e204a64f0464ea16fd1d
Artem <Artem.Betz@gmx.de>
2025-07-10 00:43:18 +0200

    temp commands to check logic, renamed the function for consistency

--- src/main/java/teamport/aether/command/AetherCommand.java
+++ src/main/java/teamport/aether/command/AetherCommand.java
@@ -4,13 +4,13 @@ import net.minecraft.core.net.command.CommandManager;
 
 public class AetherCommand {
 
-    public static void initializeCommandsClient(){
+    public static void initializeCommandClient(){
         CommandManager.registerCommand(new CommandGetHealth());
         CommandManager.registerCommand(new CommandSetHealth());
     }
 
     // I am unsure if this is correct way of doing this, cause I could not start server to check
-    public static void initializeCommandsServer(){
+    public static void initializeCommandServer(){
         CommandManager.registerCommand(new CommandGetHealth());
         CommandManager.registerCommand(new CommandSetHealth());
     }
--- src/main/java/teamport/aether/command/CommandGetHealth.java
+++ src/main/java/teamport/aether/command/CommandGetHealth.java
@@ -2,13 +2,12 @@ package teamport.aether.command;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.ArgumentBuilderLiteral;
-import net.minecraft.core.entity.Entity;
+import net.minecraft.core.entity.player.Player;
 import net.minecraft.core.net.command.CommandManager;
 import net.minecraft.core.net.command.CommandSource;
-import net.minecraft.core.net.command.helpers.EntitySelector;
 import net.minecraft.core.net.command.util.CommandHelper;
+import teamport.aether.accessory.api.HealthHelper;
 
-import java.util.List;
 
 public class CommandGetHealth implements CommandManager.CommandRegistry {
     @Override
@@ -17,12 +16,8 @@ public class CommandGetHealth implements CommandManager.CommandRegistry {
                 (ArgumentBuilderLiteral)ArgumentBuilderLiteral.literal("aether:getHealth")
                         .executes(c -> {
                             CommandSource source = ((CommandSource) c.getSource());
-                            EntitySelector entitySelector = (EntitySelector) c.getArgument("target", EntitySelector.class);
-                            List<? extends Entity> entities = entitySelector.get(source);
-
-                            for(Entity player : entities) {
-                                source.sendTranslatableMessage("command.aether.get_max_health", CommandHelper.getEntityName(player));
-                            }
+                            Player player = source.getSender();
+                            source.sendTranslatableMessage("command.aether.get_max_health", CommandHelper.getEntityName(player), HealthHelper.getExtraHealth(player));
                             return 1;
                         }
         ));
