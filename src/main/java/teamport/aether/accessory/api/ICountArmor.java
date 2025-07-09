a9b02813b06696901c8f84b3dec9aaacd3fd03c8
Artem <Artem.Betz@gmx.de>
2025-07-09 14:40:09 +0200

    moved the countArmorPiecesOfMaterial to ContainerInventory and added a helper and interface to use it outside mixin.

--- src/main/java/teamport/aether/accessory/api/ICountArmor.java
+++ src/main/java/teamport/aether/accessory/api/ICountArmor.java
@@ -2,6 +2,6 @@ package teamport.aether.accessory.api;
 import net.minecraft.core.item.material.ArmorMaterial;
 
 
-public interface IArmour {
+public interface ICountArmor {
     int aether$countArmorPiecesOfMaterial(ArmorMaterial material);
 }
