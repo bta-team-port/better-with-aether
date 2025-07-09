efa17827fd34a55d3055618ade2ce5d297af641b
Artem <Artem.Betz@gmx.de>
2025-07-09 21:49:42 +0200

    renamed to keep it consistent

--- src/main/java/teamport/aether/accessory/api/ContainerHelper.java
+++ src/main/java/teamport/aether/accessory/api/ContainerHelper.java
@@ -5,7 +5,7 @@ import net.minecraft.core.player.inventory.container.ContainerInventory;
 
 public class ContainerHelper{
 
-    public static int aether$countArmorPiecesOfMaterial(ContainerInventory inventory, ArmorMaterial material) {
+    public static int countArmorPiecesOfMaterial(ContainerInventory inventory, ArmorMaterial material) {
         return ((ICountArmor)inventory).aether$countArmorPiecesOfMaterial(material);
     }
 }
