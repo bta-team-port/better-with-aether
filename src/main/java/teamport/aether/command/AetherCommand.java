2119b56d236b168af294eb7f4fec7062160e6e63
Artem <Artem.Betz@gmx.de>
2025-07-10 15:20:55 +0200

    group and added an add extra health command

--- src/main/java/teamport/aether/command/AetherCommand.java
+++ src/main/java/teamport/aether/command/AetherCommand.java
@@ -5,13 +5,11 @@ import net.minecraft.core.net.command.CommandManager;
 public class AetherCommand {
 
     public static void initializeCommandClient(){
-        CommandManager.registerCommand(new CommandGetHealth());
-        CommandManager.registerCommand(new CommandSetHealth());
+        CommandManager.registerCommand(new CommandExtraHealth());
     }
 
     // I am unsure if this is correct way of doing this, cause I could not start server to check
     public static void initializeCommandServer(){
-        CommandManager.registerCommand(new CommandGetHealth());
-        CommandManager.registerCommand(new CommandSetHealth());
+        CommandManager.registerCommand(new CommandExtraHealth());
     }
 }
