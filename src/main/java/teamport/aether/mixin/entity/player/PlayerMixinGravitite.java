31867e4d67acde466694185f8013e0d6388b1d4b
Artem <Artem.Betz@gmx.de>
2025-07-10 20:29:27 +0200

    rename and move some mixin

--- src/main/java/teamport/aether/mixin/entity/player/PlayerMixinGravitite.java
+++ src/main/java/teamport/aether/mixin/entity/player/PlayerMixinGravitite.java
@@ -1,20 +1,31 @@
 package teamport.aether.mixin.entity.player;
 
+import net.minecraft.core.entity.Mob;
 import net.minecraft.core.entity.player.Player;
 import net.minecraft.core.player.inventory.container.ContainerInventory;
+import net.minecraft.core.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import teamport.aether.accessory.api.ContainerHelper;
 import teamport.aether.items.AetherArmorMaterial;
+import teamport.aether.mixin.accessors.EntityAccessor;
+
+import java.util.Random;
 
 @Mixin(value = Player.class, remap = false)
-public class PlayerMixinGravititeImmunity {
+public abstract class PlayerMixinGravitite extends Mob {
 
     @Shadow
     public ContainerInventory inventory;
+
+    public PlayerMixinGravitite(@Nullable World world) {
+        super(world);
+    }
 
     @Inject(method = "causeFallDamage", at= @At(value = "INVOKE", target = "Lnet/minecraft/core/entity/Mob;causeFallDamage(F)V"), cancellable = true)
     public void aether$causeFallDamage(float distance, CallbackInfo ci) {
@@ -28,5 +39,4 @@ public class PlayerMixinGravititeImmunity {
     private void aether$damageArmourGravitite(int damage) {
         ((Player) (Object) this).inventory.damageArmor((int) Math.ceil((double) damage / (double) 4.0F));
     }
-
 }
