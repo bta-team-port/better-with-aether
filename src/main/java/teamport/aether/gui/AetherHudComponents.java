12df1570d131a4031504a27aaa600967e986f4f6
Artem <Artem.Betz@gmx.de>
2025-07-10 19:44:19 +0200

    hud for lifeshard works

---
+++ src/main/java/teamport/aether/gui/AetherHudComponents.java
@@ -0,0 +1,21 @@
+package teamport.aether.gui;
+
+
+import net.minecraft.client.gui.hud.component.ComponentAnchor;
+import net.minecraft.client.gui.hud.component.HudComponent;
+import net.minecraft.client.gui.hud.component.HudComponents;
+import net.minecraft.client.gui.hud.component.layout.LayoutSnap;
+import static net.minecraft.client.gui.hud.component.HudComponents.HEALTH_BAR;
+
+/**
+ * This file is intended to initialize all gui/hud components, to keep AetherClient manageable
+ * */
+public class AetherHudComponents {
+    public static HudComponent EXTRA_HEALTH_BAR;
+
+
+    public static void registerHudComponents() {
+        EXTRA_HEALTH_BAR = HudComponents.register(new ComponentExtraHealthBar("aetherExtraHealth", new LayoutSnap(HEALTH_BAR, ComponentAnchor.TOP_LEFT, ComponentAnchor.BOTTOM_LEFT)));
+    }
+
+}
