ae99fc183874a6356823b3958f0d99909fda4b9f
Redart15 <redart15@users.noreply.github.com>
2025-07-11 14:47:05 +0200

    made the player take additional fall damage based on each equipped armor piece's protection percentage

---
+++ src/main/java/teamport/aether/mixin/entity/player/ObsidianExtraDamage.java
@@ -0,0 +1,31 @@
+package teamport.aether.mixin.entity.player;
+
+import net.minecraft.core.entity.Mob;
+import net.minecraft.core.entity.player.Player;
+import net.minecraft.core.player.inventory.container.ContainerInventory;
+import net.minecraft.core.world.World;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import teamport.aether.api.ContainerHelper;
+import teamport.aether.items.AetherArmorMaterial;
+
+@Mixin(value = Player.class, remap = false)
+public class ObsidianExtraDamage extends Mob {
+
+    @Shadow
+    public ContainerInventory inventory;
+
+    public ObsidianExtraDamage(@Nullable World world) {
+        super(world);
+    }
+
+    @Inject(method = "causeFallDamage", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/entity/Mob;causeFallDamage(F)V"), cancellable = true)
+    public void aether$causeFallDamage(float distance, CallbackInfo ci) {
+        float totalProtectionOfMaterial = ContainerHelper.getTotalEquippedArmorProtection(inventory, AetherArmorMaterial.OBSIDIAN);
+        super.causeFallDamage((1 + totalProtectionOfMaterial * 0.75F) * distance);
+    }
+}
