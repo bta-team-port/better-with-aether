21c52b56bfa797f089d604e06a41f8a7ca7bb657
Redart15 <redart15@users.noreply.github.com>
2025-07-11 11:11:25 +0200

    added TODO

--- src/main/java/teamport/aether/command/AetherCommand.java
+++ src/main/java/teamport/aether/command/AetherCommand.java
@@ -2,6 +2,8 @@ package teamport.aether.command;
 
 import net.minecraft.core.net.command.CommandManager;
 
+// TODO figure out how server-side commands are registered
+// TODO do the same optimization as in AetherItems.init()
 public class AetherCommand {
 
     public static void registerClientCommands(){
