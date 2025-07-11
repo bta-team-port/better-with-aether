b8b0e9c42ebed74a58693665fc813f70650f0c96
Redart15 <redart15@users.noreply.github.com>
2025-07-12 01:28:18 +0200

    added the effect of gravite as well as the penalty for obsidian armor

--- src/main/java/teamport/aether/mixin/armor/wolf/MobWolfMixinArmor.java
+++ src/main/java/teamport/aether/mixin/armor/wolf/MobWolfMixinArmor.java
@@ -4,12 +4,8 @@ package teamport.aether.mixin.armor.wolf;
 import net.minecraft.core.entity.animal.MobAnimal;
 import net.minecraft.core.entity.animal.MobWolf;
 import net.minecraft.core.item.IArmorItem;
-import net.minecraft.core.item.ItemStack;
-import net.minecraft.core.item.material.ArmorMaterial;
 import net.minecraft.core.world.World;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import teamport.aether.items.AetherArmorMaterial;
 import teamport.aether.items.AetherItems;
 
@@ -18,12 +14,6 @@ import static net.minecraft.core.entity.animal.MobWolf.ARMOR_MATERIALS;
 // TODO missing textures for wolfs with armour
 @Mixin(value = MobWolf.class)
 public abstract class MobWolfMixinArmor extends MobAnimal {
-
-    @Shadow
-    private ItemStack armor;
-
-    @Shadow
-    public abstract @Nullable ArmorMaterial getArmorMaterial();
 
     public MobWolfMixinArmor(World world) {
         super(world);
