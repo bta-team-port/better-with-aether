e87e3de758df3c5d5e179ed7fbdc9921bf41918e
LukeisStuff <lukeisawesome03@gmail.com>
2025-07-13 03:32:28 -0400

    Mimic Chest now can be broken and spawn a mimic, unless you use silk touch, and now activators can spawn them too, so you can make traps for your friends

--- src/main/java/teamport/aether/blocks/BlockLogicChestMimic.java
+++ src/main/java/teamport/aether/blocks/BlockLogicChestMimic.java
@@ -2,16 +2,48 @@ package teamport.aether.blocks;
 
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.BlockLogicRotatable;
+import net.minecraft.core.block.entity.TileEntity;
+import net.minecraft.core.block.entity.TileEntityActivator;
 import net.minecraft.core.block.material.Material;
 import net.minecraft.core.entity.monster.MobHuman;
 import net.minecraft.core.entity.player.Player;
+import net.minecraft.core.enums.EnumDropCause;
+import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.sound.SoundCategory;
+import net.minecraft.core.util.helper.Direction;
 import net.minecraft.core.util.helper.Side;
 import net.minecraft.core.world.World;
+import org.jetbrains.annotations.Nullable;
 
 public class BlockLogicChestMimic extends BlockLogicRotatable {
     public BlockLogicChestMimic(Block<?> block) {
         super(block, Material.wood);
+    }
+
+    public ItemStack @Nullable [] getBreakResult(World world, EnumDropCause dropCause, int x, int y, int z, int meta, TileEntity tileEntity) {
+
+        switch (dropCause) {
+            case SILK_TOUCH:
+            case PICK_BLOCK:
+                return new ItemStack[]{new ItemStack(this)};
+            default:
+                MobHuman mimic = new MobHuman(world);
+                world.playSoundEffect(mimic, SoundCategory.ENTITY_SOUNDS,x + 0.5, y, z + 0.5, "random.door_open", 1.0f, 0.5f);
+                world.setBlockWithNotify(x, y, z, 0);
+                mimic.spawnInit();
+                mimic.absMoveTo(x + 0.5, y, z + 0.5, mimic.yRot, mimic.xRot);
+                world.entityJoinedWorld(mimic);
+        }
+        return null;
+    }
+
+    public void onActivatorInteract(World world, int x, int y, int z, TileEntityActivator activator, Direction direction) {
+        world.playSoundEffect(null, SoundCategory.ENTITY_SOUNDS,x + 0.5, y, z + 0.5, "random.door_open", 1.0f, 0.5f);
+        world.setBlockWithNotify(x, y, z, 0);
+        MobHuman mimic = new MobHuman(world);
+        mimic.spawnInit();
+        mimic.absMoveTo(x + 0.5, y, z + 0.5, (mimic.yRot) - 180.0f, -(mimic.xRot));
+        world.entityJoinedWorld(mimic);
     }
 
     @Override
