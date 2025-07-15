57fefbf11de707446b48bb1f96181829907f2594
Redart15 <redart15@users.noreply.github.com>
2025-07-16 01:25:14 +0200

    extends player to allow opening of the new enchanter

---
+++ src/main/java/teamport/aether/mixin/screens/PlayerLocalMixinAetherScreens.java
@@ -0,0 +1,24 @@
+package teamport.aether.mixin.screens;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.player.PlayerLocal;
+import net.minecraft.core.entity.player.Player;
+import net.minecraft.core.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import teamport.aether.gui.IAetherScreens;
+import teamport.aether.gui.ScreenEnchanter;
+import teamport.aether.tile.TileEntityEnchanter;
+
+@Mixin(value = PlayerLocal.class, remap = false)
+public abstract class PlayerLocalMixinAetherScreens extends Player implements IAetherScreens {
+
+    @Shadow protected Minecraft mc;
+
+    public PlayerLocalMixinAetherScreens(World world) {super(world);}
+
+    @Override
+    public void aether$displayEnchanterScreen(TileEntityEnchanter tileEntity) {
+        this.mc.displayScreen(new ScreenEnchanter(this.inventory, tileEntity));
+    }
+}
