8cc06244beb43f64a981dabaeef26ee9bfba0a09
Redart15 <redart15@users.noreply.github.com>
2025-07-17 23:09:38 +0200

    allow shift clicking item into aether machines

---
+++ src/main/java/teamport/aether/mixin/accessors/ScreenContainerAbstractAccessor.java
@@ -0,0 +1,12 @@
+package teamport.aether.mixin.accessors;
+
+import net.minecraft.client.gui.container.ScreenContainerAbstract;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(value = ScreenContainerAbstract.class, remap = false)
+public interface ScreenContainerAbstractAccessor {
+
+    @Invoker("getSlotId")
+    int invokeGetSlotId(int x, int y);
+}
