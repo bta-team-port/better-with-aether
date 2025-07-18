5fb6c8aa8aea74f7e24dc4486bb0d08ba3ff1d26
Redart15 <redart15@users.noreply.github.com>
2025-07-18 17:04:44 +0200

    extended the interface and implemented the functions in player

--- src/main/java/teamport/aether/mixin/screens/PlayerMixinAetherScreens.java
+++ src/main/java/teamport/aether/mixin/screens/PlayerMixinAetherScreens.java
@@ -4,9 +4,10 @@ import net.minecraft.core.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import teamport.aether.gui.IAetherScreens;
 import teamport.aether.tile.TileEntityEnchanter;
+import teamport.aether.tile.TileEntityFreezer;
 
 @Mixin(value = Player.class, remap = false)
 public class PlayerMixinAetherScreens implements IAetherScreens {
-
-    public void aether$displayEnchanterScreen(TileEntityEnchanter tile) {}
+    public void aether$displayEnchanterScreen(TileEntityEnchanter tileEntity) {}
+    public void aether$displayFreezerScreen(TileEntityFreezer tile) {}
 }
