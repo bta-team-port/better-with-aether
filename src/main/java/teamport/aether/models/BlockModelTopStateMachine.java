d17fa6234f5edaf7097d7fecf4f1a55be6089423
Redart15 <redart15@users.noreply.github.com>
2025-07-19 18:53:27 +0200

    simplified the logic

--- src/main/java/teamport/aether/models/BlockModelTopStateMachine.java
+++ src/main/java/teamport/aether/models/BlockModelTopStateMachine.java
@@ -11,6 +11,7 @@ import net.minecraft.core.player.inventory.container.Container;
 import net.minecraft.core.util.helper.Side;
 import net.minecraft.core.util.helper.Sides;
 import net.minecraft.core.world.WorldSource;
+import org.lwjgl.opengl.GL11;
 
 @Environment(EnvType.CLIENT)
 public class BlockModelTopStateMachine<T extends BlockLogic> extends BlockModelStandard<T> {
@@ -19,21 +20,18 @@ public class BlockModelTopStateMachine<T extends BlockLogic> extends BlockModelS
     }
 
     public IconCoordinate getBlockTexture(WorldSource blockAccess, int x, int y, int z, Side side) {
-        int sideId = side.getId();
-        if (sideId >= Sides.orientationLookUpHorizontal.length) {
-            return this.blockTextures.get(Side.BOTTOM);
-        } else if (sideId == Side.TOP.getId()) {
-            IconCoordinate originalFront = this.blockTextures.get(Side.TOP);
+        if (side.getId() == Side.TOP.getId()) {
+            IconCoordinate texture = this.blockTextures.get(Side.TOP);
             Container container = (Container) blockAccess.getTileEntity(x, y, z);
             if (container != null) {
                 boolean hasOutput = container.getItem(2) != null;
                 if (hasOutput) {
-                    return TextureRegistry.getTexture(originalFront.namespaceId.namespace() + ":block/" + originalFront.namespaceId.value() + "_filled");
+                    return TextureRegistry.getTexture(texture.namespaceId.namespace() + ":block/" + texture.namespaceId.value() + "_filled");
                 }
             }
-            return originalFront;
+            return texture;
         }
-        return this.blockTextures.get(side);
+        return this.blockTextures.get(side.getId());
     }
 
 }
