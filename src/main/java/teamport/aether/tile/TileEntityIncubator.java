6ccb474ab212cb4e60b947a2ff942be7f790b983
Redart15 <redart15@users.noreply.github.com>
2025-07-20 00:10:48 +0200

    started work on incubator

--- src/main/java/teamport/aether/tile/TileEntityIncubator.java
+++ src/main/java/teamport/aether/tile/TileEntityIncubator.java
@@ -1,9 +1,12 @@
 package teamport.aether.tile;
 
 
+import net.minecraft.core.entity.EntityItem;
 import net.minecraft.core.entity.player.Player;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.world.World;
+import teamport.aether.blocks.AetherBlocks;
+import teamport.aether.blocks.BlockLogicIncubator;
 
 // TODO implement the class, this should be mostly a port from 7.2
 public class TileEntityIncubator extends AetherTileEntityMachine {
@@ -21,7 +24,7 @@ public class TileEntityIncubator extends AetherTileEntityMachine {
 
     @Override
     public String getNameTranslationKey(){
-        return "aether.container.enchanter.name";
+        return "aether.container.incubator.name";
     }
 
     @Override
@@ -34,7 +37,15 @@ public class TileEntityIncubator extends AetherTileEntityMachine {
     public void processItem() {}
 
     @Override
-    public void updateContainer(boolean forceLit){}
+    public void updateContainer(boolean forceLit) {
+        if (this.worldObj != null) {
+            BlockLogicIncubator.updateFurnaceBlockState(forceLit | this.currentEnergyTime > 0, this.worldObj, this.x, this.y, this.z);
+            return;
+        }
+        if (this.carriedBlock != null) {
+            this.carriedBlock.blockId = forceLit | this.currentEnergyTime > 0 ? AetherBlocks.INCUBATOR_ACTIVE.id() : AetherBlocks.INCUBATOR_IDLE.id();
+        }
+    }
 
     @Override
     public int getEnergyTimeFromItem(ItemStack itemStack){return 0;}
@@ -49,9 +60,34 @@ public class TileEntityIncubator extends AetherTileEntityMachine {
     }
 
     @Override
-    public void dropContents(World world, int x, int y, int z) {}
+    public void dropContents(World world, int x, int y, int z) {
+        super.dropContents(world, x, y, z);
+        if (!BlockLogicIncubator.keepIncubatorInventory) {
+            for (int l = 0; l < this.getContainerSize(); ++l) {
+                ItemStack itemstack = this.getItem(l);
+                if (itemstack != null) {
+                    float f = this.random.nextFloat() * 0.8F + 0.1F;
+                    float f1 = this.random.nextFloat() * 0.8F + 0.1F;
+                    float f2 = this.random.nextFloat() * 0.8F + 0.1F;
 
-    @Override
-    public void sortContainer() {}
+                    while (itemstack.stackSize > 0) {
+                        int i1 = this.random.nextInt(21) + 10;
+                        if (i1 > itemstack.stackSize) {
+                            i1 = itemstack.stackSize;
+                        }
 
+                        itemstack.stackSize -= i1;
+                        EntityItem entityItem = new EntityItem(
+                                world, (float) x + f, (float) y + f1, (float) z + f2,
+                                new ItemStack(itemstack.itemID, i1, itemstack.getMetadata()));
+                        float f3 = 0.05F;
+                        entityItem.xd = (float) this.random.nextGaussian() * f3;
+                        entityItem.yd = (float) this.random.nextGaussian() * f3 + 0.2F;
+                        entityItem.zd = (float) this.random.nextGaussian() * f3;
+                        world.entityJoinedWorld(entityItem);
+                    }
+                }
+            }
+        }
+    }
 }
