7d14c0d6db61da12c426c6a3a9f4ffe31b590ac8
Redart15 <redart15@users.noreply.github.com>
2025-07-19 14:57:58 +0200

    added slots for future achievements

--- src/main/java/teamport/aether/gui/machine/enchanter/SlotEnchanter.java
+++ src/main/java/teamport/aether/gui/machine/enchanter/SlotEnchanter.java
@@ -1,4 +1,28 @@
 package teamport.aether.gui.machine.enchanter;
 
-public class SlotEnchanter {
+import net.minecraft.core.entity.player.Player;
+import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.player.inventory.container.Container;
+import net.minecraft.core.player.inventory.slot.Slot;
+
+public class SlotEnchanter extends Slot {
+    private Player thePlayer;
+
+    public SlotEnchanter(Player theplayer, Container container, int index, int x, int y) {
+        super(container, index, x, y);
+        this.thePlayer = theplayer;
+    }
+
+    @Override
+    public void onTake(ItemStack itemstack){
+        // check for achievement
+    }
+
+    // prevent item to be put in the output slot
+    @Override
+    public boolean mayPlace(ItemStack itemstack)    {return false;}
+    @Override
+    public boolean enableDragAndPickup()            {return false;}
+    @Override
+    public boolean allowItemInteraction()           {return false;}
 }
