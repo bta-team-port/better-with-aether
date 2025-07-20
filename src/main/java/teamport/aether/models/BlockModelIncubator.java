c458d6b368e780d9e753cd574d7c50598fbf06fc
Redart15 <redart15@users.noreply.github.com>
2025-07-20 18:00:58 +0200

    custom model for incubator

---
+++ src/main/java/teamport/aether/models/BlockModelIncubator.java
@@ -0,0 +1,35 @@
+package teamport.aether.models;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.block.model.BlockModelStandard;
+import net.minecraft.client.render.texture.stitcher.IconCoordinate;
+import net.minecraft.client.render.texture.stitcher.TextureRegistry;
+import net.minecraft.core.block.Block;
+import net.minecraft.core.block.BlockLogic;
+import net.minecraft.core.player.inventory.container.Container;
+import net.minecraft.core.util.helper.Side;
+import net.minecraft.core.world.WorldSource;
+
+@Environment(EnvType.CLIENT)
+public class BlockModelIncubator<T extends BlockLogic> extends BlockModelStandard<T> {
+    public BlockModelIncubator(Block<T> block) {
+        super(block);
+    }
+
+    public IconCoordinate getBlockTexture(WorldSource blockAccess, int x, int y, int z, Side side) {
+        if (side.getId() == Side.TOP.getId()) {
+            IconCoordinate texture = this.blockTextures.get(Side.TOP);
+            Container container = (Container) blockAccess.getTileEntity(x, y, z);
+            if (container != null) {
+                boolean hasInput = container.getItem(0) != null;
+                if (hasInput) {
+                    return TextureRegistry.getTexture(texture.namespaceId.namespace() + ":block/" + texture.namespaceId.value() + "_filled");
+                }
+            }
+            return texture;
+        }
+        return this.blockTextures.get(side.getId());
+    }
+
+}
