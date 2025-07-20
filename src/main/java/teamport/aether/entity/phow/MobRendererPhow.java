157edeebfb7263ed1eea7a0ea9a8af6cdf2de659
LukeisStuff <lukeisawesome03@gmail.com>
2025-07-20 02:35:00 -0400

    Added Phow and Phyg, gave zephyrs aercloud variants

--- src/main/java/teamport/aether/entity/phow/MobRendererPhow.java
+++ src/main/java/teamport/aether/entity/phow/MobRendererPhow.java
@@ -4,22 +4,39 @@ import net.minecraft.client.render.entity.MobRenderer;
 import net.minecraft.client.render.model.ModelBase;
 
 public class MobRendererPhow extends MobRenderer<MobPhow> {
-    public MobRendererPhow(ModelBase modelbase, ModelBase modelwings, float f) {
+    public MobRendererPhow(ModelBase modelbase, ModelBase modelBase2, float f) {
         super(modelbase, f);
-        this.setArmorModel(modelwings);
+        this.setArmorModel(modelBase2);
     }
 
-    public boolean setWingsAndRender(MobPhow phow, int i, float f) {
-        if (i == 0) {
-            this.bindTexture("/assets/aether/textures/entity/Wings.png");
-            ModelPhowWings.cow = phow;
-            return true;
-        } else {
-            return false;
-        }
+    public float limbSway(MobPhow cow, float partialTick) {
+        float wingBend = -((float)Math.acos(cow.wingFold));
+        float x = 32.0F * cow.wingFold / 4.0F;
+        float y = -32.0F * (float)Math.sqrt(1.0F - cow.wingFold * cow.wingFold) / 4.0F;
+        float z = 0.0F;
+        float x2 = x * (float)Math.cos(cow.wingAngle) - y * (float)Math.sin(cow.wingAngle);
+        float y2 = x * (float)Math.sin(cow.wingAngle) + y * (float)Math.cos(cow.wingAngle);
+        ModelPhow.leftWingInner.setRotationPoint(4.0F + x2, y2 + 6.0F, z);
+        ModelPhow.rightWingInner.setRotationPoint(-4.0F - x2, y2 + 6.0F, z);
+        x *= 3.0F;
+        x2 = x * (float)Math.cos(cow.wingAngle) - y * (float)Math.sin(cow.wingAngle);
+        y2 = x * (float)Math.sin(cow.wingAngle) + y * (float)Math.cos(cow.wingAngle);
+
+        ModelPhow.leftWingOuter.setRotationPoint(4.0F + x2, y2 + 6.0F, z);
+        ModelPhow.rightWingOuter.setRotationPoint(-4.0F - x2, y2 + 6.0F, z);
+        ModelPhow.leftWingInner.zRot = cow.wingAngle + wingBend + 1.5707964F;
+        ModelPhow.leftWingOuter.zRot = cow.wingAngle - wingBend + 1.5707964F;
+        ModelPhow.rightWingInner.zRot = -(cow.wingAngle + wingBend - 1.5707964F);
+        ModelPhow.rightWingOuter.zRot = -(cow.wingAngle - wingBend + 1.5707964F);
+        return wingBend;
+    }
+
+    public boolean renderSaddledPhow(MobPhow entity, int i, float f) {
+        this.bindTexture("/assets/minecraft/textures/entity/saddle.png");
+        return i == 0 && entity != null && entity.getSaddled();
     }
 
     public boolean prepareArmor(MobPhow entity, int renderPass, float partialTick) {
-        return this.setWingsAndRender(entity, renderPass, partialTick);
+        return this.renderSaddledPhow(entity, renderPass, partialTick);
     }
 }
---
+++ src/main/java/teamport/aether/mixin/item/ItemSaddleMixin.java
@@ -0,0 +1,41 @@
+package teamport.aether.mixin.item;
+
+import net.minecraft.core.entity.Mob;
+import net.minecraft.core.entity.player.Player;
+import net.minecraft.core.item.Item;
+import net.minecraft.core.item.ItemSaddle;
+import net.minecraft.core.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import teamport.aether.entity.phow.MobPhow;
+import teamport.aether.entity.phyg.MobPhyg;
+
+@Mixin(value = ItemSaddle.class, remap = false)
+public class ItemSaddleMixin extends Item {
+
+    public ItemSaddleMixin(String name, String namespaceId, int id) {
+        super(name, namespaceId, id);
+    }
+
+    @Inject(method = "useItemOnEntity", at = @At("HEAD"), cancellable = true)
+    public void callOnItemUse(ItemStack itemstack, Mob entityliving, Player entityPlayer, CallbackInfoReturnable<Boolean> info) {
+        if (entityliving instanceof MobPhyg && itemstack.consumeItem(entityPlayer)) {
+            MobPhyg entitypig = (MobPhyg)entityliving;
+            if (!entitypig.getSaddled()) {
+                entitypig.setSaddled(true);
+                entityPlayer.swingItem();
+                info.setReturnValue(true);
+            }
+        }
+        if (entityliving instanceof MobPhow && itemstack.consumeItem(entityPlayer)) {
+            MobPhow entitycow = (MobPhow)entityliving;
+            if (!entitycow.getSaddled()) {
+                entitycow.setSaddled(true);
+                entityPlayer.swingItem();
+                info.setReturnValue(true);
+            }
+        }
+    }
+}
