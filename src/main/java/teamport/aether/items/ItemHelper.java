f6d020136f08d0c8ae5b937c2bf830d8fa36e91e
Redart15 <redart15@users.noreply.github.com>
2025-07-21 01:58:51 +0200

    move repairables function into helper

--- src/main/java/teamport/aether/items/ItemHelper.java
+++ src/main/java/teamport/aether/items/ItemHelper.java
@@ -4,8 +4,9 @@ import net.minecraft.core.item.*;
 import net.minecraft.core.item.tool.ItemTool;
 
 public class ItemHelper {
-    public static boolean isRepairable(ItemStack toProcess) {
-        Item item = toProcess.getItem();
+    public static boolean isRepairable(ItemStack itemStack) {
+        if(itemStack == null) return false;
+        Item item = itemStack.getItem();
         return item instanceof ItemTool
                 || item instanceof ItemArmor
                 || item instanceof ItemFireStriker
