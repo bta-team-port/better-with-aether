10d5532af9cbf404fb7573d7c6c4f967fef309d0
Redart15 <redart15@users.noreply.github.com>
2025-07-20 20:23:15 +0200

    return to previous values

--- src/main/java/teamport/aether/tile/TileEntityIncubator.java
+++ src/main/java/teamport/aether/tile/TileEntityIncubator.java
@@ -155,8 +155,7 @@ public class TileEntityIncubator extends AetherTileEntityMachine {
         Class<? extends Entity> entityClazz = AetherRecipes.INCUBATOR.findOutput(containerItemStacks[0]);
         if(entityClazz == null) return;
         Entity entity =  EntityDispatcher.createEntityInWorld(entityClazz, worldObj);
-        // TODO change this back to y+2 and x
-        entity.moveTo(this.x + 1, this.y, this.z, 0.0F, 0.0F);
+        entity.moveTo(this.x, this.y + 2, this.z, 0.0F, 0.0F);
         worldObj.entityJoinedWorld(entity);
         containerItemStacks[0].stackSize--;
         if (containerItemStacks[0].stackSize <= 0) containerItemStacks[0] = null;
