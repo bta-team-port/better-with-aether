27dc6ecccded78a1923eef121e0520437e3e36e5
Redart15 <redart15@users.noreply.github.com>
2025-07-20 17:59:40 +0200

    custom recipe entry and json parser for incubator

---
+++ src/main/java/teamport/aether/recipe/RecipeEntryIncubator.java
@@ -0,0 +1,78 @@
+package teamport.aether.recipe;
+
+import net.minecraft.core.data.registry.recipe.*;
+import net.minecraft.core.data.registry.recipe.adapter.RecipeJsonAdapter;
+import net.minecraft.core.item.ItemStack;
+
+import java.util.List;
+
+public class RecipeEntryIncubator extends RecipeEntryBase<RecipeSymbol, String, Integer> implements HasJsonAdapter {
+    public RecipeEntryIncubator(RecipeSymbol input, String output, int time) {
+        super(input, output, time);
+    }
+
+    public RecipeEntryIncubator() {
+    }
+
+    public boolean matchesQueryIgnoreExceptions(SearchQuery query) {
+        try {
+            return this.matchesQuery(query);
+        } catch (IllegalArgumentException | NullPointerException var3) {
+            return false;
+        }
+    }
+
+    public boolean matches(ItemStack stack) {
+        return ((RecipeSymbol)this.getInput()).matches(stack);
+    }
+
+    public boolean matchesQuery(SearchQuery query) {
+        switch (query.mode){
+            case ALL: {
+                if ((matchesRecipe(query) || matchesUsage(query))) return true;
+                break;
+            }
+            case RECIPE: {
+                if (matchesRecipe(query)) return true;
+                break;
+            }
+            case USAGE: {
+                if (matchesUsage(query)) return true;
+                break;
+            }
+        }
+        return false;
+    }
+
+
+    public boolean matchesRecipe(SearchQuery query) {
+        if(query.query.getLeft() == SearchQuery.QueryType.NAME) {
+            if(query.strict && this.getOutput().equalsIgnoreCase(query.query.getRight())){
+                return true;
+            }
+            return !query.strict && this.getOutput().toLowerCase().contains(query.query.getRight().toLowerCase());
+        }
+        return false;
+    }
+
+    public boolean matchesUsage(SearchQuery query) {
+        List<ItemStack> stacks = getInput().resolve();
+        for (ItemStack stack : stacks) {
+            if (stack == null) continue;
+            if(query.query.getLeft() == SearchQuery.QueryType.NAME) {
+                if(query.strict && stack.getDisplayName().equalsIgnoreCase(query.query.getRight())){
+                    return true;
+                }
+                if(!query.strict && stack.getDisplayName().toLowerCase().contains(query.query.getRight().toLowerCase())){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public RecipeJsonAdapter<?> getAdapter() {
+        return new RecipeIncubatorJsonAdapter();
+    }
+}
