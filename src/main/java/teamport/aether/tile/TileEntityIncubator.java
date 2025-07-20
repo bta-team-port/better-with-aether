f664a25ca1e7059436ea421de98b55ad61f11c65
Redart15 <redart15@users.noreply.github.com>
2025-07-20 18:34:43 +0200

    implemented the tile entity for incubator

--- src/main/java/teamport/aether/tile/TileEntityIncubator.java
+++ src/main/java/teamport/aether/tile/TileEntityIncubator.java
@@ -1,12 +1,18 @@
 package teamport.aether.tile;
 
 
+import net.minecraft.core.block.Blocks;
+import net.minecraft.core.entity.Entity;
+import net.minecraft.core.entity.EntityDispatcher;
 import net.minecraft.core.entity.EntityItem;
 import net.minecraft.core.entity.player.Player;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.world.World;
+import org.jetbrains.annotations.Nullable;
 import teamport.aether.blocks.AetherBlocks;
 import teamport.aether.blocks.BlockLogicIncubator;
+import teamport.aether.lookup.LookupFuelIncubator;
+import teamport.aether.lookup.LookupRecipeIncubator;
 
 // TODO implement the class, this should be mostly a port from 7.2
 public class TileEntityIncubator extends AetherTileEntityMachine {
@@ -28,13 +34,138 @@ public class TileEntityIncubator extends AetherTileEntityMachine {
     }
 
     @Override
-    public void tick(){}
+    public void tick(){
+        boolean isEnergyTimeHigherThan0 = this.currentEnergyTime > 0;
+        boolean updateMachine = false;
+        if (this.currentEnergyTime > 0) {
+            --this.currentEnergyTime;
+        }
+        if(canProcess()){
+            this.maxProcessTime = 100;
+        }
+        if (isUpdateMachine(updateMachine, isEnergyTimeHigherThan0)) {
+            this.setChanged();
+        }
+    }
+
+    private boolean isUpdateMachine(boolean updateMachine, boolean isEnergyTimeHigherThan0) {
+        if (this.worldObj == null || !this.worldObj.isClientSide) {
+            updateMachine = eternallyLit(updateMachine);
+
+            if (this.currentEnergyTime == 0 && this.containerItemStacks[1] != null && this.canProcess()) {
+                this.maxEnergyTime = this.currentEnergyTime = this.getEnergyTimeFromItem(this.containerItemStacks[1]);
+                if (this.currentEnergyTime > 0) {
+                    updateMachine = true;
+                    if (this.containerItemStacks[1] != null) {
+                        --this.containerItemStacks[1].stackSize;
+                        if (this.containerItemStacks[1].stackSize <= 0) {
+                            this.containerItemStacks[1] = null;
+                        }
+
+                    }
+                }
+            }
+
+            if (this.isProcessing() && this.canProcess()) {
+                ++this.currentProcessTime;
+                if (this.currentProcessTime == this.maxProcessTime) {
+                    this.currentProcessTime = 0;
+                    this.processItem();
+                    updateMachine = true;
+                }
+            } else {
+                this.currentProcessTime = 0;
+            }
+
+            if (isEnergyTimeHigherThan0 != this.currentEnergyTime > 0) {
+                this.updateContainer(false);
+                updateMachine = true;
+            }
+
+        }
+        return updateMachine;
+    }
+
+    public boolean eternallyLit(boolean updateMachine) {
+        if ((this.worldObj == null
+                || this.worldObj.getBlockId(this.x, this.y, this.z) == AetherBlocks.INCUBATOR_IDLE.id())
+                && this.currentEnergyTime == 0 && this.containerItemStacks[0] == null
+                && this.containerItemStacks[1] != null
+                && this.containerItemStacks[1].itemID == Blocks.WOOL.id()
+        ) {
+            --this.containerItemStacks[1].stackSize;
+            if (this.containerItemStacks[1].stackSize <= 0) {
+                this.containerItemStacks[1] = null;
+            }
+
+            this.updateContainer(true);
+            return true;
+        }
+        return updateMachine;
+    }
 
     @Override
-    public boolean canProcess() {return false;}
+    public @Nullable ItemStack removeItem(int index, int takeAmount) {
+        if (this.containerItemStacks[index] != null) {
+            if (this.containerItemStacks[index].stackSize <= takeAmount) {
+                ItemStack itemstack = this.containerItemStacks[index];
+                this.containerItemStacks[index] = null;
+                if (this.worldObj != null && index == 0) {
+                    this.worldObj.markBlockNeedsUpdate(this.x, this.y, this.z);
+                }
+
+                return itemstack;
+            } else {
+                ItemStack itemstack1 = this.containerItemStacks[index].splitStack(takeAmount);
+                if (this.containerItemStacks[index].stackSize <= 0) {
+                    this.containerItemStacks[index] = null;
+                    if (this.worldObj != null && index == 0) {
+                        this.worldObj.markBlockNeedsUpdate(this.x, this.y, this.z);
+                    }
+                }
+
+                return itemstack1;
+            }
+        } else {
+            return null;
+        }
+    }
 
     @Override
-    public void processItem() {}
+    public void setItem(int index, @Nullable ItemStack itemstack) {
+        this.containerItemStacks[index] = itemstack;
+        if (itemstack != null && itemstack.stackSize > this.getMaxStackSize()) {
+            itemstack.stackSize = this.getMaxStackSize();
+        }
+
+        if (this.worldObj != null && index == 0) {
+            this.worldObj.markBlockNeedsUpdate(this.x, this.y, this.z);
+        }
+
+    }
+
+    @Override
+    public void processItem() {
+        if(!this.canProcess()){
+            return;
+        }
+        Class<? extends Entity> entityClazz = LookupRecipeIncubator.instance.getEntity(containerItemStacks[0].getItem().id);
+        if(entityClazz == null) return;
+        Entity entity =  EntityDispatcher.createEntityInWorld(entityClazz, worldObj);
+        entity.moveTo(this.x, this.y + 2, this.z, 0.0F, 0.0F);
+        worldObj.entityJoinedWorld(entity);
+        containerItemStacks[0].stackSize--;
+        if (containerItemStacks[0].stackSize <= 0) containerItemStacks[0] = null;
+    }
+
+    @Override
+    public boolean canProcess() {
+        if (this.containerItemStacks[0] == null) {
+            return false;
+        }
+        // TODO change this to to a lookup in recipe.json
+        return LookupRecipeIncubator.instance.getEntity(containerItemStacks[0].getItem().id) != null;
+    }
 
     @Override
     public void updateContainer(boolean forceLit) {
@@ -48,7 +179,10 @@ public class TileEntityIncubator extends AetherTileEntityMachine {
     }
 
     @Override
-    public int getEnergyTimeFromItem(ItemStack itemStack){return 0;}
+    public int getEnergyTimeFromItem(ItemStack itemStack) {
+        // just in case where will be more options later
+        return itemStack == null ? 0 : LookupFuelIncubator.instance.getFuelYield(itemStack.getItem().id);
+    }
 
     @Override
     public boolean stillValid(Player entityplayer) {
