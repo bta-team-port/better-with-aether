8d4301d5eb4cb92b3a8d0c4742d0d305a3dc88a3
Redart15 <redart15@users.noreply.github.com>
2025-07-22 00:35:25 +0200

    moved armor to the back

--- src/main/java/teamport/aether/guidebook/GuidebookSectionFreezer.java
+++ src/main/java/teamport/aether/guidebook/GuidebookSectionFreezer.java
@@ -9,9 +9,11 @@ import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.util.collection.Pair;
 import net.minecraft.core.util.helper.MathHelper;
 import teamport.aether.AetherRecipes;
+import teamport.aether.lookup.Repairable;
 import teamport.aether.recipe.RecipeEntryAetherMachine;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
@@ -30,23 +32,20 @@ public class GuidebookSectionFreezer extends SearchableGuidebookSection {
         if (this.filteredPages != null && Objects.equals(this.filteredPages.getLeft(), query.rawQuery)) {
             return this.filteredPages.getRight();
         } else {
-            ArrayList<RecipeEntryAetherMachine> filteredRecipes = new ArrayList<>();
+            List<RecipeEntryAetherMachine> filteredRecipes = new ArrayList<>();
             List<RecipeEntryAetherMachine> allRecipes = new ArrayList<>(AetherRecipes.FREEZER.getAllRecipes());
             allRecipes.removeIf(Objects::isNull);
 
-            for (RecipeEntryAetherMachine recipe : allRecipes) {
-                if (recipe.matchesQueryIgnoreExceptions(query)) {
-                    filteredRecipes.add(recipe);
-                }
-            }
+            filterRecipe(query, allRecipes, filteredRecipes);
+            filteredRecipes = moveRepairablesToBack(filteredRecipes);
 
-            ArrayList<GuidebookPage> filteredPages = new ArrayList<>();
+            List<GuidebookPage> filteredPages = new ArrayList<>();
             int filteredRecipeSize = filteredRecipes.size();
             int filteredPageCount = MathHelper.ceilInt(filteredRecipeSize, 3);
 
             for (int i = 0; i < filteredPageCount; ++i) {
                 int j = i * 6;
-                ArrayList<RecipeEntryAetherMachine> recipes = new ArrayList<>(filteredRecipes.subList(Math.min(j, filteredRecipeSize), Math.min(j + 6, filteredRecipeSize)));
+                List<RecipeEntryAetherMachine> recipes = new ArrayList<>(filteredRecipes.subList(Math.min(j, filteredRecipeSize), Math.min(j + 6, filteredRecipeSize)));
                 if (!recipes.isEmpty()) {
                     filteredPages.add(new RecipePageFreezer(this, recipes));
                 }
@@ -64,12 +63,44 @@ public class GuidebookSectionFreezer extends SearchableGuidebookSection {
         int totalRecipes = allRecipes.size();
         int totalPages = MathHelper.ceilInt(totalRecipes, 6);
 
+        allRecipes = moveRepairablesToBack(allRecipes);
+
         for (int i = 0; i < totalPages; ++i) {
             int j = i * 6;
-            ArrayList<RecipeEntryAetherMachine> recipes = new ArrayList<>(allRecipes.subList(Math.min(j, totalRecipes), Math.min(j + 6, totalRecipes)));
+            List<RecipeEntryAetherMachine> recipes = new ArrayList<>(allRecipes.subList(Math.min(j, totalRecipes), Math.min(j + 6, totalRecipes)));
             this.pages.add(new RecipePageFreezer(this, recipes));
         }
 
+    }
+
+    private static void filterRecipe(SearchQuery query, List<RecipeEntryAetherMachine> allRecipes, List<RecipeEntryAetherMachine> filteredRecipes) {
+        for (RecipeEntryAetherMachine recipe : allRecipes) {
+            if (recipe.matchesQueryIgnoreExceptions(query)) {
+                filteredRecipes.add(recipe);
+            }
+        }
+    }
+
+    private static List<RecipeEntryAetherMachine> moveRepairablesToBack(List<RecipeEntryAetherMachine> recipes) {
+        List<RecipeEntryAetherMachine> new_recipes = new ArrayList<>(recipes.size());
+        List<RecipeEntryAetherMachine> repairable = new ArrayList<>();
+        for(RecipeEntryAetherMachine recipe : recipes){
+            ItemStack input = recipe.getInput().getStack();
+            ItemStack output = recipe.getOutput();
+            if(Repairable.instance.isRepairable(input) && Repairable.instance.isRepairable(output) && input != null){
+                repairable.add(recipe);
+            }else{
+                new_recipes.add(recipe);
+            }
+        }
+        repairable.sort(new Comparator<RecipeEntryAetherMachine>() {
+            @Override
+            public int compare(RecipeEntryAetherMachine self, RecipeEntryAetherMachine other) {
+                return self.getInput().getStack().getDisplayName().compareTo(other.getInput().getStack().getDisplayName());
+            }
+        });
+        new_recipes.addAll(repairable);
+        return new_recipes;
     }
 
     public List<GuidebookPage> getPages() {
