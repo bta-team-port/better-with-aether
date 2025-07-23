25d7a03c35c0adfa520ca55b3edf0caf7ddcb412
Redart15 <redart15@users.noreply.github.com>
2025-07-23 23:22:08 +0200

    added accessor for addSlot, cause I am lazy

---
+++ src/main/java/teamport/aether/mixin/accessors/MenuAbstractAccessor.java
@@ -0,0 +1,13 @@
+package teamport.aether.mixin.accessors;
+
+import net.minecraft.core.player.inventory.menu.MenuAbstract;
+import net.minecraft.core.player.inventory.slot.Slot;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(value = MenuAbstract.class, remap = false)
+public interface MenuAbstractAccessor {
+
+    @Invoker("addSlot")
+    void invokeAddSlot(Slot slot);
+}
