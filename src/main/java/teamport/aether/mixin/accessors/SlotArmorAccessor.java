257f992c53f8113f8703f059484570e0351e1d43
Redart15 <redart15@users.noreply.github.com>
2025-07-25 22:01:36 +0200

    added TODO for later

---
+++ src/main/java/teamport/aether/mixin/accessors/SlotArmorAccessor.java
@@ -0,0 +1,14 @@
+package teamport.aether.mixin.accessors;
+
+import net.minecraft.core.player.inventory.slot.Slot;
+import net.minecraft.core.player.inventory.slot.SlotArmor;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+
+@Mixin(value = SlotArmor.class, remap = false)
+public interface SlotArmorAccessor {
+
+    @Accessor("armorType")
+    int getArmorType();
+}
