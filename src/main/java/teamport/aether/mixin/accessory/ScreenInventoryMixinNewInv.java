149b0ee857d039d5ea40846630e8ca31d1bf8c7c
Redart15 <redart15@users.noreply.github.com>
2025-07-25 11:21:59 +0200

    redirected the texture load call to the new ui texture, fixed label position

--- src/main/java/teamport/aether/mixin/accessory/ScreenInventoryMixinNewInv.java
+++ src/main/java/teamport/aether/mixin/accessory/ScreenInventoryMixinNewInv.java
@@ -2,7 +2,6 @@ package teamport.aether.mixin.accessory;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.container.ScreenInventory;
-import net.minecraft.client.gui.container.ScreenInventoryCreative;
 import net.minecraft.client.render.Font;
 import net.minecraft.client.render.TextureManager;
 import net.minecraft.client.render.texture.Texture;
@@ -13,12 +12,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = ScreenInventoryCreative.class, remap = false)
-public class ScreenInventoryCreativeMixinAccessory {
+@Mixin(value = ScreenInventory.class, remap = false)
+public class ScreenInventoryMixinNewInv {
 
     @Redirect(method = "drawGuiContainerBackgroundLayer", at= @At(value = "INVOKE", target = "Lnet/minecraft/client/render/TextureManager;loadTexture(Ljava/lang/String;)Lnet/minecraft/client/render/texture/Texture;"))
     public Texture bindNewInventory(TextureManager instance, String name) {
-        return instance.loadTexture("/assets/aether/textures/gui/container/creative.png");
+        return instance.loadTexture("/assets/aether/textures/gui/container/inventory.png");
     }
 
     @Inject(method = "drawGuiContainerForegroundLayer", at=@At("HEAD"), cancellable = true)
@@ -27,4 +26,5 @@ public class ScreenInventoryCreativeMixinAccessory {
         font.drawString(I18n.getInstance().translateKey("gui.inventory.label.crafting"), 98, 16, 4210752);
         ci.cancel();
     }
+
 }
