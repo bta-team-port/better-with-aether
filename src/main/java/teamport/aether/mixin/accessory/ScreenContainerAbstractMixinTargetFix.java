f30157c1ed1684fe604f01f4ecf2be63bde88c2c
Redart15 <redart15@users.noreply.github.com>
2025-07-25 20:29:53 +0200

    removed for the time being

--- src/main/java/teamport/aether/mixin/accessory/ScreenContainerAbstractMixinTargetFix.java
+++
@@ -1,9 +0,0 @@
-package teamport.aether.mixin.accessory;
-
-import net.minecraft.client.gui.container.ScreenContainerAbstract;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(value = ScreenContainerAbstract.class)
-public class ScreenContainerAbstractMixinTargetFix {
-    //TODO implement a mixin into clickInventory that changes local variable target based on if its an accessory
-}
