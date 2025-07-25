9c98bf4c2aa969e44896223c67999cdb92833ccc
Redart15 <redart15@users.noreply.github.com>
2025-07-25 11:27:19 +0200

    made its separate class for each of the accessories

---
+++ src/main/java/teamport/aether/items/accessory/ItemAccessory.java
@@ -0,0 +1,30 @@
+package teamport.aether.items.accessory;
+
+import net.minecraft.core.item.Item;
+import net.minecraft.core.item.material.ArmorMaterial;
+
+public class ItemAccessory extends Item implements Accessory{
+    private final int accessoryPiece;
+    private final String name;
+
+    public ItemAccessory(String translationKey, String namespaceId, int id, String name, int accessoryPiece) {
+        super(translationKey, namespaceId, id);
+        this.accessoryPiece = accessoryPiece;
+        this.name = name;
+        this.maxStackSize = 1;
+    }
+
+    public ItemAccessory(String translationKey, String namespaceId, int id, ArmorMaterial material, int accessoryPiece) {
+        super(translationKey, namespaceId, id);
+        this.accessoryPiece = accessoryPiece;
+        this.name = material.identifier.value();
+        this.maxStackSize = 1;
+    }
+
+    @Override
+    public int getAccessoryTypes() {
+        return this.accessoryPiece;
+    }
+
+    public String name(){return this.name;}
+}
