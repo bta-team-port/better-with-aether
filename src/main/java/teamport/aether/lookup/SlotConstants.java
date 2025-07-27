085c543d5342d76de5b5ccaa57b3fc4940daabe3
Redart15 <redart15@users.noreply.github.com>
2025-07-27 14:51:40 +0200

    i am not sure if this is a good idea, but neither are magic numbers

---
+++ src/main/java/teamport/aether/lookup/SlotConstants.java
@@ -0,0 +1,17 @@
+package teamport.aether.lookup;
+
+public final class SlotConstants {
+
+    // Slot specific constants, to prevent
+    public static final int ARMOR_START_SLOT = 41;
+    public static final int GLOVES_SLOT = 45;
+    public static final int CAPE_SLOT = 46;
+    public static final int WILDCARD_1_SLOT = 47;
+    public static final int WILDCARD_2_SLOT = 48;
+
+    // index specific constants
+    public static final int GLOVES_INDEX= 5;
+    public static final int CAPE_INDEX = 6;
+    public static final int WILDCARD_1_INDEX = 7;
+    public static final int WILDCARD_2_INDEX= 8;
+}
