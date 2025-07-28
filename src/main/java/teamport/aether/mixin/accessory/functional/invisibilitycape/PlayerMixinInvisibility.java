14c71589cbc8ecee471252e1e360fb5b6050ff47
Redart15 <redart15@users.noreply.github.com>
2025-07-28 23:38:08 +0200

    ported everything needed for invisibility cape

---
+++ src/main/java/teamport/aether/mixin/accessory/functional/invisibilitycape/PlayerMixinInvisibility.java
@@ -0,0 +1,23 @@
+package teamport.aether.mixin.accessory.functional.invisibilitycape;
+
+import net.minecraft.core.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import teamport.aether.items.IAetherAccessories;
+
+@Mixin(value = Player.class, remap = false)
+public class PlayerMixinInvisibility implements IAetherAccessories {
+    @Unique
+    private boolean invisible;
+
+    @Unique
+    public void aether$setInvisible(boolean invisible) {
+        this.invisible = invisible;
+    }
+
+    @Unique
+    public boolean aether$getInvisible() {
+        return invisible;
+    }
+
+}
