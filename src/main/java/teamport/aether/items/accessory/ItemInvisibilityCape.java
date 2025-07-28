a1dc4706d489b3b5f5fff5bac377dcc256f87bba
Redart15 <redart15@users.noreply.github.com>
2025-07-28 23:45:05 +0200

    moved some classes forgot to include this one

--- src/main/java/teamport/aether/items/accessory/ItemInvisibilityCape.java
+++ src/main/java/teamport/aether/items/accessory/ItemInvisibilityCape.java
@@ -2,13 +2,14 @@ package teamport.aether.items.accessory;
 
 import net.minecraft.core.entity.player.Player;
 import net.minecraft.core.item.ItemStack;
-import teamport.aether.entity.IAetherAccessories;
+import teamport.aether.items.IAetherAccessories;
 
 public class ItemInvisibilityCape extends ItemAccessory {
 
     public ItemInvisibilityCape(String translationKey, String namespaceId, int id, String name, int accessoryPiece) {
         super(translationKey, namespaceId, id, name, accessoryPiece);
     }
+
 
     @Override
     public void onAccessoryAdded(Player player, ItemStack accessory) {
