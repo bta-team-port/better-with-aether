e9f882bdf700499c81ea43f11ca87e015a78ea96
Redart15 <redart15@users.noreply.github.com>
2025-07-28 23:38:08 +0200

    ported everything needed for invisibility cape

---
+++ src/main/java/teamport/aether/mixin/accessory/functional/invisibilitycape/MobRenderPlayerMixinInvisibility.java
@@ -0,0 +1,27 @@
+package teamport.aether.mixin.accessory.functional.invisibilitycape;
+
+import net.minecraft.client.render.entity.MobRendererPlayer;
+import net.minecraft.client.render.tessellator.Tessellator;
+import net.minecraft.core.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import teamport.aether.items.IAetherAccessories;
+
+@Mixin(value = MobRendererPlayer.class, remap = false)
+public class MobRenderPlayerMixinInvisibility {
+
+
+    //TODO Fix this so that the player becomes invisible wearing the invis cape
+
+    @Inject(method = "drawFirstPersonHand", at = @At("HEAD"), cancellable = true)
+    public void callDrawFirstPersonHandBefore(Player player, boolean isLeft, CallbackInfo ci) {
+        if (((IAetherAccessories)player).aether$getInvisible()) ci.cancel();
+    }
+
+    @Inject(method = "render(Lnet/minecraft/client/render/tessellator/Tessellator;Lnet/minecraft/core/entity/player/Player;DDDFF)V", at = @At("HEAD"), cancellable = true)
+    public void renderPlayer(Tessellator tessellator, Player entity, double x, double y, double z, float yaw, float partialTick, CallbackInfo ci) {
+        if (((IAetherAccessories)entity).aether$getInvisible()) ci.cancel();
+    }
+}
---
+++ src/main/java/teamport/aether/mixin/accessory/functional/invisibilitycape/PlayerLocalMixinInvisibility.java
@@ -0,0 +1,21 @@
+package teamport.aether.mixin.accessory.functional.invisibilitycape;
+
+import net.minecraft.client.entity.player.PlayerLocal;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import teamport.aether.items.IAetherAccessories;
+
+@Mixin(value = PlayerLocal.class, remap = false)
+public class PlayerLocalMixinInvisibility implements IAetherAccessories {
+    @Unique
+    boolean invisible = false;
+
+    @Unique
+    public void aether$setInvisible(boolean invisible) {
+        this.invisible = invisible;
+    }
+    @Unique
+    public boolean aether$getInvisible() {
+        return invisible;
+    }
+}
