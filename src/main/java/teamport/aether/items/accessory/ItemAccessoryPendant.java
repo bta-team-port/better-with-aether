7082ac4795875011f3829fb020f3e3bd9dbdaa0d
Redart15 <redart15@users.noreply.github.com>
2025-07-28 15:21:30 +0200

    added additional constructor

--- src/main/java/teamport/aether/items/accessory/ItemAccessoryPendant.java
+++ src/main/java/teamport/aether/items/accessory/ItemAccessoryPendant.java
@@ -1,7 +1,13 @@
 package teamport.aether.items.accessory;
 
+import net.minecraft.core.item.material.ArmorMaterial;
+
 public class ItemAccessoryPendant extends ItemAccessory{
     public ItemAccessoryPendant(String translationKey, String namespaceId, int id, String name, int accessoryPiece) {
+        super(translationKey, namespaceId, id, name, accessoryPiece);
+    }
+
+    public ItemAccessoryPendant(String translationKey, String namespaceId, int id, ArmorMaterial name, int accessoryPiece) {
         super(translationKey, namespaceId, id, name, accessoryPiece);
     }
 }
