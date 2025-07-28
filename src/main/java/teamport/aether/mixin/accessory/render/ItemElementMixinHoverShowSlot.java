4c713273ba183879ce0ccd445474ca634667fd10
Redart15 <redart15@users.noreply.github.com>
2025-07-28 15:22:35 +0200

    wildcard slot change icon depending on what item is hover on or what item is dragged

--- src/main/java/teamport/aether/mixin/accessory/render/ItemElementMixinHoverShowSlot.java
+++ src/main/java/teamport/aether/mixin/accessory/render/ItemElementMixinHoverShowSlot.java
@@ -2,17 +2,30 @@ package teamport.aether.mixin.accessory.render;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ItemElement;
+import net.minecraft.client.gui.container.ScreenInventory;
+import net.minecraft.client.gui.container.ScreenInventoryCreative;
 import net.minecraft.client.render.texture.stitcher.IconCoordinate;
 import net.minecraft.client.render.texture.stitcher.TextureRegistry;
+import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.item.Items;
+import net.minecraft.core.player.inventory.menu.MenuInventory;
 import net.minecraft.core.player.inventory.slot.Slot;
 import org.lwjgl.input.Mouse;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import teamport.aether.items.accessory.Accessory;
+import teamport.aether.items.AetherItemTags;
+import teamport.aether.items.accessory.*;
+
+import javax.annotation.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 import static teamport.aether.AetherMod.ARMOR_START_INDEX;
 import static teamport.aether.items.accessory.SlotAccessory.*;
@@ -20,20 +33,31 @@ import static teamport.aether.items.accessory.SlotAccessory.*;
 @Mixin(value = ItemElement.class, remap = false)
 public class ItemElementMixinHoverShowSlot {
 
-    // Store the arguments temporarily
-//    private ItemStack itemStack;
-//    private boolean isSelected;
-    private Slot slot;
+    @Unique private Slot slot;
+    @Unique private byte tick = 0;
+    @Unique private Random random = new Random();
 
-    @Inject(method = "render", at = @At("HEAD"))
+    @Unique private static final Map<Class<? extends Item>, String> CLASS_OUTLINE_TEXTURES = new HashMap<>();
+    @Unique private static final Map<Integer, String> ID_OUTLINE_TEXTURES = new HashMap<>();
+
+    static {
+        CLASS_OUTLINE_TEXTURES.put(ItemAccessoryPendant.class, "armor_pendant_outline");
+        CLASS_OUTLINE_TEXTURES.put(ItemRegenStone.class, "armor_stone_outline");
+        CLASS_OUTLINE_TEXTURES.put(ItemGoldenFeather.class, "armor_feather_outline");
+        CLASS_OUTLINE_TEXTURES.put(ItemIronBubble.class, "armor_bubble_outline");
+        CLASS_OUTLINE_TEXTURES.put(ItemShield.class, "armor_shield_outline");
+
+        ID_OUTLINE_TEXTURES.put(Items.TOOL_CLOCK.id, "armor_clock_outline");
+        ID_OUTLINE_TEXTURES.put(Items.TOOL_COMPASS.id, "armor_compass_outline");
+        ID_OUTLINE_TEXTURES.put(Items.TOOL_CALENDAR.id, "armor_calendar_outline");
+    }
+
+    // capture slot for later
+    @Inject(method = "render*", at = @At("HEAD"))
     private void captureRenderArgs(ItemStack itemStack, int x, int y, boolean isSelected, Slot slot, CallbackInfo ci) {
-//        this.itemStack = itemStack;
-//        this.isSelected = isSelected;
         this.slot = slot;
     }
 
-
-    // TODO make it so when hovering over a slot it shows where to go, currently the coordinates are off, and its inconsistent between screen resolutions
     @Redirect(
             method = "Lnet/minecraft/client/gui/ItemElement;render(Lnet/minecraft/core/item/ItemStack;IIZLnet/minecraft/core/player/inventory/slot/Slot;)V",
             at = @At(
@@ -42,43 +66,64 @@ public class ItemElementMixinHoverShowSlot {
                     ordinal = 0
             )
     )
-    public void fuck(ItemElement instance, int x, int y, int width, int height, IconCoordinate iconCoordinate){
-//        if (this.slot.index < ARMOR_START_INDEX + WILDCARD_1_SLOT) {
-//            instance.drawTexturedIcon(x,y,16,16,iconCoordinate);
-//            return;
-//        }
-//        Minecraft mc = Minecraft.getMinecraft();
-//        int mouseX = Mouse.getX() * mc.resolution.getScaledWidthScreenCoords() / mc.resolution.getWidthScreenCoords();
-//        int mouseY = Mouse.getY() * mc.resolution.getScaledHeightScreenCoords() / mc.resolution.getHeightScreenCoords() - 1;
-//        if(mc.currentScreen == null){
-//            instance.drawTexturedIcon(x,y,16,16,iconCoordinate);
-//            return;
-//        }
-//        x = mc.currentScreen.height +  x;
-//        y = mc.currentScreen.width +  y;
-//        for(Slot slot: mc.thePlayer.inventorySlots.slots){
-//            if(getIsMouseOverSlot(slot, x, y, (int)Math.round(mouseX), (int)Math.round(mouseY))){
-//                ItemStack hoverStack = slot.getItemStack();
-//                if(hoverStack == null){
-//                    continue;
-//                }
-//                IconCoordinate iconIndex;
-//                if(hoverStack.getItem() instanceof Accessory){
-//                    iconIndex = TextureRegistry.getTexture("aether:item/amber");
-//                    instance.drawTexturedIcon(x,y,16,16,iconIndex);
-//                    return;
-//                }else{
-//                    instance.drawTexturedIcon(x,y,16,16,iconCoordinate);
-//                    return;
-//                }
-//            }
-//        }
-        instance.drawTexturedIcon(x, y, 16, 16, iconCoordinate);
+    public void changeWildcardIconOnHoverAndClick(ItemElement instance, int x, int y, int slotWidth, int slotHeight, IconCoordinate defaultIcon) {
+        Minecraft mc = Minecraft.getMinecraft();
+        if (
+                this.slot.index >= ARMOR_START_INDEX + WILDCARD_1_SLOT
+                        && (mc.currentScreen instanceof ScreenInventory || mc.currentScreen instanceof ScreenInventoryCreative)
+        ) {
+            String root = "aether:item/wildcard/";
+            // got this from WoldRender, works like a charm
+            int screenWidth = mc.resolution.getScaledWidthScreenCoords();
+            int screenHeight = mc.resolution.getScaledHeightScreenCoords();
+            int mouseX = Mouse.getX() * screenWidth / mc.resolution.getWidthScreenCoords();
+            int mouseY = screenHeight - Mouse.getY() * screenHeight / mc.resolution.getHeightScreenCoords() - 1;
+
+            // TODO better way of checking if an item is dragged or not
+            if(((MenuInventory)((ScreenInventory) mc.currentScreen).inventorySlots).inventory.getHeldItemStack() != null){
+                ItemStack hoverStack = ((MenuInventory)((ScreenInventory) mc.currentScreen).inventorySlots).inventory.getHeldItemStack();
+                if (hoverStack != null) {
+                    Item item = hoverStack.getItem();
+                    if (item instanceof Accessory || item.hasTag(AetherItemTags.ACCESSORY)) {
+                        IconCoordinate displayIcon = getIconCoordinate(item, root);
+                        instance.drawTexturedIcon(x, y, 16, 16, displayIcon != null ? displayIcon : defaultIcon);
+                        return;
+                    }
+                }
+                instance.drawTexturedIcon(x, y, 16, 16, defaultIcon);
+                return;
+            }
+
+            Slot slot = ((ScreenInventory) mc.currentScreen).getSlotAtPosition(mouseX, mouseY);
+            if (slot != null) {
+                ItemStack hoverStack = slot.getItemStack();
+                if (hoverStack != null) {
+                    Item item = hoverStack.getItem();
+                    if (item instanceof Accessory || item.hasTag(AetherItemTags.ACCESSORY)) {
+                        IconCoordinate displayIcon = getIconCoordinate(item, root);
+                        instance.drawTexturedIcon(x, y, 16, 16, displayIcon != null ? displayIcon : defaultIcon);
+                        return;
+                    }
+                }
+            }
+        }
+        instance.drawTexturedIcon(x, y, 16, 16, defaultIcon);
     }
 
-    private static boolean getIsMouseOverSlot(Slot slot, int x, int y, int mouseX, int mouseY) {
-//        return mouseX >= x + slot.x - 1 && mouseX < x + slot.x + 16 + 1 && mouseY >= y + slot.y - 1 && mouseY < y + slot.y + 16 + 1;
-        return mouseX >= slot.x - 1 && mouseX < slot.x + 16 + 1 && mouseY >= slot.y - 1 && mouseY < slot.y + 16 + 1;
-    }
+    @Unique
+    private static @Nullable IconCoordinate getIconCoordinate(Item item, String root) {
+        for (Map.Entry<Class<? extends Item>, String> entry : CLASS_OUTLINE_TEXTURES.entrySet()) {
+            if (entry.getKey().isInstance(item)) {
+                return TextureRegistry.getTexture(root + entry.getValue());
+            }
+        }
 
+        if (item.hasTag(AetherItemTags.ACCESSORY)) {
+            String textureName = ID_OUTLINE_TEXTURES.get(item.id);
+            if (textureName != null) {
+                return TextureRegistry.getTexture(root + textureName);
+            }
+        }
+        return null;
+    }
 }
