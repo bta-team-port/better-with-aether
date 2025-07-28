609675315c938587bc5bcc250d12b66e832f0339
Redart15 <redart15@users.noreply.github.com>
2025-07-29 00:01:26 +0200

    setup, in case there wont be any other way of doing this.

--- src/main/java/teamport/aether/items/accessory/ItemInvisibilityCape.java
+++ src/main/java/teamport/aether/items/accessory/ItemInvisibilityCape.java
@@ -1,8 +1,12 @@
 package teamport.aether.items.accessory;
 
+import net.minecraft.core.entity.Entity;
 import net.minecraft.core.entity.player.Player;
 import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.world.World;
 import teamport.aether.items.IAetherAccessories;
+
+import static teamport.aether.items.accessory.SlotAccessory.CAPE_SLOT;
 
 public class ItemInvisibilityCape extends ItemAccessory {
 
@@ -10,6 +14,15 @@ public class ItemInvisibilityCape extends ItemAccessory {
         super(translationKey, namespaceId, id, name, accessoryPiece);
     }
 
+    public void inventoryTick(ItemStack itemstack, World world, Entity entity, int slotId, boolean flag) {
+        Player player = (Player) entity;
+        if (
+                slotId < player.inventory.mainInventory.length
+                && slotId - player.inventory.mainInventory.length != CAPE_SLOT
+        ) {
+            return;
+        }
+    }
 
     @Override
     public void onAccessoryAdded(Player player, ItemStack accessory) {
