4c62830980abaa779018a1748f4b619b0027dae2
Redart15 <redart15@users.noreply.github.com>
2025-07-29 15:41:33 +0200

    made a lookup table to simplify adding new outline for accessories, added maps as accessories, made the slots show random icons

---
+++ src/main/java/teamport/aether/lookup/LookupAccessoryIcons.java
@@ -0,0 +1,71 @@
+package teamport.aether.lookup;
+
+import net.minecraft.core.item.Item;
+import net.minecraft.core.item.Items;
+import teamport.aether.items.AetherItemTags;
+import teamport.aether.items.accessory.*;
+
+import javax.annotation.Nullable;
+import java.util.*;
+
+public class LookupAccessoryIcons {
+    public  static final LookupAccessoryIcons instance = new LookupAccessoryIcons();
+    public final Map<Class<? extends Item>, String> CLASS_OUTLINE_TEXTURES = new HashMap<>();
+    public final Map<Integer, String> ID_OUTLINE_TEXTURES = new HashMap<>();
+    public final List<String> LIST_TEXTURE = new ArrayList<>();
+    private static final Random random = new Random();
+
+    public LookupAccessoryIcons(){this.register();}
+
+    public void register() {
+        addEntry(ItemAccessoryPendant.class, "armor_pendant_outline");
+        addEntry(ItemRegenStone.class, "armor_stone_outline");
+        addEntry(ItemGoldenFeather.class, "armor_feather_outline");
+        addEntry(ItemIronBubble.class, "armor_bubble_outline");
+        addEntry(ItemShield.class, "armor_shield_outline");
+
+        addEntry(Items.TOOL_CLOCK.id, "armor_clock_outline");
+        addEntry(Items.TOOL_COMPASS.id, "armor_compass_outline");
+        addEntry(Items.TOOL_CALENDAR.id, "armor_calendar_outline");
+        addEntry(Items.MAP.id, "armor_outline_map_filled");
+//        addToList("armor_outline_map_filled");
+    }
+
+    public void addToList(String texturePath){
+        LIST_TEXTURE.add(texturePath);
+    }
+    public void addEntry(Class<? extends Item> clazz, String texturePath){
+        this.CLASS_OUTLINE_TEXTURES.put(clazz,texturePath);
+        this.LIST_TEXTURE.add(texturePath);
+
+    }
+    public void addEntry(Integer id, String texturePath){
+        this.ID_OUTLINE_TEXTURES.put(id, texturePath);
+        this.LIST_TEXTURE.add(texturePath);
+    }
+    public @Nullable String getEntry(Item item ){
+        for (Map.Entry<Class<? extends Item>, String> entry : this.CLASS_OUTLINE_TEXTURES.entrySet()) {
+            if (entry.getKey().isInstance(item)) {
+                return entry.getValue();
+            }
+        }
+
+        if (item.hasTag(AetherItemTags.ACCESSORY)) {
+            return this.ID_OUTLINE_TEXTURES.getOrDefault(item.id, null);
+        }
+        return null;
+    }
+    public String getRandomEntry(){
+        return LIST_TEXTURE.get(random.nextInt(LIST_TEXTURE.size()));
+    }
+    public Map<Class<? extends Item>, String> getClassTextureMap(){
+        return this.CLASS_OUTLINE_TEXTURES;
+    }
+    public Map<Integer, String> getIDTextureMap(){
+        return this.ID_OUTLINE_TEXTURES;
+    }
+    public List<String> getTextureList(){
+        return this.LIST_TEXTURE;
+    }
+
+}
--- src/main/java/teamport/aether/mixin/accessory/render/ItemElementMixinHoverShowSlot.java
+++ src/main/java/teamport/aether/mixin/accessory/render/ItemElementMixinHoverShowSlot.java
@@ -2,6 +2,7 @@ package teamport.aether.mixin.accessory.render;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ItemElement;
 import net.minecraft.client.gui.container.ScreenInventory;
 import net.minecraft.client.gui.container.ScreenInventoryCreative;
@@ -9,55 +10,30 @@ import net.minecraft.client.render.texture.stitcher.IconCoordinate;
 import net.minecraft.client.render.texture.stitcher.TextureRegistry;
 import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemStack;
-import net.minecraft.core.item.Items;
 import net.minecraft.core.player.inventory.menu.MenuInventory;
 import net.minecraft.core.player.inventory.slot.Slot;
 import org.lwjgl.input.Mouse;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import teamport.aether.items.AetherItemTags;
 import teamport.aether.items.accessory.*;
-
-import javax.annotation.Nullable;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import teamport.aether.lookup.LookupAccessoryIcons;
 
 import static teamport.aether.AetherMod.ARMOR_START_INDEX;
 import static teamport.aether.items.accessory.SlotAccessory.*;
 
 @Mixin(value = ItemElement.class, remap = false)
-public class ItemElementMixinHoverShowSlot {
+abstract public class ItemElementMixinHoverShowSlot extends Gui {
 
-    @Unique private Slot slot;
-    @Unique private byte tick = 0;
-    @Unique private Random random = new Random();
+    @Unique int tick = 0;
+    @Unique String currentIconPath_WILDCARD_1 = LookupAccessoryIcons.instance.getRandomEntry();
+    @Unique String currentIconPath_WILDCARD_2 = LookupAccessoryIcons.instance.getRandomEntry();
 
-    @Unique private static final Map<Class<? extends Item>, String> CLASS_OUTLINE_TEXTURES = new HashMap<>();
-    @Unique private static final Map<Integer, String> ID_OUTLINE_TEXTURES = new HashMap<>();
-
-    static {
-        CLASS_OUTLINE_TEXTURES.put(ItemAccessoryPendant.class, "armor_pendant_outline");
-        CLASS_OUTLINE_TEXTURES.put(ItemRegenStone.class, "armor_stone_outline");
-        CLASS_OUTLINE_TEXTURES.put(ItemGoldenFeather.class, "armor_feather_outline");
-        CLASS_OUTLINE_TEXTURES.put(ItemIronBubble.class, "armor_bubble_outline");
-        CLASS_OUTLINE_TEXTURES.put(ItemShield.class, "armor_shield_outline");
-
-        ID_OUTLINE_TEXTURES.put(Items.TOOL_CLOCK.id, "armor_clock_outline");
-        ID_OUTLINE_TEXTURES.put(Items.TOOL_COMPASS.id, "armor_compass_outline");
-        ID_OUTLINE_TEXTURES.put(Items.TOOL_CALENDAR.id, "armor_calendar_outline");
-    }
-
-    // capture slot for later
-    @Inject(method = "render*", at = @At("HEAD"))
-    private void captureRenderArgs(ItemStack itemStack, int x, int y, boolean isSelected, Slot slot, CallbackInfo ci) {
-        this.slot = slot;
-    }
+    @Shadow
+    Minecraft mc;
 
     @Redirect(
             method = "Lnet/minecraft/client/gui/ItemElement;render(Lnet/minecraft/core/item/ItemStack;IIZLnet/minecraft/core/player/inventory/slot/Slot;)V",
@@ -68,61 +44,52 @@ public class ItemElementMixinHoverShowSlot {
             )
     )
     public void changeWildcardIconOnHoverAndClick(ItemElement instance, int x, int y, int slotWidth, int slotHeight, IconCoordinate defaultIcon, @Local Slot currectSlot) {
-        Minecraft mc = Minecraft.getMinecraft();
         if (
                 currectSlot.index >= ARMOR_START_INDEX + WILDCARD_1_SLOT
-                        && (mc.currentScreen instanceof ScreenInventory || mc.currentScreen instanceof ScreenInventoryCreative)
+                && (this.mc.currentScreen instanceof ScreenInventory || this.mc.currentScreen instanceof ScreenInventoryCreative)
         ) {
+            tick++;
+            if(tick > 600){ // 3000
+                tick = 0;
+                currentIconPath_WILDCARD_1 = LookupAccessoryIcons.instance.getRandomEntry();
+                currentIconPath_WILDCARD_2 = LookupAccessoryIcons.instance.getRandomEntry();
+            }
             String root = "aether:item/wildcard/";
+            defaultIcon = TextureRegistry.getTexture(root + (currectSlot.index > ARMOR_START_INDEX + WILDCARD_1_SLOT ? currentIconPath_WILDCARD_2: currentIconPath_WILDCARD_1));
             // got this from WoldRender, works like a charm
-            int screenWidth = mc.resolution.getScaledWidthScreenCoords();
-            int screenHeight = mc.resolution.getScaledHeightScreenCoords();
-            int mouseX = Mouse.getX() * screenWidth / mc.resolution.getWidthScreenCoords();
-            int mouseY = screenHeight - Mouse.getY() * screenHeight / mc.resolution.getHeightScreenCoords() - 1;
+            int screenWidth = this.mc.resolution.getScaledWidthScreenCoords();
+            int screenHeight = this.mc.resolution.getScaledHeightScreenCoords();
+            int mouseX = Mouse.getX() * screenWidth / this.mc.resolution.getWidthScreenCoords();
+            int mouseY = screenHeight - Mouse.getY() * screenHeight / this.mc.resolution.getHeightScreenCoords() - 1;
 
             // TODO better way of checking if an item is dragged or not
-            if(((MenuInventory)((ScreenInventory) mc.currentScreen).inventorySlots).inventory.getHeldItemStack() != null){
-                ItemStack hoverStack = ((MenuInventory)((ScreenInventory) mc.currentScreen).inventorySlots).inventory.getHeldItemStack();
+            if(((MenuInventory)((ScreenInventory) this.mc.currentScreen).inventorySlots).inventory.getHeldItemStack() != null){
+                ItemStack hoverStack = ((MenuInventory)((ScreenInventory) this.mc.currentScreen).inventorySlots).inventory.getHeldItemStack();
                 if (hoverStack != null) {
                     Item item = hoverStack.getItem();
                     if (item instanceof Accessory || item.hasTag(AetherItemTags.ACCESSORY)) {
-                        IconCoordinate displayIcon = getIconCoordinate(item, root);
-                        instance.drawTexturedIcon(x, y, 16, 16, displayIcon != null ? displayIcon : defaultIcon);
+                        String iconPath = LookupAccessoryIcons.instance.getEntry(item);
+                        IconCoordinate displayIcon = iconPath != null ? TextureRegistry.getTexture(root + iconPath) : defaultIcon;
+                        instance.drawTexturedIcon(x, y, 16, 16, displayIcon);
                         return;
                     }
                 }
             }
 
-            Slot slot = ((ScreenInventory) mc.currentScreen).getSlotAtPosition(mouseX, mouseY);
+            Slot slot = ((ScreenInventory) this.mc.currentScreen).getSlotAtPosition(mouseX, mouseY);
             if (slot != null) {
                 ItemStack hoverStack = slot.getItemStack();
                 if (hoverStack != null) {
                     Item item = hoverStack.getItem();
                     if (item instanceof Accessory || item.hasTag(AetherItemTags.ACCESSORY)) {
-                        IconCoordinate displayIcon = getIconCoordinate(item, root);
-                        instance.drawTexturedIcon(x, y, 16, 16, displayIcon != null ? displayIcon : defaultIcon);
+                        String iconPath = LookupAccessoryIcons.instance.getEntry(item);
+                        IconCoordinate displayIcon = iconPath != null ? TextureRegistry.getTexture(root + iconPath) : defaultIcon;
+                        instance.drawTexturedIcon(x, y, 16, 16, displayIcon);
                         return;
                     }
                 }
             }
         }
         instance.drawTexturedIcon(x, y, 16, 16, defaultIcon);
-    }
-
-    @Unique
-    private static @Nullable IconCoordinate getIconCoordinate(Item item, String root) {
-        for (Map.Entry<Class<? extends Item>, String> entry : CLASS_OUTLINE_TEXTURES.entrySet()) {
-            if (entry.getKey().isInstance(item)) {
-                return TextureRegistry.getTexture(root + entry.getValue());
-            }
-        }
-
-        if (item.hasTag(AetherItemTags.ACCESSORY)) {
-            String textureName = ID_OUTLINE_TEXTURES.get(item.id);
-            if (textureName != null) {
-                return TextureRegistry.getTexture(root + textureName);
-            }
-        }
-        return null;
     }
 }
