af82e6f80789449fe5a5474c8725c4d1089ab1c9
LukeisStuff <lukeisawesome03@gmail.com>
2025-07-29 04:25:33 -0400

    added phyg variants, finished valkyrie model, added another set of phow variants, fixed the accessory glove and pendant rendering, added todo

--- src/main/java/teamport/aether/mixin/accessory/render/MobRendererPlayerMixinGlovesAndPendantRender.java
+++ src/main/java/teamport/aether/mixin/accessory/render/MobRendererPlayerMixinGlovesAndPendantRender.java
@@ -8,7 +8,9 @@ import net.minecraft.client.render.model.ModelBiped;
 import net.minecraft.core.entity.player.Player;
 import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,6 +26,8 @@ import static teamport.aether.items.accessory.SlotAccessory.GLOVES_SLOT;
 @Mixin(value = MobRendererPlayer.class, remap = false)
 abstract public class MobRendererPlayerMixinGlovesAndPendantRender extends MobRenderer<Player> {
 
+    @Shadow private ModelBiped modelBipedMain;
+    @Shadow @Final private ModelBiped modelArmorChestplate;
     @Unique
     public final ModelBiped modelAccessories = new ModelBiped(1.0F);
 
@@ -32,36 +36,30 @@ abstract public class MobRendererPlayerMixinGlovesAndPendantRender extends MobRe
     }
 
     // TODO fix the gloves in the first person
-    // TODO fix pendant also being rendered when gloves are equipped
+    // TODO fix armor not shaking with the player when punching in third person
     @Inject(method = "drawFirstPersonHand", at = @At("TAIL"), cancellable = true)
     public void callDrawFirstPersonHandAfter(Player player, boolean isLeft, CallbackInfo ci) {
         ItemStack itemStack = player.inventory.armorInventory[GLOVES_SLOT];
         if (itemStack != null && itemStack.getItem() instanceof ItemAccessoryGloves) {
             Item item = itemStack.getItem();
             String path = String.format("/assets/%s/textures/armor/%s_pendant_and_gloves.png", item.namespaceID.namespace(), ((ItemAccessory) item).name());
-//            String path = String.format("/assets/%s/textures/armor/armor_gloves_%s.png", item.namespaceID.namespace(), ((ItemAccessory) item).name());
             if (renderDispatcher.textureManager == null) return;
             renderDispatcher.textureManager.loadTexture(path).bind();
             this.modelAccessories.onGround = 0.0f;
             this.modelAccessories.isRiding = false;
-            this.modelAccessories.armRight.visible = true;
             this.modelAccessories.setupAnimation(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0625f);
-            this.modelAccessories.armRight.render(0.0625f);
+            if (isLeft) {
+                this.modelAccessories.armLeft.visible = true;
+                this.modelAccessories.armLeft.render(0.0625F);
+            } else {
+                this.modelAccessories.armRight.visible = true;
+                this.modelAccessories.armRight.render(0.0625F);
+            }
+            this.modelAccessories.armLeft.visible = false;
             this.modelAccessories.armRight.visible = false;
         }
+        ci.cancel();
     }
-
-    //TODO Fix this so that the player becomes invisible wearing the invis cape
-
-    //    @Inject(method = "drawFirstPersonHand", at = @At("HEAD"), cancellable = true)
-//    public void callDrawFirstPersonHandBefore(Player player, boolean isLeft, CallbackInfo ci) {
-//        if (((IAetherAccessories)player).aether$getInvisible()) ci.cancel();
-//    }
-
-//    @Inject(method = "render(Lnet/minecraft/client/render/tessellator/Tessellator;Lnet/minecraft/core/entity/player/Player;DDDFF)V", at = @At("HEAD"), cancellable = true)
-//    public void renderPlayer(Tessellator tessellator, Player entity, double x, double y, double z, float yaw, float partialTick, CallbackInfo ci) {
-//        if (((IAetherAccessories)entity).aether$getInvisible()) ci.cancel();
-//    }
 
     @ModifyArg(method = "prepareArmor*", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/player/inventory/container/ContainerInventory;armorItemInSlot(I)Lnet/minecraft/core/item/ItemStack;"))
     public int getArmorItemNotNegative(int i, @Local(argsOnly = true) int renderPass) {
@@ -73,9 +71,9 @@ abstract public class MobRendererPlayerMixinGlovesAndPendantRender extends MobRe
         this.modelAccessories.holdingRightHand = player.inventory.getCurrentItem() != null;
         this.modelAccessories.sneaking = player.isSneaking();
         this.modelAccessories.isRiding = player.isPassenger();
-        this.modelAccessories.body.visible = renderPass == 4;
-        this.modelAccessories.armLeft.visible = renderPass == 10;
-        this.modelAccessories.armRight.visible = renderPass == 10;
+        this.modelAccessories.body.visible = renderPass == 6 || renderPass == 7;
+        this.modelAccessories.armLeft.visible = renderPass == 4;
+        this.modelAccessories.armRight.visible = renderPass == 4;
 
         ItemStack itemStack = player.inventory.armorInventory[renderPass];
         if (itemStack == null) {
@@ -86,6 +84,7 @@ abstract public class MobRendererPlayerMixinGlovesAndPendantRender extends MobRe
         if (!(item instanceof ItemAccessoryGloves) && !(item instanceof ItemAccessoryPendant)) {
             return;
         }
+
         String path = String.format("/assets/%s/textures/armor/%s_pendant_and_gloves.png", item.namespaceID.namespace(), ((ItemAccessory) item).name());
         renderDispatcher.textureManager.loadTexture(path).bind();
         this.setArmorModel(this.modelAccessories);
