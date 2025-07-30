e1ab1fc1bb3c8ff3400700c63d58175503d4be4b
LukeisStuff <lukeisawesome03@gmail.com>
2025-07-30 13:51:12 -0400

    fix cloud fall damage, adjust armor material stats (might adjust more later)

--- src/main/java/teamport/aether/items/AetherArmorMaterial.java
+++ src/main/java/teamport/aether/items/AetherArmorMaterial.java
@@ -18,10 +18,26 @@ public class AetherArmorMaterial {
     public static final ArmorMaterial GRAVITITE = ArmorHelper.createArmorMaterial(MOD_ID,
             "gravitite",
             800,
-            45f,
-            45f,
-            45f,
+            56f,
+            56f,
+            56f,
             134f);
+
+    public static final ArmorMaterial DIAMOND = ArmorHelper.createArmorMaterial(MOD_ID,
+            "diamond",
+            800,
+            66.0F,
+            66.0F,
+            124.0F,
+            66.0F);
+
+    public static final ArmorMaterial STEEL = ArmorHelper.createArmorMaterial(MOD_ID,
+            "steel",
+            1200,
+            55.0F,
+            150.0F,
+            55.0F,
+            55.0F);
 
     public static final ArmorMaterial PHOENIX = ArmorHelper.createArmorMaterial(MOD_ID,
             "phoenix",
@@ -36,15 +52,15 @@ public class AetherArmorMaterial {
             1200,
             45f,
             134f,
-            134f,
-            0f);
+            70f,
+            25f);
 
     public static final ArmorMaterial NEPTUNE = ArmorHelper.createArmorMaterial(MOD_ID,
             "neptune",
             800,
             75f,
             75f,
-            75f,
+            20f,
             75f)
             .withProtectionPercentage(DamageType.DROWN, 50f);
 }
--- src/main/java/teamport/aether/mixin/EntityMixin.java
+++ src/main/java/teamport/aether/mixin/EntityMixin.java
@@ -1,11 +1,13 @@
 package teamport.aether.mixin;
 
 import net.minecraft.core.entity.Entity;
+import net.minecraft.core.util.helper.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = Entity.class, remap = false)
@@ -22,4 +24,10 @@ public class EntityMixin {
         deltaY = y - prevY;
         prevY = y - 0.5;
     }
+
+    @Redirect(method = "move(DDD)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/util/helper/MathHelper;floor(D)I", ordinal = 5))
+    private int extendBlockRange(double xd, double yd, double zd){
+        return MathHelper.floor(((Entity)(Object)this).bb.minY + 0.001 + deltaY);
+    }
+
 }
