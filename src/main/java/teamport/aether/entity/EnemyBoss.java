4ef2722f8d051280814af68592e5168b0b30e338
Oly <33404595+Olypolyu@users.noreply.github.com>
2025-08-01 19:19:21 -0300

    port Slider AI

--- src/main/java/teamport/aether/entity/EnemyBoss.java
+++ src/main/java/teamport/aether/entity/EnemyBoss.java
@@ -1,4 +1,15 @@
 package teamport.aether.entity;
 
+import net.minecraft.core.item.ItemStack;
+
+import javax.annotation.Nullable;
+
 public interface EnemyBoss {
+
+    int dungeonID = -1;
+    String getBossName = "";
+    @Nullable
+    ItemStack trophy = null;
+
+    void returnToPedestal();
 }
