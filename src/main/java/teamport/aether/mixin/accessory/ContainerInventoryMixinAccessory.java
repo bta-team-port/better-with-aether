d9a085b02f39922c33c0cc10952947f78b8e9299
Redart15 <redart15@users.noreply.github.com>
2025-08-01 13:12:49 +0200

    fixed targeting, made effect update on drag and drop, changed interface to effect, merged some mixins, removed some todo

--- src/main/java/teamport/aether/mixin/accessory/ContainerInventoryMixinAccessory.java
+++ src/main/java/teamport/aether/mixin/accessory/ContainerInventoryMixinAccessory.java
@@ -8,12 +8,13 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import teamport.aether.items.accessory.IAccessory;
 import teamport.aether.items.accessory.IAccessoryEffects;
 
 
 @Mixin(value = ContainerInventory.class, remap = false)
-public class ContainerInventoryMixinIncArmorInv {
+public class ContainerInventoryMixinAccessory {
 
     @Shadow public Player player;
 
@@ -38,7 +39,7 @@ public class ContainerInventoryMixinIncArmorInv {
         ContainerInventory inv = (ContainerInventory) (Object) this;
         for (ItemStack item : inv.armorInventory) {
             if (item != null && item.getItem() instanceof IAccessoryEffects) {
-                ((IAccessoryEffects) item.getItem()).onAccessoryAdded(inv.player, item);
+                ((IAccessoryEffects) item.getItem()).addEffect(inv.player, item);
             }
         }
     }
@@ -49,10 +50,42 @@ public class ContainerInventoryMixinIncArmorInv {
         return this.armorInventory.length;
     }
 
+    @Inject(method = "decrementAnimations", at = @At("TAIL"))
+    public void addArmorAnimations(CallbackInfo ci){
+        ContainerInventory inv = (ContainerInventory)(Object) this;
+        for(int slot = 0; slot < inv.armorInventory.length; slot++){
+            if (inv.armorInventory[slot] != null) {
+                inv.armorInventory[slot].updateAnimation(inv.player.world, inv.player, slot + inv.mainInventory.length, inv.getCurrentItemIndex() == slot);
+            }
+        }
+    }
 
-    // TODO this just does not work, currently no way to know from what slot an item came, only where it goes
+
+    /**
+     * @reason 7.3_04 currently handles left click and drop differently from shift clicking.
+     * To guarantee that the effect of the accessories is correctly remove on left click and drop
+     * a mixin is needed into removeItem. - Redart15
+     * */
+    @Inject(method = "removeItem", at = @At("HEAD"))
+    public void updateEffects(int index, int takeAmount, CallbackInfoReturnable<ItemStack> cir){
+        if (index < this.mainInventory.length) {
+            return;
+        }
+        ContainerInventory cont = (ContainerInventory)(Object) this;
+        ItemStack itemStack = this.armorInventory[index - this.mainInventory.length];
+        if(itemStack != null && itemStack.getItem() instanceof IAccessory){
+            ((IAccessoryEffects) itemStack.getItem()).removeEffect(player, itemStack);
+        }
+    }
+
+
+    /**
+     * @reason 7.3_04 currently handles left click and drop differently from shift clicking.
+     * To guarantee that the effect of the accessories is correctly remove on shift clicking
+     * a mixin is needed into setItem. - Redart15
+     * */
     @Inject(method = "setItem", at = @At("HEAD"))
-    public void onSetInventoryItem(int index, ItemStack newItem, CallbackInfo ci) {
+    public void updateEffects(int index, ItemStack newItem, CallbackInfo ci) {
         if (index < this.mainInventory.length) {
             return;
         }
@@ -62,11 +95,11 @@ public class ContainerInventoryMixinIncArmorInv {
 
         // this is only called when we SWAP an item
         if (oldItem != null && oldItem.getItem() instanceof IAccessoryEffects) {
-            ((IAccessoryEffects) oldItem.getItem()).onAccessorySwapped(player, oldItem);
+            ((IAccessoryEffects) oldItem.getItem()).removeEffect(player, oldItem);
         }
 
         if (newItem != null && newItem.getItem() instanceof IAccessoryEffects) {
-            ((IAccessoryEffects) newItem.getItem()).onAccessoryAdded(player, newItem);
+            ((IAccessoryEffects) newItem.getItem()).addEffect(player, newItem);
         }
     }
 }
---
+++ src/main/java/teamport/aether/mixin/accessory/ScreenContainerAbstractMixinTargetAccessory.java
@@ -0,0 +1,46 @@
+package teamport.aether.mixin.accessory;
+
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.gui.container.ScreenContainerAbstract;
+import net.minecraft.client.player.controller.PlayerController;
+import net.minecraft.core.InventoryAction;
+import net.minecraft.core.entity.player.Player;
+import net.minecraft.core.item.Item;
+import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.player.inventory.menu.MenuInventory;
+import net.minecraft.core.player.inventory.slot.Slot;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import teamport.aether.items.accessory.IAccessory;
+
+@Mixin(value = ScreenContainerAbstract.class, remap = false)
+public class ScreenContainerAbstractMixinTargetAccessory {
+
+    @WrapOperation(
+            method = "clickInventory",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/player/controller/PlayerController;handleInventoryMouseClick(ILnet/minecraft/core/InventoryAction;[ILnet/minecraft/core/entity/player/Player;)Lnet/minecraft/core/item/ItemStack;"
+            )
+    )
+    private ItemStack adjustTargetForAccessory(
+            PlayerController instance,
+            int containerID,
+            InventoryAction action,
+            int[] args,
+            Player player,
+            Operation<ItemStack> original
+    ) {
+        ScreenContainerAbstract screen = (ScreenContainerAbstract) (Object) this;
+        int slotId = args[0];
+        Slot slot = screen.inventorySlots.getSlot(slotId);
+        ItemStack stackInSlot = slot != null ? slot.getItemStack() : null;
+        Item itemInSlot = stackInSlot != null ? stackInSlot.getItem() : null;
+        if (screen.inventorySlots instanceof MenuInventory && itemInSlot instanceof IAccessory) {
+            args[1] = 2;
+        }
+        return original.call(instance, containerID, action, args, player);
+    }
+}
