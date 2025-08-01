e57e87bb3ee00a5b5736ea8ca1091610d23536eb
Redart15 <redart15@users.noreply.github.com>
2025-08-01 02:31:41 +0200

    split the trinkets and pendants

---
+++ src/main/java/teamport/aether/items/accessory/ItemTrinket.java
@@ -0,0 +1,23 @@
+package teamport.aether.items.accessory;
+
+import net.minecraft.core.data.tag.Tag;
+import net.minecraft.core.item.Item;
+import teamport.aether.items.AetherItemTags;
+import java.util.Set;
+
+public class ItemTrinket extends Item implements IAccessory {
+    private final String name;
+    public Set<Integer> slotIDs;
+
+    public ItemTrinket(String translationKey, String namespaceId, int id, String name) {
+        super(translationKey, namespaceId, id);
+        this.name = name;
+        this.maxStackSize = 1;
+        this.withTags(new Tag[]{AetherItemTags.TRINKET});
+    }
+
+    @Override
+    public String name() {
+        return name;
+    }
+}
