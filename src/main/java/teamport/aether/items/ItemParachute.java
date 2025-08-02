2669078d8f9cbf95f16797551a328ab9caf29ac6
LukeisStuff <lukeisawesome03@gmail.com>
2025-08-02 13:00:44 -0400

    improved on clouds, no more jittering made element hitbox bigger and cleaned up

--- src/main/java/teamport/aether/items/ItemParachute.java
+++ src/main/java/teamport/aether/items/ItemParachute.java
@@ -22,7 +22,7 @@ public class ItemParachute extends Item {
                 if (!gold) {
                     EntityParachute cloud = new EntityParachute(world);
                     cloud.spawnInit();
-                    cloud.absMoveTo(entityplayer.x, entityplayer.y - 1, entityplayer.z, (entityplayer.yRot), (entityplayer.xRot));
+                    cloud.absMoveTo(entityplayer.x, entityplayer.y - 2, entityplayer.z, (entityplayer.yRot), (entityplayer.xRot));
                     world.spawnParticle("explode", entityplayer.x + 0.5, entityplayer.y + 1, entityplayer.z + 0.5, 0.0, 0.0, 0.0, 0);
                     entityplayer.startRiding(cloud);
                     world.entityJoinedWorld(cloud);
@@ -31,7 +31,7 @@ public class ItemParachute extends Item {
                 } else {
                     EntityParachuteGold cloud = new EntityParachuteGold(world);
                     cloud.spawnInit();
-                    cloud.absMoveTo(entityplayer.x, entityplayer.y - 1, entityplayer.z, (entityplayer.yRot), (entityplayer.xRot));
+                    cloud.absMoveTo(entityplayer.x, entityplayer.y - 2, entityplayer.z, (entityplayer.yRot), (entityplayer.xRot));
                     world.spawnParticle("explode", entityplayer.x + 0.5, entityplayer.y + 1, entityplayer.z + 0.5, 0.0, 0.0, 0.0, 0);
                     entityplayer.startRiding(cloud);
                     world.entityJoinedWorld(cloud);
