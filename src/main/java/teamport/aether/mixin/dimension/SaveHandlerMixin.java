8879159a459b6789ef931fae8a7ed0e599e6a541
Oly <Oly@yamums.house>
2025-08-02 21:43:07 -0300

    add dungeonmap

---
+++ src/main/java/teamport/aether/mixin/dimension/SaveHandlerMixin.java
@@ -0,0 +1,63 @@
+package teamport.aether.mixin.dimension;
+
+import com.mojang.nbt.tags.CompoundTag;
+import com.mojang.nbt.tags.Tag;
+import net.minecraft.core.world.save.DimensionData;
+import net.minecraft.core.world.save.ISaveFormat;
+import net.minecraft.core.world.save.LevelStorage;
+import net.minecraft.core.world.save.SaveHandlerBase;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import teamport.aether.AetherMod;
+import teamport.aether.helper.BlockCoordinate;
+import teamport.aether.world.AetherDimension;
+
+@Mixin(value =  SaveHandlerBase.class, remap = false)
+public abstract class SaveHandlerMixin implements LevelStorage {
+
+    @Shadow
+    @Final
+    ISaveFormat saveFormat;
+
+    @Shadow @Final
+    String worldDirName;
+
+    @Inject(method = "getDimensionData", at = @At("HEAD"))
+    public void getDimensionData(int dimensionId, CallbackInfoReturnable<DimensionData> cir) {
+        if (dimensionId != AetherDimension.AetherDimensionID) {
+            return;
+        }
+        AetherMod.LOGGER.info("Loading additional level data.");
+        CompoundTag data = saveFormat.getDimensionDataRaw(worldDirName, dimensionId);
+        if (data != null) {
+            AetherDimension.dungeonMap.clear();
+
+            for (Tag<?> tag: data.getCompound("aether.dungeon").getValues()) {
+                if(tag instanceof CompoundTag) {
+                    AetherDimension.dungeonMap.put(
+                        Integer.parseInt(tag.getTagName()),
+                        BlockCoordinate.fromCompoundTag(((CompoundTag) tag))
+                    );
+                }
+            }
+        }
+    }
+
+    @Inject(method = "saveDimensionDataRaw", at = @At("HEAD"))
+    public void saveDimensionDataRaw(int dimensionId, CompoundTag dimensionDataTag, CallbackInfo ci) {
+        if (dimensionId != AetherDimension.AetherDimensionID) {
+            return;
+        }
+
+        AetherMod.LOGGER.info("Saving additional level data.");
+        CompoundTag dungeonMapNBT = new CompoundTag();
+        AetherDimension.dungeonMap.forEach( (id, coords) -> dungeonMapNBT.put(String.valueOf(id), coords.toCompoundTag()));
+        dimensionDataTag.putCompound("aether.dungeon", dungeonMapNBT);
+    }
+
+}
