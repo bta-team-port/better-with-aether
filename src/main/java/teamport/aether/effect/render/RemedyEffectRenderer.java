60e0df83868b91f6d2bb8b171328081a796d0527
Oly <Oly@yamums.house>
2025-08-03 22:49:08 -0300

    fix visibility problems

--- src/main/java/teamport/aether/effect/render/RemedyEffectRenderer.java
+++ src/main/java/teamport/aether/effect/render/RemedyEffectRenderer.java
@@ -1,5 +1,7 @@
 package teamport.aether.effect.render;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.option.GameSettings;
 import net.minecraft.client.render.tessellator.Tessellator;
 import org.lwjgl.opengl.GL11;
 import sunsetsatellite.catalyst.effects.api.effect.EffectStack;
@@ -10,24 +12,44 @@ public class RemedyEffectRenderer implements EffectRenderer {
     // TODO a circle that cleans the screen from center to edge of the effect
     @Override
     public void drawEffect(int width, int height, EffectStack effectStack, IHudVisibility effect) {
-        float alpha = calcAlpha(effectStack);
-        int tint = effect.getTint();
-        float r = (float)(tint >> 16 & 0xFF) / 255.0f;
-        float g = (float)(tint >> 8 & 0xFF) / 255.0f;
-        float b = (float)(tint & 0xFF) / 255.0f;
-        Tessellator tessellator = Tessellator.instance;
-        GL11.glEnable(3042);
-        GL11.glDisable(3553);
-        GL11.glBlendFunc(770, 771);
-        GL11.glColor4f(r, g, b, alpha);
-        tessellator.startDrawingQuads();
-        tessellator.addVertex(0, height, 0.0);
-        tessellator.addVertex(width, height, 0.0);
-        tessellator.addVertex(width, 0, 0.0);
-        tessellator.addVertex(0, 0, 0.0);
-        tessellator.draw();
-        GL11.glEnable(3553);
-        GL11.glDisable(3042);
+        Minecraft mc = Minecraft.getMinecraft();
+        if (mc.gameSettings.vignette.value) {
+            GL11.glPushMatrix();
+            GL11.glEnable(GL11.GL_BLEND);
+            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+
+            mc.textureManager.bindTexture(mc.textureManager.loadTexture("/assets/aether/textures/other/curevignette.png"));
+            GL11.glColor4f(1.0F, 1.0F, 1.0F, calcAlpha(effectStack));
+
+            Tessellator t = Tessellator.instance;
+            t.startDrawingQuads();
+            t.addVertexWithUV(0, 0, 100, 0, 0);
+            t.addVertexWithUV(0, height, 100, 0, 1);
+            t.addVertexWithUV(width, height, 100, 1, 1);
+            t.addVertexWithUV(width, 0, 100, 1, 0);
+            t.draw();
+            GL11.glPopMatrix();
+
+        } else {
+            float alpha = calcAlpha(effectStack);
+            int tint = effect.getTint();
+            float r = (float) (tint >> 16 & 0xFF) / 255.0f;
+            float g = (float) (tint >> 8 & 0xFF) / 255.0f;
+            float b = (float) (tint & 0xFF) / 255.0f;
+            Tessellator tessellator = Tessellator.instance;
+            GL11.glEnable(3042);
+            GL11.glDisable(3553);
+            GL11.glBlendFunc(770, 771);
+            GL11.glColor4f(r, g, b, alpha);
+            tessellator.startDrawingQuads();
+            tessellator.addVertex(0, height, 0.0);
+            tessellator.addVertex(width, height, 0.0);
+            tessellator.addVertex(width, 0, 0.0);
+            tessellator.addVertex(0, 0, 0.0);
+            tessellator.draw();
+            GL11.glEnable(3553);
+            GL11.glDisable(3042);
+        }
     }
 
     public void drawEffectK(int width, int height, EffectStack effectStack, IHudVisibility effect) {
@@ -46,6 +68,6 @@ public class RemedyEffectRenderer implements EffectRenderer {
 
     private static float calcAlpha(EffectStack effectStack) {
         float percent = (float)effectStack.getTimeLeft() / (float)(effectStack.getDuration());
-        return 0.65F * percent * percent;
+        return 0.35F * percent * percent;
     }
 }
