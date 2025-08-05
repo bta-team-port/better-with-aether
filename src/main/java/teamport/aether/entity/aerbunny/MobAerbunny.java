8569df0ac3253db0056544198398e60956742f6b
LukeisStuff <lukeisawesome03@gmail.com>
2025-08-05 10:08:06 -0400

    aerbunnys no longer freeze when on the head sometimes, and have been cleaned up more to remove pointless broken ai

--- src/main/java/teamport/aether/entity/aerbunny/MobAerbunny.java
+++ src/main/java/teamport/aether/entity/aerbunny/MobAerbunny.java
@@ -14,20 +14,15 @@ import net.minecraft.core.util.collection.NamespaceID;
 import net.minecraft.core.util.helper.DamageType;
 import net.minecraft.core.util.helper.MathHelper;
 import net.minecraft.core.world.World;
-import net.minecraft.core.world.pathfinder.Path;
 import org.jetbrains.annotations.NotNull;
 import teamport.aether.entity.MobAetherAnimal;
 import teamport.aether.items.AetherItemTags;
 import teamport.aether.mixin.accessors.EntityAccessor;
 import teamport.aether.mixin.accessors.MobAccessor;
 
-import java.util.List;
-
 public class MobAerbunny extends MobAetherAnimal {
     public boolean grab;
-    public boolean fear;
     public boolean gotRider;
-    public Entity runFrom;
     public float puffiness;
     public MobAerbunny(World world) {
         super(world);
@@ -49,14 +44,8 @@ public class MobAerbunny extends MobAetherAnimal {
     }
 
     public void tick() {
-        if (this.gotRider) {
-            this.gotRider = false;
-            if (this.vehicle == null) {
-                Player entityplayer = (Player) this.findPlayerToRunFrom();
-                if (entityplayer != null && this.distanceTo(entityplayer) < 2.0F && entityplayer.passenger == null) {
-                    this.startRiding(entityplayer);
-                }
-            }
+        if (this.passenger != null) {
+            gotRider = true;
         }
 
         if (this.puffiness > 0.0F) {
@@ -73,86 +62,59 @@ public class MobAerbunny extends MobAetherAnimal {
 
     public void addAdditionalSaveData(@NotNull CompoundTag tag) {
         super.addAdditionalSaveData(tag);
-        tag.putBoolean("Fear", this.fear);
-        if (this.passenger != null) {
-            this.gotRider = true;
-        }
-        tag.putBoolean("GotRider", this.gotRider);
+        tag.putBoolean("GotRider", gotRider);
     }
 
     public void readAdditionalSaveData(@NotNull CompoundTag tag) {
         super.readAdditionalSaveData(tag);
-        this.fear = tag.getBoolean("Fear");
-        this.gotRider = tag.getBoolean("GotRider");
+        gotRider = tag.getBoolean("GotRider");
     }
 
     public void onLivingUpdate() {
-        int i;
-        if (this.onGround) {
-            if (this.moveForward != 0.0F) {
-                this.jump();
-            }
-        } else if (this.vehicle != null) {
+        if (onGround && this.moveForward != 0.0F) {
+            this.jump();
+        }
+
+        if (this.vehicle != null) {
             if (this.vehicle.isRemoved()) {
                 this.startRiding(this.vehicle);
-            } else if (!this.vehicle.getPassenger().onGround && !this.vehicle.getPassenger().isInWaterOrRain()) {
-                ((EntityAccessor) this.vehicle).setFallDistance(0.0F);
-                Entity var10000 = (Entity) this.vehicle;
-                var10000.yd += 0.05000000074505806;
-                if (((Entity) this.vehicle).yd < -0.22499999403953552 && this.vehicle instanceof Mob && ((MobAccessor) this.vehicle).getJumping()) {
-                    ((Mob) this.vehicle).yd = 0.125F;
-                    this.cloudPoop();
-                    this.puffiness = 1.15F;
+            } else {
+                Entity passenger = this.vehicle.getPassenger();
+                if (!passenger.onGround && !passenger.isInWaterOrRain()) {
+                    ((EntityAccessor) this.vehicle).setFallDistance(0.0F);
+                    ((Mob) this.vehicle).yd += 0.05F;
+                    if (this.vehicle instanceof Mob && ((Mob) this.vehicle).yd < -0.225F && ((MobAccessor) this.vehicle).getJumping()) {
+                        ((Mob) this.vehicle).yd = 0.125F;
+                        this.cloudPoop();
+                        this.puffiness = 1.15F;
+                    }
                 }
             }
-        } else if (!this.grab) {
+        } else if (!grab) {
             if (this.moveForward != 0.0F) {
-                int j = MathHelper.floor(this.x);
-                i = MathHelper.floor(this.bb.minY);
-                int k1 = MathHelper.floor(this.bb.minY - 0.5);
-                int l1 = MathHelper.floor(this.z);
-                if ((this.world.getBlockId(j, i - 1, l1) != 0 || this.world.getBlockId(j, k1 - 1, l1) != 0) && this.world.getBlockId(j, i + 2, l1) == 0 && this.world.getBlockId(j, i + 1, l1) == 0) {
+                int x = MathHelper.floor(this.x);
+                int y = MathHelper.floor(this.bb.minY);
+                int z = MathHelper.floor(this.z);
+                if ((this.world.getBlockId(x, y - 1, z) != 0 || this.world.getBlockId(x, y - 2, z) != 0) &&
+                        this.world.getBlockId(x, y + 1, z) == 0 && this.world.getBlockId(x, y + 2, z) == 0) {
                     if (this.yd < 0.0) {
                         this.cloudPoop();
                         this.puffiness = 0.9F;
                     }
-
                     this.yd = 0.2;
                 }
             }
-
             if (this.yd < -0.1) {
                 this.yd = -0.1;
             }
         }
 
-        if (!this.grab) {
-            super.onLivingUpdate();
-            if (this.fear && this.random.nextInt(4) == 0) {
-                if (this.runFrom != null) {
-                    this.runLikeHell();
-                    this.world.spawnParticle("splash", this.x, this.y, this.z, 0.0, 0.0, 0.0, 0);
-                    if (!this.hasPath()) {
-                        this.lookAt(this.runFrom, 30.0F, 30.0F);
-                    }
-
-                    if (this.runFrom.removed || this.distanceTo(this.runFrom) > 16.0F) {
-                        this.runFrom = null;
-                    }
-                } else {
-                    this.runFrom = this.findPlayerToRunFrom();
-                }
-            }
-        } else if (this.onGround) {
-            this.grab = false;
+        if (grab && onGround) {
+            grab = false;
             this.world.playSoundAtEntity(null, this, "aether:mob.aerbunny.land", 1.0F, (this.random.nextFloat() - this.random.nextFloat()) * 0.2F + 1.0F);
-            List<Entity> list = this.world.getEntitiesWithinAABBExcludingEntity(this, this.bb.expand(12.0, 12.0, 12.0));
-
-            for (i = 0; i < list.size(); ++i) {
-                Entity entity = list.get(i);
+            for (Entity entity : this.world.getEntitiesWithinAABBExcludingEntity(this, this.bb.expand(12.0, 12.0, 12.0))) {
                 if (entity instanceof MobMonster) {
-                    MobMonster entitymobs = (MobMonster) entity;
-                    entitymobs.setTarget(this);
+                    ((MobMonster) entity).setTarget(this);
                 }
             }
         }
@@ -161,6 +123,7 @@ public class MobAerbunny extends MobAetherAnimal {
             this.jump();
         }
 
+        super.onLivingUpdate();
     }
 
     public void cloudPoop() {
@@ -173,46 +136,17 @@ public class MobAerbunny extends MobAetherAnimal {
 
     public boolean hurt(Entity entity, int i, DamageType type) {
         if (this.vehicle == entity) {
-            return false;
+            return super.hurt(entity, i ,type);
         } else {
-            return super.hurt(entity, i, type);
+            return false;
         }
-    }
-
-    public Entity findPlayerToRunFrom() {
-        Player entityplayer = this.world.getClosestPlayerToEntity(this, 12.0);
-        return entityplayer != null && this.canEntityBeSeen(entityplayer) ? entityplayer : null;
-    }
-
-    public void runLikeHell() {
-        double a = this.x - this.runFrom.x;
-        double b = this.z - this.runFrom.z;
-        double crazy = Math.atan2(a, b);
-        crazy += (double) (this.random.nextFloat() - this.random.nextFloat()) * 0.75;
-        double c = this.x + Math.sin(crazy) * 8.0;
-        double d = this.z + Math.cos(crazy) * 8.0;
-        int x = MathHelper.floor(c);
-        int y = MathHelper.floor(this.bb.minY);
-        int z = MathHelper.floor(d);
-
-        for (int q = 0; q < 16; ++q) {
-            int i = x + this.random.nextInt(4) - this.random.nextInt(4);
-            int j = y + this.random.nextInt(4) - this.random.nextInt(4) - 1;
-            int k = z + this.random.nextInt(4) - this.random.nextInt(4);
-            if (j > 4 && (this.world.getBlockId(i, j, k) == 0 || this.world.getBlockId(i, j, k) == Blocks.BLOCK_SNOW.id()) && this.world.getBlockId(i, j - 1, k) != 0) {
-                Path dogs = this.world.getEntityPathToXYZ(this, i, j, k, 16.0F);
-                this.setPathToEntity(dogs);
-                break;
-            }
-        }
-
     }
 
     public boolean interact(@NotNull Player entityplayer) {
         if (!this.world.isClientSide) {
             if (!entityplayer.isSneaking()) {
                 if (this.vehicle == entityplayer) {
-                    this.grab = false;
+                    grab = false;
                     this.ejectRider();
                     if (this.vehicle != null) {
                         this.vehicle = null;
@@ -225,15 +159,9 @@ public class MobAerbunny extends MobAetherAnimal {
                 }
 
                 this.startRiding(entityplayer);
-                this.grab = true;
+                grab = true;
                 this.world.playSoundAtEntity(null, this, "aether:mob.aerbunny.lift", 1.0F, (this.random.nextFloat() - this.random.nextFloat()) * 0.2F + 1.0F);
-                this.xd = entityplayer.xd * 5.0;
-                this.yd = entityplayer.yd / 2.0 + 0.5;
-                this.zd = entityplayer.zd * 5.0;
                 this.isJumping = false;
-                this.moveForward = 0.0F;
-                this.moveStrafing = 0.0F;
-                this.setPathToEntity(null);
                 return true;
             }
             super.interact(entityplayer);
