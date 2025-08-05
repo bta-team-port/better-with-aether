a4cd3ce05c30a5da0ea9d8395dbef99664b91cd1
Oly <33404595+Olypolyu@users.noreply.github.com>
2025-08-04 22:18:30 -0300

    attempt making entities also fall through for multiplayer

--- src/main/java/teamport/aether/mixin/dimension/MPBumpToOverworldMixin.java
+++ src/main/java/teamport/aether/mixin/dimension/MPBumpToOverworldMixin.java
@@ -1,27 +1,28 @@
 package teamport.aether.mixin.dimension;
 
-import net.minecraft.core.data.registry.Registries;
+import com.mojang.nbt.tags.CompoundTag;
+import net.minecraft.core.entity.Entity;
+import net.minecraft.core.entity.EntityDispatcher;
 import net.minecraft.core.entity.player.Player;
-import net.minecraft.core.net.packet.PacketGameRule;
-import net.minecraft.core.net.packet.PacketPlayerGamemode;
-import net.minecraft.core.net.packet.PacketRespawn;
 import net.minecraft.core.util.helper.DyeColor;
 import net.minecraft.core.world.Dimension;
 import net.minecraft.core.world.World;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.entity.player.PlayerServer;
-import net.minecraft.server.net.handler.PacketHandlerServer;
-import net.minecraft.server.world.WorldServer;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import teamport.aether.AetherMod;
 import teamport.aether.world.AetherDimension;
 
 @Mixin(value = PlayerServer.class, remap = false)
 public abstract class MPBumpToOverworldMixin extends Player {
+    @Shadow public abstract @NotNull String getDisplayName();
+
     public MPBumpToOverworldMixin(World world) {
         super(world);
     }
@@ -36,11 +37,32 @@ public abstract class MPBumpToOverworldMixin extends Player {
         if (teleportDelay < 0 && dimension == AetherDimension.AetherDimensionID && this.y < world.worldType.getMinY() - 10) {
             teleportDelay = 20;
 
+            AetherMod.LOGGER.info(String.format("Sending %s to overworld", getDisplayName()));
+
             MinecraftServer server = MinecraftServer.getInstance();
+
+            CompoundTag passengerNBT = null;
+            if (getPassenger() != null) {
+                Entity p = getPassenger();
+                this.ejectRider();
+
+                passengerNBT = new CompoundTag();
+
+                p.save(passengerNBT);
+                p.remove();
+            }
 
             float scale = Dimension.getCoordScale(AetherDimension.AETHER, Dimension.OVERWORLD);
             moveTo(x * scale, 600, z * scale, yRot, xRot);
             server.playerList.sendPlayerToOtherDimension((PlayerServer) (Object) this, Dimension.OVERWORLD.id, DyeColor.BLUE, false);
+
+            if (passengerNBT != null) {
+                Entity p = EntityDispatcher.createEntityFromNBT(passengerNBT, world);
+                p.moveTo(x, y, z, 0f, 0f);
+                world.entityJoinedWorld(p);
+
+                p.startRiding(this);
+            }
         }
     }
 }
