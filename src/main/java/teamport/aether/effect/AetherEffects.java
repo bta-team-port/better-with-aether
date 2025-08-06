ba05e5e9c7a39ce150cc833a2c56b728049702c3
Redart15 <redart15@users.noreply.github.com>
2025-08-06 14:37:21 +0200

    added the ability to lock the effects container to prevent certain effects from applying

--- src/main/java/teamport/aether/effect/AetherEffects.java
+++ src/main/java/teamport/aether/effect/AetherEffects.java
@@ -2,15 +2,48 @@ package teamport.aether.effect;
 
 import net.minecraft.core.entity.player.Player;
 import sunsetsatellite.catalyst.effects.api.effect.*;
-import sunsetsatellite.catalyst.effects.api.modifier.Modifier;
 import teamport.aether.gui.IHudVisibility;
 
-import java.util.List;
+import javax.annotation.Nullable;
+import java.util.*;
 
 import static teamport.aether.AetherMod.MOD_ID;
 
 
 public class AetherEffects {
+
+    protected static class LookupLooks {
+        public static final LookupLooks instance = new LookupLooks();
+        public final Map<Effect, Effect> locker = new HashMap<>();
+        public final Map<Effect, HashSet<Effect>> lockedEffects = new HashMap<>();
+
+        public void addEntry(Effect getLocked, Effect lock){
+            this.locker.put(getLocked, lock);
+            if(this.lockedEffects.containsKey(lock)){
+                HashSet<Effect> effects = this.lockedEffects.get(lock);
+                effects.add(getLocked);
+                return;
+            }
+            HashSet<Effect> effects = new HashSet<>();
+            effects.add(getLocked);
+            this.lockedEffects.put(lock, effects);
+        }
+
+        public @Nullable Effect getLocker(Effect id){
+            return this.locker.getOrDefault(id,null);
+        }
+
+        public @Nullable HashSet<Effect> getLockedEffects(Effect id){
+            return this.lockedEffects.getOrDefault(id, null);
+        }
+
+        public Map<Effect, Effect> getLockerMap(){
+            return this.locker;
+        }
+        public Map<Effect, HashSet<Effect>> getLockedEffectsMap(){
+            return this.lockedEffects;
+        }
+    }
 
     public static PoisonEffect poisonEffect;
     public static RemedyEffect remedyEffect;
@@ -26,6 +59,7 @@ public class AetherEffects {
     private static void initializeItems() {
         assigneEffects();
         registerEffects();
+        registerLocks();
     }
 
     /**
@@ -63,6 +97,11 @@ public class AetherEffects {
         Effects.getInstance().register(remedyEffect.id, remedyEffect);
     }
 
+    private static void registerLocks() {
+        LookupLooks.instance.addEntry(poisonEffect, remedyEffect);
+    }
+
+
     public static EffectStack resolveDominantEffect(Player player) {
         EffectStack dominant = null;
         for (EffectStack effectStack : ((IHasEffects) player).getContainer().getEffects()) {
@@ -77,18 +116,23 @@ public class AetherEffects {
     }
 
 
-    public static void fixedAdd(IHasEffects player, Effect newEffect, int amount) {
-        for(EffectStack effect : player.getContainer().getEffects()) {
+    public static void fixedAdd(IHasEffects entity, Effect newEffect, int amount) {
+        for(EffectStack effect : entity.getContainer().getEffects()) {
             if (effect.getEffect() == newEffect) {
                 if(effect.getAmount() + amount >= effect.getEffect().getMaxStack()){
                     amount = effect.getEffect().getMaxStack() - effect.getAmount();
-                    effect.add(amount, player.getContainer());
+                    effect.add(amount, entity.getContainer());
                     return;
                 }
             }
         }
-        EffectStack stack = new EffectStack(player, newEffect, amount);
-        stack.start(player.getContainer());
-        player.getContainer().add(stack);
+        Effect lock = LookupLooks.instance.getLocker(newEffect);
+        if(lock != null && entity.getContainer().hasEffect(lock) && lock instanceof ILockInteractable){
+            ((ILockInteractable) lock).lockTriggered(entity);
+            return;
+        }
+        EffectStack stack = new EffectStack(entity, newEffect, amount);
+        stack.start(entity.getContainer());
+        entity.getContainer().add(stack);
     }
 }
