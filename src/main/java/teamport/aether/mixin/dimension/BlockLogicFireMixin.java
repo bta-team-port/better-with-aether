576ae9a5f01db0893e9af18c8d37d9618dcc9e3d
LukeisStuff <lukeisawesome03@gmail.com>
2025-08-06 15:13:34 -0400

    fixed fire mixin, fixed minicloud not removing when dead, other minor cleanup

--- src/main/java/teamport/aether/mixin/dimension/BlockLogicFireMixin.java
+++ src/main/java/teamport/aether/mixin/dimension/BlockLogicFireMixin.java
@@ -3,13 +3,19 @@ package teamport.aether.mixin.dimension;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.BlockLogic;
 import net.minecraft.core.block.BlockLogicFire;
+import net.minecraft.core.block.Blocks;
 import net.minecraft.core.block.material.Material;
 import net.minecraft.core.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import teamport.aether.world.AetherDimension;
 
 @Mixin(value = BlockLogicFire.class, remap = false)
 public abstract class BlockLogicFireMixin extends BlockLogic {
+
+    @Shadow protected abstract boolean canNeighborCatchFire(World world, int x, int y, int z);
+
+    @Shadow protected abstract int getBurnResultId(World world, int x, int y, int z);
 
     public BlockLogicFireMixin(Block<?> block, Material material) {
         super(block, material);
@@ -20,6 +26,15 @@ public abstract class BlockLogicFireMixin extends BlockLogic {
         if (world.dimension == AetherDimension.AETHER) {
             world.setBlock(x, y, z, 0);
             return;
+        }
+
+        if (world.getBlockId(x, y - 1, z) != Blocks.OBSIDIAN.id() || !Blocks.PORTAL_NETHER.getLogic().tryToCreatePortal(world, x, y, z, null)) {
+            if (!world.isBlockNormalCube(x, y - 1, z) && !this.canNeighborCatchFire(world, x, y, z)) {
+                world.setBlockWithNotify(x, y, z, this.getBurnResultId(world, x, y, z));
+            } else {
+                world.scheduleBlockUpdate(x, y, z, this.block.id(), this.tickDelay());
+            }
+
         }
 
         super.onBlockPlacedByWorld(world, x, y, z);
