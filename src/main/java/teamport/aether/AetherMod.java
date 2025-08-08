67342493a7174f989ec07bc6174252dc522a168b
LukeisStuff <lukeisawesome03@gmail.com>
2025-08-08 01:12:00 -0400

    silver fireflies spawn in the aether now and can be collected

--- src/main/java/teamport/aether/AetherMod.java
+++ src/main/java/teamport/aether/AetherMod.java
@@ -56,15 +56,14 @@ public class AetherMod implements GameStartEntrypoint, ModInitializer {
 
     @Override
     public void beforeGameStart() {
-
-        SILVER = register(new MobFireflyCluster.FireflyColor(10, "fireflySilver", new Biome[]{AetherDimension.AETHER_PLAINS}, new float[]{0.5F, 1.0F, 0.88F}));
-
         AetherConfig.Setup();
         AetherEntities.init();
         AetherBlocks.init();
         AetherItems.init();
         AetherDimension.init();
         AetherEffects.init();
+
+        SILVER = register(new MobFireflyCluster.FireflyColor(10, "fireflySilver", new Biome[]{AetherDimension.AETHER_PLAINS}, new float[]{0.5F, 1.0F, 0.88F}));
 
         NetEntityHandler.registerNetworkEntry(new NetEntryLightning(), 32);
         NetEntityHandler.registerNetworkEntry(new NetEntryAetherProjectile(), 35);
