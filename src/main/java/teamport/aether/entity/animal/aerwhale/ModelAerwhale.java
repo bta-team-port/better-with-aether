34db17ee3911b79447e15bfdba3b612511dfc1e4
LukeisStuff <lukeisawesome03@gmail.com>
2025-08-09 03:25:46 -0400

    aerwhale visual overhaul

--- src/main/java/teamport/aether/entity/animal/aerwhale/ModelAerwhale.java
+++ src/main/java/teamport/aether/entity/animal/aerwhale/ModelAerwhale.java
@@ -23,16 +23,22 @@ public class ModelAerwhale extends ModelBase {
 
     public ModelAerwhale() {
         this.frontBody = new Cube(20, 0);
-        this.frontBody.addBox(-3.5F, -3.5F, -12.5F, 7, 6, 10);
+        this.frontBody.addBox(-3.5F, -3.5F, -12.5F, 7, 5, 10);
+        this.frontPleats = new Cube(30, 21);
+        this.frontPleats.addBox(-3.5F, 1.5F, -12.5F, 7, 1, 10, 1/3f);
 
         this.middleBody = new Cube(0, 0);
-        this.middleBody.addBox(-2.5F, -2.5F, -2.5F, 5, 5, 5);
+        this.middleBody.addBox(-2.5F, -2.8F, -2.5F, 5, 5, 5);
+        this.middlePleats = new Cube(37, 26);
+        this.middlePleats.addBox(-2.5F, 1.5F, -2.5F, 5, 1, 5, 1/3f);
 
         this.backBody = new Cube(0, 10);
         this.backBody.addBox(-1.5F, -1.5F, 2.5F, 3, 3, 4);
+        this.backPleats = new Cube(40, 27);
+        this.backPleats.addBox(-1.5F, 1.5F, 2.5F, 3, 1, 4, 1/3f);
 
-        this.topFin = new Cube(4, 17);
-        this.topFin.addBox(-0.5F, -4.25F, -1.5F, 1, 2, 3);
+        this.topFin = new Cube(21, 0);
+        this.topFin.addBox(-0.5F, -4.5F, -1.8F, 1, 2, 3);
         this.topFin.setRotationAngle(-0.125f, 0.0f, 0.0f);
 
         this.backLeftFin = new Cube(4, 18);
@@ -53,8 +59,14 @@ public class ModelAerwhale extends ModelBase {
     public void render(float limbSwing, float limbYaw, float limbPitch, float headYaw, float headPitch, float scale) {
         this.setupAnimation(limbSwing, limbYaw, limbPitch, headYaw, headPitch, scale);
         this.frontBody.render(scale);
+        this.frontPleats.render(scale);
+
         this.middleBody.render(scale);
+        this.middlePleats.render(scale);
+
         this.backBody.render(scale);
+        this.backPleats.render(scale);
+
 
         this.topFin.render(scale);
 
