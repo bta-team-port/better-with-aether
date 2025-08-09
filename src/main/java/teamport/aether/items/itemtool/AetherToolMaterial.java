70ac9c1b467901b7b4f6c7227d5b123a8216cbe3
Oly <Oly@yamums.house>
2025-08-09 15:29:20 -0300

    fix zanite tools; tweak numbers

--- src/main/java/teamport/aether/items/itemtool/AetherToolMaterial.java
+++ src/main/java/teamport/aether/items/itemtool/AetherToolMaterial.java
@@ -5,8 +5,8 @@ import net.minecraft.core.item.material.ToolMaterial;
 public class AetherToolMaterial {
     public static final ToolMaterial SKYROOT = new ToolMaterial().setDurability(64).setEfficiency(2.0F, 4.0f).setMiningLevel(0);
     public static final ToolMaterial HOLYSTONE = new ToolMaterial().setDurability(128).setEfficiency(4.0F, 6.0F).setMiningLevel(1);
-    public static final ToolMaterial ZANITE = new ToolMaterial().setDurability(384).setEfficiency(6.0F, 12.0F).setMiningLevel(2);
-    public static final ToolMaterial GRAVITITE = new ToolMaterial().setDurability(1536).setEfficiency(10.0F, 25.0F).setMiningLevel(3);
+    public static final ToolMaterial ZANITE = new ToolMaterial().setDurability(384).setEfficiency(8.0F, 12.0F).setMiningLevel(2);
+    public static final ToolMaterial GRAVITITE = new ToolMaterial().setDurability(1536).setEfficiency(12.0F, 25.0F).setMiningLevel(3);
     public static final ToolMaterial VALKYRIE = new ToolMaterial().setDurability(768).setEfficiency(12.0f, 35.0f).setMiningLevel(3).setDamage(4).setBlockHitDelay(4);
     public static final ToolMaterial SPECIAL = new ToolMaterial().setDurability(256).setEfficiency(5.0f, 7.0f).setMiningLevel(2).setDamage(2);
 }
--- src/main/java/teamport/aether/items/itemtool/ItemToolZanite/ItemToolSwordZanite.java
+++ src/main/java/teamport/aether/items/itemtool/ItemToolZanite/ItemToolSwordZanite.java
@@ -1,8 +1,10 @@
 package teamport.aether.items.itemtool.ItemToolZanite;
 
+import net.minecraft.core.block.Block;
 import net.minecraft.core.entity.Entity;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.item.material.ToolMaterial;
+import teamport.aether.blocks.AetherBlockTags;
 import teamport.aether.items.itemtool.AetherToolMaterial;
 import teamport.aether.items.itemtool.ItemToolSwordAether;
 
@@ -23,6 +25,18 @@ public class ItemToolSwordZanite extends ItemToolSwordAether {
         float starting_damage = (float) super.getDamageVsEntity(entity, is);
         float ending_damage = starting_damage * factor;
         return Math.round(starting_damage * (1.0F - durability_progress) + (ending_damage * durability_progress));
+    }
+
+    @Override
+    public float getStrVsBlock(ItemStack itemstack, Block<?> block) {
+        if (!block.hasTag(AetherBlockTags.MINEABLE_BY_AETHER_SWORD)) return 1.0F;
+        float durability_progress = ((float) itemstack.getMetadata() / this.getMaxDamage());
+
+        // we will 'lerp' between the starting efficiency and the unused 'haste' efficiency of tools
+        float base_efficiency = this.material.getEfficiency(false);
+        float haste_efficiency = this.material.getEfficiency(true);
+
+        return  base_efficiency + (haste_efficiency - base_efficiency) * durability_progress;
     }
 
 }
