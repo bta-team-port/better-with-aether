3cdbfef9d0a4b88020a7c299ded91713b9b1ca1c
LukeisStuff <lukeisawesome03@gmail.com>
2025-08-08 20:20:16 -0400

    made shield better, fixed aerbunnies being damaged when riding and invincible when not

--- src/main/java/teamport/aether/items/accessory/trinket/ItemRepulsionShield.java
+++ src/main/java/teamport/aether/items/accessory/trinket/ItemRepulsionShield.java
@@ -22,25 +22,31 @@ public class ItemRepulsionShield extends ItemShield {
     public void inventoryTick(ItemStack stack, World world, Entity entity, int slotId, boolean flag) {
         // First, we get the player and their held stack.
         Player player = (Player) entity;
-        if (slotId < player.inventory.mainInventory.length || slotId - player.inventory.mainInventory.length < TRINKET_1_SLOT)
+        if (slotId < player.inventory.mainInventory.length || slotId - player.inventory.mainInventory.length < TRINKET_1_SLOT) {
             return;
+        }
 
         // Checks for player movement - First is if the player is moving,
         // second is if the player is jumping/falling. If either is too
         // high, we return.
-        double velocity = MathHelper.sqrt(player.xd * player.xd + player.zd * player.zd);
 
-        if (velocity > 0.075D || !player.onGround) return;
+        double velocity = MathHelper.sqrt(player.xd * player.xd + player.zd * player.zd);
+        if (!player.isSneaking() && (!player.onGround || velocity > 0.075D)) {
+            return;
+        }
 
         // Now we do the same as above, just without a cooldown.
         List<Projectile> projectiles = world.getEntitiesWithinAABB(Projectile.class, player.bb.grow(1.25D, 1.25D, 1.25D));
-        if (projectiles.isEmpty()) return;
-        for (Projectile projectile : projectiles) {
-            if (projectile.owner == player) continue;
+        if (projectiles.isEmpty()) {
+            return;
+        }
 
+        for (Projectile projectile : projectiles) {
+            if (projectile.owner == player) {
+                continue;
+            }
             projectile.xd = -projectile.xd;
             projectile.zd = -projectile.zd;
-            break;
         }
     }
 }
