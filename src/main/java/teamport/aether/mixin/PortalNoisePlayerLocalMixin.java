a765a6d7d4290d497bd25bf96262181ea0f34d24
LukeisStuff <lukeisawesome03@gmail.com>
2025-08-14 17:06:23 -0400

    added portal noises for travel and trigger

---
+++ src/main/java/teamport/aether/mixin/PortalNoisePlayerLocalMixin.java
@@ -0,0 +1,29 @@
+package teamport.aether.mixin;
+
+import net.minecraft.client.entity.player.PlayerLocal;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import teamport.aether.blocks.AetherBlocks;
+
+@Mixin(value = PlayerLocal.class, remap = false)
+public class PortalNoisePlayerLocalMixin {
+    @ModifyArg(method = "onLivingUpdate()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/sound/SoundEngine;playSound(Ljava/lang/String;Lnet/minecraft/core/sound/SoundCategory;FF)V", ordinal = 0), index = 0)
+
+    private String modifyPortalTriggerSound(String originalSound) {
+        PlayerLocal player = (PlayerLocal)(Object)this;
+        if (player.portalID == AetherBlocks.PORTAL_AETHER.id()) {
+            return "aether:trigger";
+        }
+        return originalSound;
+    }
+
+    @ModifyArg(method = "onLivingUpdate()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/sound/SoundEngine;playSound(Ljava/lang/String;Lnet/minecraft/core/sound/SoundCategory;FF)V", ordinal = 1), index = 0)
+    private String modifyPortalTravelSound(String originalSound) {
+        PlayerLocal player = (PlayerLocal)(Object)this;
+        if (player.portalID == AetherBlocks.PORTAL_AETHER.id()) {
+            return "aether:travel";
+        }
+        return originalSound;
+    }
+}
