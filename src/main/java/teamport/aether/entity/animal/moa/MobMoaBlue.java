f7b55ff5cb336dfbade1f447f34e119f2f04ba8b
Oly <Oly@yamums.house>
2025-08-16 16:56:30 -0300

    made moas work on a server!

--- src/main/java/teamport/aether/entity/animal/moa/MobMoaBlue.java
+++ src/main/java/teamport/aether/entity/animal/moa/MobMoaBlue.java
@@ -1,6 +1,7 @@
 package teamport.aether.entity.animal.moa;
 
 import com.mojang.nbt.tags.CompoundTag;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.WeightedRandomLootObject;
 import net.minecraft.core.entity.animal.Creature;
 import net.minecraft.core.entity.player.Player;
@@ -16,8 +17,12 @@ import teamport.aether.entity.AetherJumpAmount;
 import teamport.aether.entity.animal.MobAetherAnimal;
 import teamport.aether.items.AetherItemTags;
 import teamport.aether.items.AetherItems;
+
 import teamport.aether.mixin.accessors.EntityAccessor;
-import teamport.aether.mixin.accessors.MobAccessor;
+import teamport.aether.net.message.AetherRideableNetworkMessage;
+import turniplabs.halplibe.helper.EnvironmentHelper;
+import turniplabs.halplibe.helper.network.NetworkHandler;
+
 
 public class MobMoaBlue extends MobAetherAnimal implements Creature, AetherJumpAmount, AetherRideable {
     public float flap = 0.0F;
@@ -30,6 +35,13 @@ public class MobMoaBlue extends MobAetherAnimal implements Creature, AetherJumpA
     public int jumpsRemaining;
     public boolean jumpPressed;
     public boolean tamed;
+
+
+    double xdChange = 0;
+    double zdChange = 0;
+
+    boolean playerUsedJump = false;
+
 
     public MobMoaBlue(@Nullable World world) {
         super(world);
@@ -116,6 +128,105 @@ public class MobMoaBlue extends MobAetherAnimal implements Creature, AetherJumpA
             this.eggTimer = this.random.nextInt(6000) + 6000;
         }
 
+        vehicleMovement();
+    }
+
+    @Override
+    public void controlEntity(float moveForward, float moveStrafe, boolean isJumping, float xRot, float yRot) {
+        if (EnvironmentHelper.isClientWorld()) {
+            NetworkHandler.sendToServer(
+                new AetherRideableNetworkMessage(moveForward, moveStrafe, isJumping, xRot, yRot)
+            );
+        }
+
+        float yawDeg = (float) (yRot * (Math.PI/180));
+        float step = 0.175F;
+
+        if (moveForward > 0.1F) {
+            xdChange += (double) moveForward * -Math.sin(yawDeg) * step;
+            zdChange += (double) moveForward * Math.cos(yawDeg) * step;
+
+        } else if (moveForward < -0.1F) {
+            xdChange += (double) moveForward * -Math.sin(yawDeg) * step;
+            zdChange += (double) moveForward * Math.cos(yawDeg) * step;
+        }
+
+        if (moveStrafe > 0.1F) {
+            xdChange += (double) moveStrafe * Math.cos(yawDeg) * step;
+            zdChange += (double) moveStrafe * Math.sin(yawDeg) * step;
+
+        } else if (moveStrafe < -0.1F) {
+            xdChange += (double) moveStrafe * Math.cos(yawDeg) * step;
+            zdChange += (double) moveStrafe * Math.sin(yawDeg) * step;
+        }
+
+        if (isJumping && !jumpPressed) {
+            playerUsedJump = true;
+            jumpPressed = true;
+        }
+
+        if (this.jumpPressed && !isJumping) {
+            this.jumpPressed = false;
+        }
+    }
+
+    public void vehicleMovement() {
+        if (passenger == null || !(this.passenger instanceof Player)) return;
+        Player player = (Player) this.passenger;
+
+        player.handleSpecialVehicleControl();
+
+        xd += xdChange;
+        zd += zdChange;
+        xdChange = 0.0;
+        zdChange = 0.0;
+
+        if (playerUsedJump) {
+            boolean canJump = this.jumpsRemaining > 0;
+
+            if (this.onGround) {
+                yd = 1.4;
+                this.onGround = false;
+            }
+            else {
+                if (isInWater()) yd = 0.5;
+                else if (canJump) yd = 1.2;
+                --this.jumpsRemaining;
+            }
+
+            if (isInWater() || canJump) {
+                world.playSoundAtEntity(null, this, "aether:mob.wingflap", 2.0f, 1.0f);
+            }
+        }
+
+        playerUsedJump = false;
+
+        player.sendSpecialVehiclePacket();
+
+        double horizontalSpeed = Math.abs(Math.sqrt(this.xd * this.xd + this.zd * this.zd));
+        if (horizontalSpeed > 0.375) {
+            double normal = 0.375 / horizontalSpeed;
+            this.xd *= normal;
+            this.zd *= normal;
+        }
+    }
+
+    public void updateAI() {
+        if (this.passenger != null && this.passenger instanceof Player) {
+            this.moveSpeed = 0.0F;
+            this.moveStrafing = 0.0F;
+            this.isJumping = false;
+            this.footSize = 1.5f;
+
+            this.yRotO = this.yRot = this.passenger.yRot;
+            this.xRotO = this.xRot = this.passenger.xRot;
+
+            Player player = (Player) this.passenger;
+            ((EntityAccessor) player).setFallDistance(0.0F);
+        } else {
+            this.footSize = 1.0f;
+            super.updateAI();
+        }
     }
 
     public void onGround() {
@@ -126,75 +237,6 @@ public class MobMoaBlue extends MobAetherAnimal implements Creature, AetherJumpA
 
     public double getRideHeight() {
         return this.bbHeight - 0.6f;
-    }
-
-    public void updateAI() {
-        if (this.passenger != null && this.passenger instanceof Player) {
-            this.moveSpeed = 0.0F;
-            this.moveStrafing = 0.0F;
-            this.isJumping = false;
-            this.footSize = 1.5f;
-
-
-            this.yRotO = this.yRot = this.passenger.yRot;
-            this.xRotO = this.xRot = this.passenger.xRot;
-
-            Player player = (Player) this.passenger;
-            ((EntityAccessor) player).setFallDistance(0.0F);
-
-            float playerYawDegrees = (float) (player.yRot * (Math.PI/180));
-            float step = 0.175F;
-
-            if (((MobAccessor) player).getForwardVelocity() > 0.1F) {
-                this.xd += (double) ((MobAccessor) player).getForwardVelocity() * -Math.sin(playerYawDegrees) * step;
-                this.zd += (double) ((MobAccessor) player).getForwardVelocity() * Math.cos(playerYawDegrees) * step;
-
-            } else if (((MobAccessor) player).getForwardVelocity() < -0.1F) {
-                this.xd += (double) ((MobAccessor) player).getForwardVelocity() * -Math.sin(playerYawDegrees) * step;
-                this.zd += (double) ((MobAccessor) player).getForwardVelocity() * Math.cos(playerYawDegrees) * step;
-            }
-
-            if (((MobAccessor) player).getHorizontalVelocity() > 0.1F) {
-                this.xd += (double) ((MobAccessor) player).getHorizontalVelocity() * Math.cos(playerYawDegrees) * step;
-                this.zd += (double) ((MobAccessor) player).getHorizontalVelocity() * Math.sin(playerYawDegrees) * step;
-
-            } else if (((MobAccessor) player).getHorizontalVelocity() < -0.1F) {
-                this.xd += (double) ((MobAccessor) player).getHorizontalVelocity() * Math.cos(playerYawDegrees) * step;
-                this.zd += (double) ((MobAccessor) player).getHorizontalVelocity() * Math.sin(playerYawDegrees) * step;
-            }
-
-            if (((MobAccessor) player).getJumping() && !jumpPressed) {
-                boolean canJump = this.jumpsRemaining > 0;
-
-                if (this.onGround) {
-                    this.yd = 1.4;
-                    this.onGround = false;
-                } else {
-                    if (isInWater()) this.yd = 0.5;
-                    else if (canJump) this.yd = 1.2;
-                    --this.jumpsRemaining;
-                }
-
-                if (isInWater() || canJump) {
-                    world.playSoundAtEntity(null, this, "aether:mob.wingflap", 2.0f, 1.0f);
-                    this.jumpPressed = true;
-                }
-            }
-
-            if (this.jumpPressed && !((MobAccessor) player).getJumping()) {
-                this.jumpPressed = false;
-            }
-
-            double horizontalSpeed = Math.abs(Math.sqrt(this.xd * this.xd + this.zd * this.zd));
-            if (horizontalSpeed > 0.375) {
-                double normal = 0.375 / horizontalSpeed;
-                this.xd *= normal;
-                this.zd *= normal;
-            }
-        } else {
-            this.footSize = 1.0f;
-            super.updateAI();
-        }
     }
 
     @Override
---
+++ src/main/java/teamport/aether/net/message/AetherRideableNetworkMessage.java
@@ -0,0 +1,54 @@
+package teamport.aether.net.message;
+
+import net.minecraft.core.entity.player.Player;
+import org.jetbrains.annotations.NotNull;
+import teamport.aether.entity.AetherRideable;
+import turniplabs.halplibe.helper.EnvironmentHelper;
+import turniplabs.halplibe.helper.network.NetworkMessage;
+import turniplabs.halplibe.helper.network.UniversalPacket;
+
+public class AetherRideableNetworkMessage implements NetworkMessage {
+    float moveForward, moveStrafe;
+    float xRot, yRot;
+    boolean isJumping;
+
+    public AetherRideableNetworkMessage() {}
+
+    public AetherRideableNetworkMessage(float moveForward, float moveStrafe, boolean isJumping, float xRot, float yRot) {
+        this.moveForward = moveForward;
+        this.moveStrafe = moveStrafe;
+        this.isJumping = isJumping;
+        this.xRot = xRot;
+        this.yRot = yRot;
+    }
+
+    @Override
+    public void encodeToUniversalPacket(@NotNull UniversalPacket packet) {
+        packet.writeDouble(moveForward);
+        packet.writeDouble(moveStrafe);
+        packet.writeBoolean(isJumping);
+        packet.writeDouble(xRot);
+        packet.writeDouble(yRot);
+    }
+
+    @Override
+    public void decodeFromUniversalPacket(@NotNull UniversalPacket packet) {
+        this.moveForward = (float) packet.readDouble();
+        this.moveStrafe = (float) packet.readDouble();
+        this.isJumping = packet.readBoolean();
+        this.xRot = (float) packet.readDouble();
+        this.yRot = (float) packet.readDouble();
+    }
+
+    @Override
+    public void handle(NetworkContext networkContext) {
+        if (EnvironmentHelper.isServerEnvironment()) {
+            Player player = networkContext.player;
+
+            if (player.vehicle instanceof AetherRideable) {
+                AetherRideable vehicle = (AetherRideable) player.vehicle;
+                vehicle.controlEntity(moveForward, moveStrafe, isJumping, xRot, yRot);
+            }
+        }
+    }
+}
