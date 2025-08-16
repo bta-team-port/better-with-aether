650932f0cd843ed360a616918b48fc2bacc59014
Oly <Oly@yamums.house>
2025-08-16 16:56:30 -0300

    made moas work on a server!

--- src/main/java/teamport/aether/entity/AetherRideable.java
+++ src/main/java/teamport/aether/entity/AetherRideable.java
@@ -1,4 +1,6 @@
 package teamport.aether.entity;
 
 public interface AetherRideable {
+
+     void controlEntity(float movingForward, float moveStrafing, boolean isJumping, float xRot, float yRot);
 }
