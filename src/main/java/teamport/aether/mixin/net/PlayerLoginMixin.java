0c8655ddb10763464a51729a6409fe13c2362174
Oly <Oly@yamums.house>
2025-08-15 23:56:01 -0300

    got the clock thing to sync across the network!

---
+++ src/main/java/teamport/aether/mixin/net/PlayerLoginMixin.java
@@ -0,0 +1,32 @@
+package teamport.aether.mixin.net;
+
+import net.minecraft.core.net.NetworkManager;
+import net.minecraft.core.net.packet.PacketLogin;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.entity.player.PlayerServer;
+import net.minecraft.server.net.handler.PacketHandlerLogin;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import teamport.aether.AetherServer;
+
+@Mixin(value = PacketHandlerLogin.class, remap = false)
+public class PlayerLoginMixin {
+
+    @Shadow @Final private MinecraftServer mcServer;
+
+    @Shadow public NetworkManager netManager;
+
+    @Inject(method = {"doLogin"}, at = {@At(value = "TAIL")})
+    public void doLogin(PacketLogin loginPacket, CallbackInfo ci) {
+        PlayerServer player = this.mcServer.playerList.getPlayerEntity(loginPacket.username);
+
+        if (player != null) {
+            AetherServer.onPlayerJoinedServer(player, this.netManager);
+        }
+    }
+
+}
