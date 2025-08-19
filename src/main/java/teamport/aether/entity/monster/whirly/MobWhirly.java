6e8b9324c3d790550bc35b9926c1ac9598a33023
Redart15 <redart15@users.noreply.github.com>
2025-08-20 00:02:26 +0200

    interface name changes, lang adjustemnt, lang for projectile deaths

--- src/main/java/teamport/aether/entity/monster/whirly/MobWhirly.java
+++ src/main/java/teamport/aether/entity/monster/whirly/MobWhirly.java
@@ -14,14 +14,14 @@ import net.minecraft.core.world.World;
 import org.jetbrains.annotations.NotNull;
 import teamport.aether.blocks.AetherBlockTags;
 import teamport.aether.blocks.AetherBlocks;
-import teamport.aether.entity.AetherTranslatableDeathMessage;
+import teamport.aether.entity.AetherDeathMessage;
 import teamport.aether.entity.monster.MobMonsterAether;
 import teamport.aether.items.AetherItems;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MobWhirly extends MobMonsterAether implements Enemy, AetherTranslatableDeathMessage {
+public class MobWhirly extends MobMonsterAether implements Enemy, AetherDeathMessage {
     public int entcount = 0;
     public int Life;
     public List<Particle> fluffies;
