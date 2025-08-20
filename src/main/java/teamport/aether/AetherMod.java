913eb7aa60c33661dfb6091fe6262a65f39bec4c
Oly <Oly@yamums.house>
2025-08-19 23:23:09 -0300

    nothing fucking works i want to kill myself

--- src/main/java/teamport/aether/AetherMod.java
+++ src/main/java/teamport/aether/AetherMod.java
@@ -38,6 +38,7 @@ import teamport.aether.items.AetherItems;
 import teamport.aether.items.accessory.ItemTrinket;
 import teamport.aether.net.NetEntryAetherProjectile;
 import teamport.aether.net.NetEntryLightning;
+import teamport.aether.net.NetEntrySlider;
 import teamport.aether.net.message.AetherRideableNetworkMessage;
 import teamport.aether.net.message.SunspiritDeathNetworkMessage;
 import teamport.aether.world.AetherDimension;
@@ -80,6 +81,7 @@ public class AetherMod implements GameStartEntrypoint, ModInitializer {
 
         NetEntityHandler.registerNetworkEntry(new NetEntryLightning(), 32);
         NetEntityHandler.registerNetworkEntry(new NetEntryAetherProjectile(), 35);
+        NetEntityHandler.registerNetworkEntry(new NetEntrySlider());
 
         SoundTypes.loadSoundsJson(MOD_ID);
     }
