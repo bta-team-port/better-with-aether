f9f4a16c9e1cec3a3942f79f70d3c4b721e9a3c3
Oly <Oly@yamums.house>
2025-08-20 22:12:06 -0300

    sync boss list from server

--- src/main/java/teamport/aether/AetherMod.java
+++ src/main/java/teamport/aether/AetherMod.java
@@ -40,6 +40,7 @@ import teamport.aether.net.NetEntryAetherProjectile;
 import teamport.aether.net.NetEntryLightning;
 import teamport.aether.net.NetEntrySlider;
 import teamport.aether.net.message.AetherRideableNetworkMessage;
+import teamport.aether.net.message.BossListNetworkMessage;
 import teamport.aether.net.message.SunspiritDeathNetworkMessage;
 import teamport.aether.world.AetherDimension;
 import turniplabs.halplibe.helper.network.NetworkHandler;
@@ -66,6 +67,7 @@ public class AetherMod implements GameStartEntrypoint, ModInitializer {
         LOGGER.info(" Welcome to a hostile paradise. Version {} {}", state , versionString);
         NetworkHandler.registerNetworkMessage(SunspiritDeathNetworkMessage::new);
         NetworkHandler.registerNetworkMessage(AetherRideableNetworkMessage::new);
+        NetworkHandler.registerNetworkMessage(BossListNetworkMessage::new);
     }
 
     @Override
