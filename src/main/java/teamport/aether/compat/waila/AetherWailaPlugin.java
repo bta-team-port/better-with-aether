713c95290ea066b9072d1c9d9a7b9416914167cf
Oly <Oly@yamums.house>
2025-08-24 20:28:41 -0300

    add wail plugin

---
+++ src/main/java/teamport/aether/compat/waila/AetherWailaPlugin.java
@@ -0,0 +1,17 @@
+package teamport.aether.compat.waila;
+
+import net.minecraft.core.player.inventory.container.Container;
+import org.slf4j.Logger;
+import teamport.aether.entity.tile.TileEntityMimic;
+import toufoumaster.btwaila.entryplugins.waila.BTWailaCustomTooltipPlugin;
+import toufoumaster.btwaila.entryplugins.waila.BTWailaPlugin;
+import toufoumaster.btwaila.tooltips.TileTooltip;
+import toufoumaster.btwaila.tooltips.TooltipRegistry;
+
+public class AetherWailaPlugin implements BTWailaCustomTooltipPlugin {
+    @Override
+    public void initializePlugin(TooltipRegistry tooltipRegistry, Logger logger) {
+        TileTooltip<Container> inventory = BTWailaPlugin.INVENTORY;
+        inventory.addClass(TileEntityMimic.class);
+    }
+}
