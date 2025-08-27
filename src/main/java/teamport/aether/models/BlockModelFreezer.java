9b32297e287cfd8c7dc7d1ced20a101f231c3255
oly <33404595+Olypolyu@users.noreply.github.com>
2025-08-27 20:22:27 -0300

    fix freezer rotation  (#445)
    * yeah. Turns out this was the reason world gen was slow.
    
    * oopsies daisies
    
    * oopsies daisies x2
    
    * fix freezer rotation
    
    * error out when can't find dungeon
    
    ---------
    
    Co-authored-by: Oly <Oly@yamums.house>

--- src/main/java/teamport/aether/models/BlockModelFreezer.java
+++ src/main/java/teamport/aether/models/BlockModelFreezer.java
@@ -2,18 +2,22 @@ package teamport.aether.models;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.render.block.model.BlockModelRotatable;
+import net.minecraft.client.render.block.model.BlockModelStandard;
+import net.minecraft.client.render.tessellator.Tessellator;
 import net.minecraft.client.render.texture.stitcher.IconCoordinate;
 import net.minecraft.client.render.texture.stitcher.TextureRegistry;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.BlockLogic;
 import net.minecraft.core.player.inventory.container.Container;
+import net.minecraft.core.util.helper.Direction;
 import net.minecraft.core.util.helper.Side;
 import net.minecraft.core.util.helper.Sides;
 import net.minecraft.core.world.WorldSource;
+import teamport.aether.blocks.BlockLogicFreezer;
 
 @Environment(EnvType.CLIENT)
-public class BlockModelFreezer<T extends BlockLogic> extends BlockModelRotatable<T> {
+public class BlockModelFreezer<T extends BlockLogic> extends BlockModelStandard<T> {
+
     public BlockModelFreezer(Block<T> block) {
         super(block);
     }
@@ -22,18 +26,52 @@ public class BlockModelFreezer<T extends BlockLogic> extends BlockModelRotatable
     public IconCoordinate getBlockTexture(WorldSource blockAccess, int x, int y, int z, Side side) {
         int data = blockAccess.getBlockMetadata(x, y, z);
         int index = Sides.orientationLookUpHorizontal[6 * Math.min(data, 5) + side.getId()];
+
         if (index == Side.TOP.getId()) {
             IconCoordinate texture = this.blockTextures.get(Side.TOP);
             Container container = (Container) blockAccess.getTileEntity(x, y, z);
+
             if (container != null) {
                 boolean hasOutput = container.getItem(2) != null;
                 if (hasOutput) {
                     return TextureRegistry.getTexture(texture.namespaceId.namespace() + ":block/" + texture.namespaceId.value() + "_filled");
                 }
             }
+
             return texture;
         }
+
         return this.blockTextures.get(side.getId());
     }
 
+    @Override
+    public boolean render(Tessellator tessellator, int x, int y, int z) {
+        int meta = renderBlocks.blockAccess.getBlockMetadata(x, y, z);
+        Direction direction = BlockLogicFreezer.getDirectionFromMeta(meta);
+
+        switch (direction) {
+            case NORTH:
+                renderBlocks.uvRotateTop = 3;
+                break;
+
+            case EAST:
+                renderBlocks.uvRotateTop = 2;
+                break;
+
+            case SOUTH:
+                renderBlocks.uvRotateTop = 0;
+                break;
+
+            case WEST:
+                renderBlocks.uvRotateTop = 1;
+                break;
+
+            default:
+                break;
+        }
+
+        this.renderStandardBlock(tessellator, this.block.getBounds(), x, y, z);
+        this.resetRenderBlocks();
+        return true;
+    }
 }
