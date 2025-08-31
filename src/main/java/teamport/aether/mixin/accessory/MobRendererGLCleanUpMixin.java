a45f155c055808045609c389363d69b093967f5c
Redart15 <redart15@users.noreply.github.com>
2025-08-31 15:44:07 +0200

    move file and removed unneeded depth mask restoration

--- src/main/java/teamport/aether/mixin/accessory/MobRendererGLCleanUpMixin.java
+++ src/main/java/teamport/aether/mixin/accessory/MobRendererGLCleanUpMixin.java
@@ -1,9 +1,8 @@
-package teamport.aether.mixin;
+package teamport.aether.mixin.accessory;
 
 import net.minecraft.client.render.entity.MobRenderer;
 import net.minecraft.client.render.tessellator.Tessellator;
 import net.minecraft.core.entity.Mob;
-import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,19 +13,16 @@ import teamport.aether.helper.GLManager;
 public abstract class MobRendererGLCleanUpMixin<T extends Mob> {
     @Inject(method = "render(Lnet/minecraft/client/render/tessellator/Tessellator;Lnet/minecraft/core/entity/Mob;DDDFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/model/ModelBase;render(FFFFFF)V", shift = At.Shift.AFTER, ordinal = 2))
     public void restoreGLState2(Tessellator tessellator, T entity, double x, double y, double z, float yaw, float partialTick, CallbackInfo ci) {
-        GL11.glDepthMask(true);
         GLManager.restore();
     }
 
     @Inject(method = "render(Lnet/minecraft/client/render/tessellator/Tessellator;Lnet/minecraft/core/entity/Mob;DDDFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/model/ModelBase;render(FFFFFF)V", shift = At.Shift.AFTER, ordinal = 5))
     public void restoreGLState5(Tessellator tessellator, T entity, double x, double y, double z, float yaw, float partialTick, CallbackInfo ci) {
-        GL11.glDepthMask(true);
         GLManager.restore();
     }
 
     @Inject(method = "render(Lnet/minecraft/client/render/tessellator/Tessellator;Lnet/minecraft/core/entity/Mob;DDDFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/model/ModelBase;render(FFFFFF)V", shift = At.Shift.AFTER, ordinal = 8))
     public void restoreGLState8(Tessellator tessellator, T entity, double x, double y, double z, float yaw, float partialTick, CallbackInfo ci) {
-        GL11.glDepthMask(true);
         GLManager.restore();
     }
 }
