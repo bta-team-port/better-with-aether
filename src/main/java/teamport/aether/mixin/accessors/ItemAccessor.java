555f7248c473a139ddd0be6c1091965a61962bfa
Redart15 <redart15@users.noreply.github.com>
2025-08-31 03:54:03 +0200

    make accessor to random of item

---
+++ src/main/java/teamport/aether/mixin/accessors/ItemAccessor.java
@@ -0,0 +1,14 @@
+package teamport.aether.mixin.accessors;
+
+import net.minecraft.core.item.Item;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Random;
+@Mixin(value = Item.class, remap = false)
+public interface ItemAccessor {
+
+    @Accessor("itemRand")
+    Random getItemRand();
+
+}
