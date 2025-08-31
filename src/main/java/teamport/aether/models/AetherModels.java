8f68e9aaa3bf31d6d698d2db730b031e8ba03dcc
Oly <Oly@yamums.house>
2025-08-31 14:24:00 -0300

    some changes to the deathray

--- src/main/java/teamport/aether/models/AetherModels.java
+++ src/main/java/teamport/aether/models/AetherModels.java
@@ -573,7 +573,7 @@ public class AetherModels implements ModelEntrypoint {
 
 
         dispatcher.addDispatch((new ItemModelBlock((ItemBlock<?>) AetherBlocks.TORCH_AMBROSIUM.asItem())).setFullBright());
-        dispatcher.addDispatch((new ItemModelStandard(AetherItems.DEATH_RAY, null)).setIcon("aether:item/stick_skyroot").setFullBright());
+            dispatcher.addDispatch((new ItemModelStandard(AetherItems.DEATH_RAY, null)).setIcon("minecraft:item/wand_monster").setFullBright());
 
     }
 
