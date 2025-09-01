7104679e94acaa4aae859d4f1c9f1a33627acad1
Redart15 <redart15@users.noreply.github.com>
2025-09-01 15:40:04 +0200

    adjusted world feature point

--- src/main/java/teamport/aether/world/generate/feature/components/WorldFeaturePoint.java
+++ src/main/java/teamport/aether/world/generate/feature/components/WorldFeaturePoint.java
@@ -6,6 +6,8 @@ import net.minecraft.core.util.helper.MathHelper;
 import net.minecraft.core.util.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
+
 public class WorldFeaturePoint {
     public int x;
     public int y;
@@ -33,9 +35,6 @@ public class WorldFeaturePoint {
         );
     }
 
-    public void move() {
-    }
-
     public void rotateFixPointYAxis(int fixPointX, int fixPointY, int fixPointZ, float angle) {
         Vec3 vec = Vec3.getPermanentVec3(this.x - fixPointX, this.y - fixPointY, this.z - fixPointZ);
         vec.rotateAroundY(MathHelper.toRadians(angle));
@@ -47,6 +46,9 @@ public class WorldFeaturePoint {
     public void rotateFixPointXAxis(int fixPointX, int fixPointY, int fixPointZ, float angle) {
         Vec3 vec = Vec3.getPermanentVec3(this.x - fixPointX, this.y - fixPointY, this.z - fixPointZ);
         vec.rotateAroundX(MathHelper.toRadians(angle));
+        this.x = (int) Math.round(vec.x + fixPointX);
+        this.y = (int) Math.round(vec.y + fixPointY);
+        this.z = (int) Math.round(vec.z + fixPointZ);
     }
 
     public void rotateFixPointZAxis(int fixPointX, int fixPointY, int fixPointZ, float angle) {
@@ -56,22 +58,6 @@ public class WorldFeaturePoint {
         this.y = (int) Math.round(vec.y + fixPointY);
         this.z = (int) Math.round(vec.z + fixPointZ);
     }
-
-    public void rotateSkewCenterYAxis(int fixPointX, int fixPointY, int fixPointZ, float angle) {
-        int dx = this.x - fixPointX;
-        int dy = this.y - fixPointY;
-        int dz = this.z - fixPointZ;
-        dx = (int) Math.round(dx - Math.tan(angle / 2) * dz);
-        dz = (int) Math.round(dx * Math.sin(angle) + dz);
-        dx = (int) Math.round(dx - Math.tan(angle / 2) * dz);
-        this.x = dx + fixPointX;
-        this.y = dy + fixPointY;
-        this.z = dz + fixPointZ;
-    }
-
-    public void pointMirror(int fixPointX, int fixPointY, int fixPointZ) {
-    }
-
 
     public CompoundTag toCompoundTag() {
         CompoundTag result = new CompoundTag();
@@ -90,11 +76,16 @@ public class WorldFeaturePoint {
         );
     }
 
-    public boolean equals(@Nullable WorldFeaturePoint obj) {
-        if (obj == null) return false;
+    @Override
+    public boolean equals(@Nullable Object o) {
+        if (o == null) return false;
+        if (!(o instanceof WorldFeaturePoint)) return false;
+        WorldFeaturePoint wfp = (WorldFeaturePoint) o;
+        return wfp.x == this.x && wfp.y == this.y && wfp.z == this.z;
+    }
 
-        return  obj.x == x &&
-                obj.y == y &&
-                obj.z == z;
+    @Override
+    public int hashCode(){
+        return Objects.hash(this.x, this.y, this.z);
     }
 }
