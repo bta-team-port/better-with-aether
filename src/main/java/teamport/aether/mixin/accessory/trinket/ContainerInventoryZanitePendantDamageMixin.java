67b9dae38c51ef4e0c619d0972d1e23f3596fe81
Redart15 <redart15@users.noreply.github.com>
2025-09-03 23:50:18 +0200

    zanite now increases damage vs entities by at most 1 heart per pendant

---
+++ src/main/java/teamport/aether/mixin/accessory/trinket/ContainerInventoryZanitePendantDamageMixin.java
@@ -0,0 +1,37 @@
+package teamport.aether.mixin.accessory.trinket;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.core.entity.player.Player;
+import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.player.inventory.container.ContainerInventory;
+import net.minecraft.core.util.helper.MathHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import teamport.aether.items.AetherItems;
+
+import static teamport.aether.items.accessory.SlotAccessory.*;
+
+@Mixin(value = ContainerInventory.class, remap = false)
+public class ContainerInventoryZanitePendantDamageMixin {
+    @Shadow public ItemStack[] mainInventory;
+    @Shadow public Player player;
+
+    @ModifyReturnValue(method = "getDamageVsEntity", at = @At("RETURN"))
+    public int getGloveDamage(int damage){
+        ItemStack trinketOne = player.inventory.armorInventory[TRINKET_1_SLOT];
+        ItemStack trinketTwo = player.inventory.armorInventory[TRINKET_2_SLOT];
+        if (trinketOne != null && trinketOne.itemID == AetherItems.ARMOR_TALISMAN_ZANITE.id) {
+            float damagePercent = (float) trinketOne.getMetadata() / trinketOne.getMaxDamage();
+            float speed = MathHelper.lerp(0.0F, 3.0F, damagePercent);
+            damage += (int)Math.floor(speed);
+        }
+        if (trinketTwo != null && trinketTwo.itemID == AetherItems.ARMOR_TALISMAN_ZANITE.id) {
+            float damagePercent = (float) trinketTwo.getMetadata() / trinketTwo.getMaxDamage();
+            float speed = MathHelper.lerp(0.0F, 3.0F, damagePercent);
+            damage += (int)Math.floor(speed);
+        }
+        return damage;
+    }
+
+}
