8a5edba37633bd23abdff5f54d3cafc911e3b1ba
Redart15 <redart15@users.noreply.github.com>
2025-09-04 16:57:08 +0200

    made that pendant only looses 1 charge per block traveled, adn only one pendant takes damages at a time

--- src/main/java/teamport/aether/items/accessory/pendant/ItemIcePendant.java
+++ src/main/java/teamport/aether/items/accessory/pendant/ItemIcePendant.java
@@ -41,29 +41,39 @@ public class ItemIcePendant extends ItemPendant {
         int x = MathHelper.ceil(hits.x);
         int y = MathHelper.ceil(hits.y);
         int z = MathHelper.ceil(hits.z);
+        int proc = 0;
         for (int radius = -1; radius <= 1; radius++) {
             for (int depth = -1; depth <= 1; depth++) {
                 int xPos = x + radius;
                 int zPos = z + depth;
                 if (material == Material.water) {
-                    damagePendant(stack, player);
+                    proc++;
                     world.setBlockWithNotify(xPos, y, zPos, Blocks.ICE.id());
                 } else if (material == Material.lava) {
+                    proc++;
                     world.setBlockWithNotify(xPos, y, zPos, Blocks.OBSIDIAN.id());
-                    damagePendant(stack, player);
                 }
             }
+        }
+        if(proc > 0){
+            damagePendant(stack, player);
         }
     }
 
     private void damagePendant(ItemStack stack, Player player) {
         stack.damageItem(1, player);
         if (player.inventory.armorInventory[TRINKET_1_SLOT] == stack) {
-            if (stack.stackSize <= 0) player.inventory.armorInventory[TRINKET_1_SLOT] = null;
+            if (stack.stackSize <= 0) {
+                player.inventory.armorInventory[TRINKET_1_SLOT] = null;
+                return;
+            }
         }
 
         if (player.inventory.armorInventory[TRINKET_2_SLOT] == stack) {
-            if (stack.stackSize <= 0) player.inventory.armorInventory[TRINKET_2_SLOT] = null;
+            if (stack.stackSize <= 0) {
+                player.inventory.armorInventory[TRINKET_2_SLOT] = null;
+                return;
+            }
         }
     }
 }
