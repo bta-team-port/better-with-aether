5c0734ec549d62cb1e851e717865618477135e4a
Your Name <you@example.com>
2025-09-08 09:46:16 -0300

    apply bandaids to a few crashes.

---
+++ src/main/java/teamport/aether/mixin/fix/WorldGetCubesMixin.java
@@ -0,0 +1,44 @@
+package teamport.aether.mixin.fix;
+
+import net.minecraft.core.entity.Entity;
+import net.minecraft.core.util.phys.AABB;
+import net.minecraft.core.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static teamport.aether.AetherMod.LOGGER;
+
+@Mixin(value = World.class, remap = false)
+public class WorldGetCubesMixin {
+
+    @Inject(method = "getCubes", at = @At("HEAD"), cancellable = true)
+    public void getCubes(Entity entity, AABB aabb, CallbackInfoReturnable<List<AABB>> cir) {
+        double diffX = Math.abs(aabb.maxX - aabb.minX);
+        double diffY = Math.abs(aabb.maxY - aabb.minY);
+        double diffZ = Math.abs(aabb.maxZ - aabb.minZ);
+
+        if (
+               diffX > 1_000_000
+            || diffY > 1_000_000
+            || diffZ > 1_000_000
+            || Double.isNaN(diffX)
+            || Double.isNaN(diffY)
+            || Double.isNaN(diffZ)
+        ) {
+            LOGGER.error("{} is moving too fast! Please send this to a developer!", Entity.getNameFromEntity(entity, true));
+            Thread.dumpStack();
+
+            entity.absMoveTo(0, 255, 0, 0f, 0f);
+            entity.xo = 0;
+            entity.yo = 0;
+            entity.zo = 0;
+
+            cir.setReturnValue(new ArrayList<>());
+        }
+    }
+}
---
+++ src/main/java/teamport/aether/mixin/fix/WorldTypeOverworldRespawnMixin.java
@@ -0,0 +1,44 @@
+package teamport.aether.mixin.fix;
+
+import net.minecraft.core.world.World;
+import net.minecraft.core.world.save.LevelData;
+import net.minecraft.core.world.type.WorldType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static teamport.aether.AetherMod.LOGGER;
+
+@Mixin(value = WorldType.class, remap = false, priority = 0)
+public abstract class WorldTypeOverworldRespawnMixin {
+
+    @Shadow public abstract int getOceanY();
+
+    @Inject(method = "getRespawnLocation", at=@At("HEAD"), cancellable = true)
+    public void respawnLocation(World world, CallbackInfo ci) {
+        LOGGER.debug("Commandeering respawnLocation...");
+
+        LevelData levelData = world.getLevelData();
+        if (levelData.getSpawnY() <= 0) levelData.setSpawnY(getOceanY());
+
+        int attempts = 0;
+
+        int x = levelData.getSpawnX();
+        int z = levelData.getSpawnZ();
+        while (world.getTopBlock(x, z) == 0) {
+            if (attempts++ > 500) break;
+
+            z += world.rand.nextInt(8) - world.rand.nextInt(8);
+            x += world.rand.nextInt(8) - world.rand.nextInt(8);
+        }
+
+        if (attempts < 500) {
+            levelData.setSpawnX(x);
+            levelData.setSpawnZ(z);
+        }
+
+        ci.cancel();
+    }
+}
