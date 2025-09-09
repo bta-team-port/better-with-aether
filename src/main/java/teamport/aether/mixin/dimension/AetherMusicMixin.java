2ae934c4d8e5e065adb943afd0cd66793a1bc5d7
Oly <33404595+Olypolyu@users.noreply.github.com>
2025-09-09 00:10:15 -0300

    remove horrible reflection hacks.

--- src/main/java/teamport/aether/mixin/dimension/AetherMusicMixin.java
+++ src/main/java/teamport/aether/mixin/dimension/AetherMusicMixin.java
@@ -11,14 +11,10 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import paulscode.sound.SoundSystem;
 import teamport.aether.world.AetherDimension;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.net.URL;
 import java.util.Random;
-
-import static teamport.aether.AetherMod.LOGGER;
 
 @Mixin(value = SoundEngine.class, remap = false)
 public abstract class AetherMusicMixin {
@@ -41,13 +37,15 @@ public abstract class AetherMusicMixin {
     @Final
     private Random random;
 
+    @Shadow
+    private static @Nullable SoundSystem soundSystem;
+
     @Inject(method = "tick", at= @At(value = "INVOKE", target = "Ljava/util/concurrent/locks/Lock;lock()V", shift = At.Shift.AFTER), cancellable = true)
     public void tick(CallbackInfo ci) {
-        if (!(this.isLoaded() && SoundCategoryHelper.getEffectiveVolume(SoundCategory.MUSIC, this.options) != 0.0F))
-            return;
+        if (this.mc.currentWorld.dimension.id == AetherDimension.AetherDimensionID) ci.cancel();
 
-        if (this.mc.currentWorld.dimension.id == AetherDimension.AetherDimensionID) {
-            ci.cancel();
+        if (!(this.isLoaded() && SoundCategoryHelper.getEffectiveVolume(SoundCategory.MUSIC, this.options) != 0.0F)) {
+            return;
         }
 
         if (this.ticksBeforeMusic > 0) {
@@ -55,49 +53,17 @@ public abstract class AetherMusicMixin {
             return;
         }
 
-        // okay, firstly I will do horrible hacks to get the soundsystem via reflection.
-        // so that we don't have to sort out the dependency mess that would be compiling against paulscode.
+        SoundEvent redirect = SoundRepository.SOUNDS.getSoundEvent("aether:music.aether");
+        if (redirect == null) return;
 
-        // I feel like i'm writing C 🥲
-        Object paulsCode;
-        Method backgroundMusic;
-        Method setPitch;
-        Method setVolume;
-        Method play;
+        SoundEntry entry = redirect.getRandomEntry();
+        if (entry == null) return;
 
-       try {
-           Field paulsField = SoundEngine.class.getDeclaredField("soundSystem");
-           paulsField.setAccessible(true);
+        this.ticksBeforeMusic = this.random.nextInt(6000) + 6000;
 
-           paulsCode = paulsField.get(this);
-           backgroundMusic = paulsCode.getClass().getDeclaredMethod("backgroundMusic", String.class, URL.class, String.class, boolean.class);
-           setPitch = paulsCode.getClass().getDeclaredMethod("setPitch", String.class, float.class);
-           setVolume = paulsCode.getClass().getDeclaredMethod("setVolume", String.class, float.class);
-           play = paulsCode.getClass().getDeclaredMethod("play", String.class);
-       }
-
-       catch (Exception ignored) {
-           LOGGER.error("Failed to commandeer sound system!");
-           Thread.dumpStack();
-           return;
-       }
-
-       try {
-           SoundEvent redirect = SoundRepository.SOUNDS.getSoundEvent("aether:music.aether");
-           if (redirect == null) return;
-
-           SoundEntry entry = redirect.getRandomEntry();
-           if (entry == null) return;
-
-           this.ticksBeforeMusic = this.random.nextInt(6000) + 6000;
-
-           backgroundMusic.invoke(paulsCode, "BgMusic", entry.getURL(), entry.name, false);
-           setPitch.invoke(paulsCode, "BgMusic", entry.pitch);
-           setVolume.invoke(paulsCode, "BgMusic", SoundCategoryHelper.getEffectiveVolume(SoundCategory.MUSIC, this.options) * entry.volume);
-           play.invoke(paulsCode, "BgMusic");
-       }
-
-       catch (Exception ignored) {}
-
+        soundSystem.backgroundMusic("BgMusic", entry.getURL(), entry.name, false);
+        soundSystem.setPitch("BgMusic", entry.pitch);
+        soundSystem.setVolume("BgMusic", SoundCategoryHelper.getEffectiveVolume(SoundCategory.MUSIC, this.options) * entry.volume);
+        soundSystem.play("BgMusic");
     }
 }
