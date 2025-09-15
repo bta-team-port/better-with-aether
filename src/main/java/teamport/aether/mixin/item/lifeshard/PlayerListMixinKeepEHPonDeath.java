65ad6bd79d771dc06703712a2315c449ce9d0250
Redart15 <redart15@users.noreply.github.com>
2025-09-15 15:57:30 +0200

    keeps health after death, does not work with relog

---
+++ src/main/java/teamport/aether/mixin/item/lifeshard/PlayerListMixinKeepEHPonDeath.java
@@ -0,0 +1,20 @@
+package teamport.aether.mixin.item.lifeshard;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.server.entity.player.PlayerServer;
+import net.minecraft.server.net.PlayerList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import teamport.aether.helper.HealthHelper;
+
+@Mixin(value = PlayerList.class)
+
+
+public class PlayerListMixinKeepEHPonDeath {
+    @Inject(method = "recreatePlayerEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/player/inventory/container/ContainerInventory;transferAllContents(Lnet/minecraft/core/player/inventory/container/ContainerInventory;)V"), remap = false)
+    public void keepExtraHealthServer(final PlayerServer previousPlayer, final int i, final CallbackInfoReturnable<PlayerServer> cir, @Local(name = "newPlayer") final PlayerServer newPlayer) {
+        HealthHelper.setExtraHealth(newPlayer, HealthHelper.getExtraHealth(previousPlayer));
+    }
+}
