a6d42e728c850dbefe81311d86ce09f2fe49226f
Oly <33404595+Olypolyu@users.noreply.github.com>
2025-09-15 22:14:35 -0300

    prevent game from freezing with invalid AABB

--- src/main/java/teamport/aether/mixin/fix/WorldGetCubesMixin.java
+++ src/main/java/teamport/aether/mixin/fix/WorldGetCubesMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.core.entity.Entity;
 import net.minecraft.core.util.phys.AABB;
 import net.minecraft.core.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -16,21 +17,56 @@ import static teamport.aether.AetherMod.LOGGER;
 @Mixin(value = World.class, remap = false, priority = 0)
 public class WorldGetCubesMixin {
 
-    @Inject(method = "getCubes", at = @At("HEAD"), cancellable = true)
-    public void getCubes(Entity entity, AABB aabb, CallbackInfoReturnable<List<AABB>> cir) {
+    public boolean isBrokenAABB(AABB aabb) {
         double diffX = Math.abs(aabb.maxX - aabb.minX);
         double diffY = Math.abs(aabb.maxY - aabb.minY);
         double diffZ = Math.abs(aabb.maxZ - aabb.minZ);
 
-        if (
-               diffX > 1_000_000
-            || diffY > 1_000_000
-            || diffZ > 1_000_000
-            || Double.isNaN(diffX)
-            || Double.isNaN(diffY)
-            || Double.isNaN(diffZ)
-        ) {
-            LOGGER.error("{} is moving too fast! Please send this to a developer!", Entity.getNameFromEntity(entity, true));
+        return diffX > 1_000_000
+                || diffY > 1_000_000
+                || diffZ > 1_000_000
+                || Double.isNaN(diffX)
+                || Double.isNaN(diffY)
+                || Double.isNaN(diffZ);
+    }
+
+
+    @Unique
+    public void preventStupidShit(Entity entity, AABB aabb, CallbackInfoReturnable<List<Entity>> cir) {
+        if (isBrokenAABB(aabb)) {
+            if (entity != null) {
+                LOGGER.error("{} is moving too fast! Please send this to a developer!", Entity.getNameFromEntity(entity, true));
+                Thread.dumpStack();
+
+                entity.absMoveTo(0, 255, 0, 0f, 0f);
+                entity.xo = 0;
+                entity.yo = 0;
+                entity.zo = 0;
+            }
+            else {
+                LOGGER.error("Something is moving too fast! Please send this to a developer!");
+                Thread.dumpStack();
+            }
+
+            cir.setReturnValue(new ArrayList<>());
+            cir.cancel();
+        }
+    }
+
+    @Inject(method = "getEntitiesWithinAABB", at = @At("HEAD"), cancellable = true)
+    public <T extends Entity> void getEntitiesWithinAABB(Class<T> ofClass, AABB aabb, CallbackInfoReturnable<List<Entity>> cir) {
+        preventStupidShit(null, aabb, cir);
+    }
+
+    @Inject(method = "getEntitiesWithinAABBExcludingEntity", at = @At("HEAD"), cancellable = true)
+    public void getEntitiesWithinAABBExcludingEntity(Entity entity, AABB aabb, CallbackInfoReturnable<List<Entity>> cir) {
+        preventStupidShit(entity, aabb, cir);
+    }
+
+    @Inject(method = "getCubes", at = @At("HEAD"), cancellable = true)
+    public void getCubes(Entity entity, AABB aabb, CallbackInfoReturnable<List<AABB>> cir) {
+        if (isBrokenAABB(aabb)) {
+            LOGGER.error("{} is moving too fast!! Please send this to a developer!", Entity.getNameFromEntity(entity, true));
             Thread.dumpStack();
 
             entity.absMoveTo(0, 255, 0, 0f, 0f);
