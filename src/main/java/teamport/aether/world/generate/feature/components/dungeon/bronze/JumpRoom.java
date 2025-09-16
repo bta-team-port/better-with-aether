bb0d55c69e4c2e8e352bc00b3d15424dce6030bd
Redart15 <redart15@users.noreply.github.com>
2025-09-16 03:44:23 +0200

    added jumpy room

---
+++ src/main/java/teamport/aether/world/generate/feature/components/dungeon/bronze/JumpRoom.java
@@ -0,0 +1,68 @@
+package teamport.aether.world.generate.feature.components.dungeon.bronze;
+
+import teamport.aether.blocks.AetherBlocks;
+import teamport.aether.helper.Pair;
+import teamport.aether.world.generate.feature.BlockPallet;
+import teamport.aether.world.generate.feature.components.WorldFeatureComponent;
+import teamport.aether.world.generate.feature.components.WorldFeaturePoint;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static net.minecraft.core.util.helper.Direction.*;
+import static net.minecraft.core.util.helper.Direction.EAST;
+import static net.minecraft.core.util.helper.Direction.SOUTH;
+import static net.minecraft.core.util.helper.Direction.UP;
+import static net.minecraft.core.util.helper.Direction.WEST;
+import static teamport.aether.world.generate.feature.components.WorldFeatureComponent.*;
+import static teamport.aether.world.generate.feature.components.WorldFeaturePoint.wfp;
+
+public class JumpRoom extends BaseBronzeRoom{
+    public static BlockPallet chestOrMimic = new BlockPallet();
+    static {
+        chestOrMimic.addEntry(0, 1);
+        chestOrMimic.addEntry(AetherBlocks.CHEST_PLANKS_SKYROOT.id(), 1);
+        chestOrMimic.addEntry(AetherBlocks.CHEST_MIMIC.id(), 1);
+    }
+
+    public JumpRoom(){
+        super();
+        this.height = 15;
+        addDoor(NORTH, wfp(4, 1, 0), UP, 6, EAST, 4);
+        addDoor(EAST, wfp(11, 1, 4), UP, 6, SOUTH, 4);
+        addDoor(SOUTH, wfp(4, 1, 11), UP, 6, EAST, 4);
+        addDoor(WEST, wfp(0, 1, 4), UP, 6, SOUTH, 4);
+    }
+
+    @Override
+    public void makeRoom() {
+        // Shell
+        room.add(drawShell(random, ROOM_PALLET, EAST, 12, UP, height, SOUTH, 12, x, y, z, true));
+        room.add(drawVolume(0, 0, EAST, 10, UP, height - 2, SOUTH, 10, x + 1, y + 1, z + 1, true));
+
+        // Platforms
+        decoration.add(drawPlane(random, ROOM_PALLET, SOUTH, 2, EAST, 4, x + 4, y + 7, z + 1, true));
+        decoration.add(drawPlane(random, ROOM_PALLET, SOUTH, 4, EAST, 2, x + 1, y + 7, z + 4, true));
+        decoration.add(drawPlane(random, ROOM_PALLET, SOUTH, 4, EAST, 2, x + 9, y + 7, z + 4, true));
+        decoration.add(drawPlane(random, ROOM_PALLET, SOUTH, 2, EAST, 4, x + 4, y + 7, z + 9, true));
+
+        // Clouds
+        decoration.add(drawPlane(AetherBlocks.AERCLOUD_BLUE.id(), 0, SOUTH, 2, EAST, 2, x + 1, y, z + 1, true));
+        decoration.add(drawPlane(AetherBlocks.AERCLOUD_BLUE.id(), 0, SOUTH, 2, WEST, 2, x + 10, y, z + 1, true));
+        decoration.add(drawPlane(AetherBlocks.AERCLOUD_BLUE.id(), 0, NORTH, 2, EAST, 2, x + 1, y, z + 10, true));
+        decoration.add(drawPlane(AetherBlocks.AERCLOUD_BLUE.id(), 0, NORTH, 2, WEST, 2, x + 10, y, z + 10, true));
+
+        // Chests
+        List<WorldFeatureComponent> listChestPos = new ArrayList<>();
+        listChestPos.add(drawLine(random, chestOrMimic, EAST, 2, x + 5, y + 8, z + 1, true));
+        listChestPos.add(drawLine(random, chestOrMimic, SOUTH, 2, x + 1, y + 8, z + 5, true));
+        listChestPos.add(drawLine(random, chestOrMimic, SOUTH, 2, x + 10, y + 8, z + 5, true));
+        listChestPos.add(drawLine(random, chestOrMimic, EAST, 2, x + 5, y + 8, z + 10, true));
+        Collections.shuffle(listChestPos, random);
+        int max = random.nextInt(2) + 1;
+        for(int i = 0; i < max; i++){
+            chest.add(listChestPos.get(i));
+        }
+    }
+}
