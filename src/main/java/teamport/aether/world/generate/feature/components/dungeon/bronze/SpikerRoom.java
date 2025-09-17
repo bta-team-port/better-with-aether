522bd2394ae88689102b7b478c7349e0106fac45
Redart15 <redart15@users.noreply.github.com>
2025-09-17 11:45:07 +0200

    adjusted the spiker room to be correctly detected when checking for bounding boxes

--- src/main/java/teamport/aether/world/generate/feature/components/dungeon/bronze/SpikerRoom.java
+++ src/main/java/teamport/aether/world/generate/feature/components/dungeon/bronze/SpikerRoom.java
@@ -13,25 +13,26 @@ public class SpikerRoom extends BaseBronzeRoom {
     public SpikerRoom() {
         super();
         this.height = 13;
-        addDoor(NORTH, wfp(4, 1, 0), UP, 6, EAST, 4);
-        addDoor(EAST, wfp(11, 1, 4), UP, 6, SOUTH, 4);
-        addDoor(SOUTH, wfp(4, 1, 11), UP, 6, EAST, 4);
-        addDoor(WEST, wfp(0, 1, 4), UP, 6, SOUTH, 4);
+        addDoor(NORTH, wfp(4, 2, 0), UP, 6, EAST, 4);
+        addDoor(EAST, wfp(11, 2, 4), UP, 6, SOUTH, 4);
+        addDoor(SOUTH, wfp(4, 2, 11), UP, 6, EAST, 4);
+        addDoor(WEST, wfp(0, 2, 4), UP, 6, SOUTH, 4);
 
         addDoor(DOWN, wfp(5,0,5), EAST, 2, SOUTH, 2);
-        addDoor(UP, wfp(5,12,5), EAST, 2, SOUTH, 2);
+        addDoor(UP, wfp(5,13,5), EAST, 2, SOUTH, 2);
     }
     public void makeShell(){
         room.add(drawShell(random, ROOM_PALLET, SOUTH, width, UP, height, EAST, width, x, y, z, true));
         room.add(drawVolume(0, 0, SOUTH, width - 2, UP, height - 2, EAST, width - 2, x + 1, y + 1, z + 1, true));
     }
     public void makePitBottom(){
-        decoration.add(drawPlane(random, ROOM_PALLET, SOUTH, width - 2, EAST, width - 2, x + 1, y - 1, z + 1, true));
+        decoration.add(drawPlane(random, ROOM_PALLET, SOUTH, width - 2, EAST, width - 2, x + 1, y + 1, z + 1, true));
     }
     public void makeSpikePit(){
-        decoration.add(drawPlane(Blocks.SPIKES.id(), 0, SOUTH, 6, EAST, 6, x + 3, y, z + 3, true));
+        decoration.add(drawPlane(Blocks.SPIKES.id(), 0, SOUTH, 6, EAST, 6, x + 3, y + 1, z + 3, true));
     }
-//    @Override
+
+    @Override
     public void makeRoom() {
         this.makeShell();
         this.makePitBottom();
