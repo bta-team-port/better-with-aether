a8e7a20f82fbf9ba678dfd6b814791b19794482f
Redart15 <redart15@users.noreply.github.com>
2025-09-17 23:05:11 +0200

    some improvements toward the staircase room

--- src/main/java/teamport/aether/world/generate/feature/components/dungeon/bronze/StairwellRoom.java
+++ src/main/java/teamport/aether/world/generate/feature/components/dungeon/bronze/StairwellRoom.java
@@ -1,5 +1,7 @@
 package teamport.aether.world.generate.feature.components.dungeon.bronze;
 
+import net.minecraft.core.util.helper.Direction;
+import net.minecraft.core.util.helper.MathHelper;
 import teamport.aether.blocks.AetherBlocks;
 import teamport.aether.world.generate.feature.components.WorldFeaturePoint;
 
@@ -8,10 +10,11 @@ import static net.minecraft.core.util.helper.Direction.EAST;
 import static net.minecraft.core.util.helper.Direction.SOUTH;
 import static net.minecraft.core.util.helper.Direction.UP;
 import static net.minecraft.core.util.helper.Direction.WEST;
+import static teamport.aether.world.generate.feature.components.WorldFeatureBlock.wfb;
 import static teamport.aether.world.generate.feature.components.WorldFeatureComponent.*;
 import static teamport.aether.world.generate.feature.components.WorldFeaturePoint.wfp;
 
-public class StairwellRoom extends BaseBronzeRoom{
+public class StairwellRoom extends BaseBronzeRoom {
 
     public StairwellRoom() {
         super();
@@ -30,38 +33,21 @@ public class StairwellRoom extends BaseBronzeRoom{
         addDoor(WEST, wfp(0, height - 8, 2), UP, 6, SOUTH, 4);
     }
 
-    private static final WorldFeaturePoint[] steps = {
-            wfp(2, 0, 4),
-            wfp(4, 0, 4),
-            wfp(4, 0, 2),
-            wfp(2, 0, 2),
-    };
-
     @Override
     public void makeRoom() {
-        room.add(drawShell(random, ROOM_PALLET, SOUTH, width, UP, 9, EAST, width, x, y, z, true));
-        room.add(drawVolume(0, 0, SOUTH, width - 2, UP, 7, EAST, width - 2, x + 1, y + 1, z + 1, false));
+        room.add(drawHollowShell(random, ROOM_PALLET, SOUTH, width, UP, 9, EAST, width, x, y, z, true));
+        decoration.add(drawShell(random, ROOM_PALLET, SOUTH, width, UP, 9, EAST, width, x, y + height - 9, z, true));
+        decoration.add(drawVolume(0, 0, SOUTH, width - 2, UP, 7, EAST, width - 2, x + 1, y + 1 + height - 9, z + 1, false));
+        decoration.add(drawSquareCylinder(random, ROOM_PALLET, SOUTH, width - 2, EAST, width - 2, UP, height - 16, x + 1, y + 8, z + 1, false));
 
-        room.add(drawShell(random, ROOM_PALLET, SOUTH, width, UP, 9, EAST, width, x, y + height - 9, z, true));
-        room.add(drawVolume(0, 0, SOUTH, width - 2, UP, 7, EAST, width - 2, x + 1, y + 1 + height - 9, z + 1, false));
-
-        room.add(drawSquareCylinder(random, ROOM_PALLET, SOUTH, width-2, EAST, width-2, UP, height-16, x+1, y+8, z+1, false));
-
-        for (int i = 0; i < height - 8; i++) {
-            WorldFeaturePoint offset = steps[i%4];
-            WorldFeaturePoint stepPosition = wfp(x, y + i, z).add(offset);
-            room.add(
-                drawVolume(
-                    AetherBlocks.SLAB_HOLYSTONE_POLISHED.id(), 1,
-                    SOUTH, 2,
-                    UP, 1,
-                    EAST, 2,
-                    stepPosition.x, stepPosition.y, stepPosition.z,
-                    false
-                )
-            );
+        Direction dir = WEST;
+        WorldFeaturePoint stepPosition = wfp(x + 2, y + 1, z + 5);
+        for (int i = 0; i < (height - 8) << 1; i++) {
+            if ((i % 3) == 0) dir = dir.rotate(1);
+            stepPosition.moveInDirection(dir);
+            decoration.add(wfb(stepPosition.x, stepPosition.y + MathHelper.floor(i / 2.0f), stepPosition.z, AetherBlocks.SLAB_CARVED_STONE.id(), i & 1));
         }
 
-        room.add(drawVolume(random, ROOM_PALLET, SOUTH, 2, UP, height, EAST, 2, x+3, y, z+3, true));
+        decoration.add(drawVolume(random, ROOM_PALLET, SOUTH, 2, UP, height, EAST, 2, x + 3, y, z + 3, true));
     }
 }
