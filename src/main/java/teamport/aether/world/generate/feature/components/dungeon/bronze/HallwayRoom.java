6d4e7f63a75d0ab025b42ec011359e390fccf3a8
Redart15 <redart15@users.noreply.github.com>
2025-09-18 02:37:15 +0200

    reverted the room to the original

--- src/main/java/teamport/aether/world/generate/feature/components/dungeon/bronze/HallwayRoom.java
+++ src/main/java/teamport/aether/world/generate/feature/components/dungeon/bronze/HallwayRoom.java
@@ -1,8 +1,11 @@
 package teamport.aether.world.generate.feature.components.dungeon.bronze;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static net.minecraft.core.util.helper.Direction.*;
 import static teamport.aether.world.generate.feature.components.WorldFeatureComponent.drawVolume;
-import static teamport.aether.world.generate.feature.components.WorldFeatureComponent.drawVolumeX;
 import static teamport.aether.world.generate.feature.components.WorldFeaturePoint.wfp;
 
 public class HallwayRoom extends BaseBronzeRoom {
@@ -21,14 +24,25 @@ public class HallwayRoom extends BaseBronzeRoom {
 
     @Override
     public void makeRoom() {
+        List<Door> doorList = new ArrayList<>(doors);
+        Collections.shuffle(doorList, random);
+        int end = random.nextInt(2) + 2;
+        for(int c = 0; c < end; c++){
+            Door closingDoor = doorList.get(c);
+            room.add(drawVolume(0, 0, closingDoor.p1, wfp(0,0,0).moveInDirection(closingDoor.heading.getOpposite()).multiply(6).add(closingDoor.p2), true));
+            closingDoor.mark = false;
+        }
+        for(int c = end; c < doorList.size(); c++){
+            doorList.get(c).mark = true;
+        }
     }
 
 
     @Override
     public void markDoor(Door door) {
         super.markDoor(door);
-        room.add(drawVolumeX(0, 0, door.p1, wfp().moveInDirection(door.heading.getOpposite()).multiply(8).add(door.p2), false));
-        room.add(drawVolume(0, 0, SOUTH, 4, UP, 6, EAST, 4, x + 4, y + 1, x + 4, false));
+        room.add(drawVolume(0, 0, door.p1, wfp().moveInDirection(door.heading.getOpposite()).multiply(7).add(door.p2), false));
+//        room.add(drawVolume(0, 0, SOUTH, 4, UP, 6, EAST, 4, x + 4, y + 1, x + 4, false));
         this.placeRoom();
     }
 }
