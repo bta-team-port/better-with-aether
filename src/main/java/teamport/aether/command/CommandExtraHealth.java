40eebd7d239e34dc2ef77ec94699f2479fef9341
Oly <33404595+Olypolyu@users.noreply.github.com>
2025-09-18 23:12:49 -0300

    clean up extra health command

--- src/main/java/teamport/aether/command/CommandExtraHealth.java
+++ src/main/java/teamport/aether/command/CommandExtraHealth.java
@@ -4,11 +4,15 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.ArgumentTypeInteger;
 import com.mojang.brigadier.builder.ArgumentBuilderLiteral;
 import com.mojang.brigadier.builder.ArgumentBuilderRequired;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.core.entity.Entity;
 import net.minecraft.core.entity.player.Player;
 import net.minecraft.core.net.command.CommandManager;
 import net.minecraft.core.net.command.CommandSource;
+import net.minecraft.core.net.command.arguments.ArgumentTypeBlock;
 import net.minecraft.core.net.command.arguments.ArgumentTypeEntity;
+import net.minecraft.core.net.command.arguments.ArgumentTypeIntegerCoordinates;
 import net.minecraft.core.net.command.helpers.EntitySelector;
 import net.minecraft.core.net.command.util.CommandHelper;
 import teamport.aether.helper.HealthHelper;
@@ -19,8 +23,6 @@ import java.util.List;
 
 import static com.mojang.brigadier.builder.ArgumentBuilderLiteral.literal;
 
-// for now, TODO figure out what that warning mean
-@SuppressWarnings("unchecked")
 public class CommandExtraHealth implements CommandManager.CommandRegistry{
 
     /* @a, all players
@@ -30,74 +32,101 @@ public class CommandExtraHealth implements CommandManager.CommandRegistry{
     *  @s, self
     * */
 
+    static int add(CommandContext<?> c) {
+        CommandSource source = (CommandSource) c.getSource();
+        int amount = c.getArgument("amount", Integer.class);
+        EntitySelector entitySelector = c.getArgument("target", EntitySelector.class);
+        List<? extends Player> entities = (List<Player>)entitySelector.get(source);
+
+        int max_health_added = 0;
+        for(Player player: entities){
+            int current_extra_health = HealthHelper.getExtraHealth(player);
+            HealthHelper.addExtraHealth(player, amount);
+            int new_extra_health = HealthHelper.getExtraHealth(player);
+            max_health_added = Math.max(new_extra_health - current_extra_health, max_health_added);
+            NetworkHandler.sendToPlayer(player, new CommandExtraHealthMessage(new_extra_health));
+        }
+
+        if(entities.size() == 1) {
+            source.sendTranslatableMessage(
+                "command.aether.add.extra_health.success_single_entity",
+                max_health_added,
+                CommandHelper.getEntityName(entities.get(0))
+            );
+        }
+
+        else source.sendTranslatableMessage("command.aether.add.extra_health.success_multiple_entities", max_health_added);
+        return 0;
+    }
+
+    static int set(CommandContext<?> c) {
+        CommandSource source = (CommandSource) c.getSource();
+
+        int amount = c.getArgument("amount", Integer.class);
+        amount = Math.min(20, amount);
+
+        EntitySelector entitySelector = c.getArgument("target", EntitySelector.class);
+        List<? extends Player> entities = (List<Player>)entitySelector.get(source);
+
+        for(Player player: entities){
+            HealthHelper.setExtraHealth(player, amount);
+            NetworkHandler.sendToPlayer(player, new CommandExtraHealthMessage(amount));
+        }
+
+        if(entities.size() == 1) {
+            source.sendTranslatableMessage(
+                "command.aether.set.extra_health.success_single_entity",
+                CommandHelper.getEntityName(entities.get(0)), amount
+            );
+        }
+
+        else source.sendTranslatableMessage("command.aether.set.extra_health.success_multiple_entities", amount);
+        return 0;
+    }
+
+    static int get(CommandContext<?> c) throws CommandSyntaxException {
+        CommandSource source = (CommandSource) c.getSource();
+        EntitySelector entitySelector = c.getArgument("target", EntitySelector.class);
+        List<? extends Entity> entities = entitySelector.get(source);
+
+        int total_health = 0;
+        for (Entity player: entities){
+            total_health += HealthHelper.getExtraHealth((Player) player);
+        }
+
+        if (entities.size() == 1) {
+            source.sendTranslatableMessage("command.aether.get.extra_health.success_single_entity",
+                    CommandHelper.getEntityName(entities.get(0)), total_health);
+        }
+
+        else source.sendTranslatableMessage("command.aether.get.extra_health.success_multiple_entities", total_health);
+        return 0;
+    }
+
     @Override
     public void register(CommandDispatcher<CommandSource> commandDispatcher) {
-        commandDispatcher
-                .register((ArgumentBuilderLiteral)literal("aether:extraHealth").requires(t -> ((CommandSource) t).hasAdmin())
-                        .then(literal("add")
-                        .then(ArgumentBuilderRequired.argument("target", ArgumentTypeEntity.usernames())
-                        .then(ArgumentBuilderRequired.argument("amount", ArgumentTypeInteger.integer())
-                        .executes(c ->{
-                            CommandSource source = (CommandSource) c.getSource();
-                            int amount = c.getArgument("amount", Integer.class);
-                            EntitySelector entitySelector = c.getArgument("target", EntitySelector.class);
-                            List<? extends Player> entities = (List<Player>)entitySelector.get(source);
-                            int max_health_added = 0;
-                            for(Player player: entities){
-                                int current_extra_health = HealthHelper.getExtraHealth(player);
-                                HealthHelper.addExtraHealth(player, amount);
-                                int new_extra_health = HealthHelper.getExtraHealth(player);
-                                max_health_added = Math.max(new_extra_health - current_extra_health, max_health_added);
-                                NetworkHandler.sendToPlayer(player, new CommandExtraHealthMessage(new_extra_health));
-                            }
+        // You can't, it just IS this bad. :^)
+        ArgumentBuilderLiteral<CommandSource> commandExtraHealth = (ArgumentBuilderLiteral)
+            ((ArgumentBuilderLiteral) literal("aether:extraHealth")
+            .requires(t -> ((CommandSource) t).hasAdmin()))
+            .then(
+                literal("add")
+                .then(ArgumentBuilderRequired.argument("target", ArgumentTypeEntity.usernames())
+                .then(ArgumentBuilderRequired.argument("amount", ArgumentTypeInteger.integer())
+                .executes(CommandExtraHealth::add)))
+            )
+            .then(
+                literal("get")
+                .then(ArgumentBuilderRequired.argument("target", ArgumentTypeEntity.usernames())
+                .executes(CommandExtraHealth::get))
+            )
+            .then(
+                literal("set")
+                .then(ArgumentBuilderRequired.argument("target", ArgumentTypeEntity.usernames())
+                .then(ArgumentBuilderRequired.argument("amount", ArgumentTypeInteger.integer())
+                .executes(CommandExtraHealth::set)))
+            );
 
-                            if(entities.size() == 1) {
-                                source.sendTranslatableMessage("command.aether.add.extra_health.success_single_entity",
-                                        max_health_added, CommandHelper.getEntityName(entities.get(0)));
-                            }else{
-                                source.sendTranslatableMessage("command.aether.add.extra_health.success_multiple_entities", max_health_added);
-                            }
-                            return 0;
-                        }))))
-                        .then((ArgumentBuilderLiteral.literal("set"))
-                        .then(ArgumentBuilderRequired.argument("target", ArgumentTypeEntity.usernames())
-                        .then(ArgumentBuilderRequired.argument("amount", ArgumentTypeInteger.integer())
-                        .executes(c ->{
-                            CommandSource source = (CommandSource) c.getSource();
-                            int amount = c.getArgument("amount", Integer.class);
-                            amount = Math.min(20, amount);
-                            EntitySelector entitySelector = c.getArgument("target", EntitySelector.class);
-                            List<? extends Player> entities = (List<Player>)entitySelector.get(source);
-                            for(Player player: entities){
-                                HealthHelper.setExtraHealth(player, amount);
-                                NetworkHandler.sendToPlayer(player, new CommandExtraHealthMessage(amount));
-                            }
-
-                            if(entities.size() == 1) {
-                                source.sendTranslatableMessage("command.aether.set.extra_health.success_single_entity",
-                                        CommandHelper.getEntityName(entities.get(0)), amount);
-                            }else{
-                                source.sendTranslatableMessage("command.aether.set.extra_health.success_multiple_entities", amount);
-                            }
-                            return 0;
-                        }))))
-                        .then(ArgumentBuilderLiteral.literal("get")
-                        .then(ArgumentBuilderRequired.argument("target", ArgumentTypeEntity.usernames())
-                        .executes(c ->{
-                            CommandSource source = (CommandSource) c.getSource();
-                            EntitySelector entitySelector = c.getArgument("target", EntitySelector.class);
-                            List<? extends Entity> entities = entitySelector.get(source);
-                            int total_health = 0;
-                            for(Entity player: entities){
-                                total_health += HealthHelper.getExtraHealth((Player) player);
-                            }
-                            if(entities.size() == 1) {
-                                source.sendTranslatableMessage("command.aether.get.extra_health.success_single_entity",
-                                        CommandHelper.getEntityName(entities.get(0)), total_health);
-                            }else{
-                                source.sendTranslatableMessage("command.aether.get.extra_health.success_multiple_entities", total_health);
-                            }
-                            return 0;
-                        }))));
+        commandDispatcher.register(commandExtraHealth);
     }
 }
