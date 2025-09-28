6035acc834ffd5c96ee23610bb09001a59e0166a
Oly <33404595+Olypolyu@users.noreply.github.com>
2025-09-27 22:16:44 -0300

    added easteregg for when sleeping in the aether.

---
+++ src/main/java/teamport/aether/mixin/dimension/AetherBedMixin.java
@@ -0,0 +1,103 @@
+package teamport.aether.mixin.dimension;
+
+import net.minecraft.core.block.Block;
+import net.minecraft.core.block.BlockLogic;
+import net.minecraft.core.block.BlockLogicBed;
+import net.minecraft.core.block.Blocks;
+import net.minecraft.core.block.material.Material;
+import net.minecraft.core.entity.player.Player;
+import net.minecraft.core.sound.SoundCategory;
+import net.minecraft.core.util.helper.Direction;
+import net.minecraft.core.util.helper.Side;
+import net.minecraft.core.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import teamport.aether.entity.monster.fireminion.MobFireMinion;
+import teamport.aether.world.AetherDimension;
+import teamport.aether.world.generate.feature.components.WorldFeaturePoint;
+
+import java.util.Random;
+
+import static teamport.aether.world.generate.feature.components.WorldFeaturePoint.wfp;
+
+@Mixin(value = BlockLogicBed.class, remap = false)
+public class AetherBedMixin extends BlockLogic {
+    public AetherBedMixin(Block<?> block, Material material) {
+        super(block, material);
+    }
+
+    @Unique
+    private void doEffect(World world, Random r, int x, int y, int z) {
+        for (int i = 0; i < 8; i++) {
+            world.spawnParticle(
+                    "largesmoke",
+                    x + r.nextFloat(), y + r.nextFloat(), z + r.nextFloat(),
+                    0, 0.01, 0, 0
+            );
+        }
+    }
+
+    @Inject(method = "onBlockRightClicked", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/world/World;setBlockWithNotify(IIII)Z", shift = At.Shift.AFTER), cancellable = true)
+    public void onBlockRightClicked(World world, int x, int y, int z, Player player, Side side, double xPlaced, double yPlaced, CallbackInfoReturnable<Boolean> cir) {
+        if (world.dimension.id == AetherDimension.AetherDimensionID) {
+            cir.setReturnValue(true);
+            cir.cancel();
+        }
+        else return;
+
+        Random rand = world.rand;
+        for (int i = 0; i < 32; i++) {
+            world.spawnParticle(
+                    "flame",
+                    x + rand.nextGaussian(), y + rand.nextGaussian(), z + rand.nextGaussian(),
+                    0, 0.2, 0, 0
+            );
+
+            world.spawnParticle(
+                    "smoke",
+                    x + rand.nextGaussian(), y + rand.nextGaussian(), z + rand.nextGaussian(),
+                    0, 0.2, 0, 0
+            );
+        }
+
+        doEffect(world, rand, x, y, z);
+        for (Direction d : Direction.horizontalDirections) {
+            WorldFeaturePoint p = wfp(x, y, z).moveInDirection(d);
+            if (world.getBlockId(p.x, p.y, p.z) == Blocks.BED.id()) doEffect(world, rand, p.x, p.y, p.z);
+        }
+
+        player.fireHurt();
+        world.playSoundEffect(player, SoundCategory.WORLD_SOUNDS, x, y, z, "mob.ghast.fireball", 0.25F, (1.3F + (world.rand.nextFloat() - world.rand.nextFloat()) * 0.2F) * 0.7F);
+
+        WorldFeaturePoint anchor = wfp((int) player.x, (int) player.y, (int) player.z);
+
+        int mobsToSpawn = 5;
+        while (mobsToSpawn > 0) {
+            int attempts = 0;
+
+            MobFireMinion minion = new MobFireMinion(world);
+            WorldFeaturePoint spawn;
+
+            do {
+                spawn = anchor.copy();
+                spawn.moveInDirection(Direction.NORTH, 4 + (int) ((1.0F + rand.nextGaussian()) * 3));
+                spawn.rotateYAroundPivot(anchor, rand.nextFloat() * 360);
+
+                attempts++;
+                if (attempts > 30) break;
+            } while (minion.isInWall());
+
+            if (attempts > 30) break;
+
+            minion.setPos(spawn.x, spawn.y, spawn.z);
+            minion.setTarget(player);
+            world.entityJoinedWorld(minion);
+
+            world.playSoundEffect(null, SoundCategory.WORLD_SOUNDS, minion.x, minion.y, minion.z, "mob.ghast.fireball", 0.25F, (1.3F + (world.rand.nextFloat() - world.rand.nextFloat()) * 0.2F) * 0.7F);
+            mobsToSpawn--;
+        }
+    }
+}
