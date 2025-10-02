1053cdeb9c47903fef49fd609c37dda7d99ecbe9
Redart15 <redart15@users.noreply.github.com>
2025-10-03 00:07:13 +0200

    removed the mess into its own ItemModel

---
+++ src/main/java/teamport/aether/models/ItemModelPaintedSkyrootSign.java
@@ -0,0 +1,33 @@
+package teamport.aether.models;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.item.model.ItemModelStandard;
+import net.minecraft.client.render.texture.stitcher.IconCoordinate;
+import net.minecraft.client.render.texture.stitcher.TextureRegistry;
+import net.minecraft.core.entity.Entity;
+import net.minecraft.core.item.Item;
+import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.util.helper.DyeColor;
+import org.jetbrains.annotations.NotNull;
+
+@Environment(EnvType.CLIENT)
+public class ItemModelPaintedSkyrootSign extends ItemModelStandard{
+    public static IconCoordinate[] sign = new IconCoordinate[16];
+
+    public ItemModelPaintedSkyrootSign(Item item) {
+        super(item, (String) null);
+    }
+
+    public @NotNull IconCoordinate getIcon(Entity entity, ItemStack itemStack) {
+        int meta = itemStack.getMetadata();
+        return sign[meta & 15];
+    }
+
+    static {
+        for (DyeColor dye : DyeColor.itemOrderedColors()) {
+            sign[dye.itemMeta] = TextureRegistry.getTexture("aether:item/sign_skyroot/" + dye.colorID);
+        }
+    }
+}
+
