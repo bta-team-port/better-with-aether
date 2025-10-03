a0d58d530bf73e8a3e8141e1825eb0906bd6fd35
Redart15 <redart15@users.noreply.github.com>
2025-10-03 12:59:25 +0200

    renamed base class for fence paintable added painted fences, added the lang needed for the rest of the painted blocks

---
+++ src/main/java/teamport/aether/models/BlockModelPaintedSkyrootFence.java
@@ -0,0 +1,20 @@
+package teamport.aether.models;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.block.model.BlockModelFence;
+import net.minecraft.client.render.texture.stitcher.IconCoordinate;
+import net.minecraft.core.block.Block;
+import net.minecraft.core.block.BlockLogicFence;
+import net.minecraft.core.util.helper.Side;
+
+@Environment(EnvType.CLIENT)
+public class BlockModelPaintedSkyrootFence<T extends BlockLogicFence> extends BlockModelFence<T> {
+    public BlockModelPaintedSkyrootFence(Block<T> block) {
+        super(block);
+    }
+
+    public IconCoordinate getBlockTextureFromSideAndMetadata(Side side, int meta) {
+        return BlockModelPaintedSkyrootPlanks.texCoords[meta & 15];
+    }
+}
