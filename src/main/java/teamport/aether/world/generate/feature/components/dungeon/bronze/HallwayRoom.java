73586061eaca87fe97ff96866275131c3b1ffe58
Redart15 <redart15@users.noreply.github.com>
2025-10-05 15:17:33 +0200

    made hallways generate cleaner and without extra bumps

--- src/main/java/teamport/aether/world/generate/feature/components/dungeon/bronze/HallwayRoom.java
+++ src/main/java/teamport/aether/world/generate/feature/components/dungeon/bronze/HallwayRoom.java
@@ -1,13 +1,10 @@
 package teamport.aether.world.generate.feature.components.dungeon.bronze;
 
 import net.minecraft.core.block.Block;
+import net.minecraft.core.block.Blocks;
 import net.minecraft.core.block.material.Material;
 import teamport.aether.world.generate.feature.components.WorldFeatureComponent;
 import teamport.aether.world.generate.feature.components.WorldFeaturePoint;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 import static net.minecraft.core.util.helper.Direction.*;
 import static teamport.aether.world.generate.feature.components.WorldFeatureComponent.drawVolume;
@@ -50,25 +47,26 @@ public class HallwayRoom extends BaseBronzeRoom {
 
     @Override
     public void makeRoom() {
-        List<Door> doorList = new ArrayList<>(doors);
-        Collections.shuffle(doorList, random);
-        int end = random.nextInt(2) + 2;
-        for(int c = 0; c < end; c++){
-            Door closingDoor = doorList.get(c);
-            room.add(drawVolume(0, 0, closingDoor.p1, wfp(0,0,0).moveInDirection(closingDoor.heading.getOpposite()).multiply(6).add(closingDoor.p2), true));
-            closingDoor.mark = false;
-        }
-        for(int c = end; c < doorList.size(); c++){
-            doorList.get(c).mark = true;
-        }
+//        List<Door> doorList = new ArrayList<>(doors);
+//        Collections.shuffle(doorList, random);
+//        int end = random.nextInt(2) + 2;
+//        for(int c = 0; c < end; c++){
+//            Door closingDoor = doorList.get(c);
+//            room.add(drawVolume(0, 0, closingDoor.p1, wfp(0,0,0).moveInDirection(closingDoor.heading.getOpposite()).multiply(6).add(closingDoor.p2), true));
+//            closingDoor.mark = ClosingType.OPEN;
+//        }
+//        for(int c = end; c < doorList.size(); c++){
+//            doorList.get(c).mark = ClosingType.ROOM_LOCKED;
+//        }
     }
 
 
     @Override
-    public void markDoor(Door door) {
-        super.markDoor(door);
+    public void markDoor(Door door, ClosingType closingType) {
+        super.markDoor(door, closingType);
+        if(closingType != ClosingType.PLACED) return;
         room.add(drawVolume(0, 0, door.p1, wfp().moveInDirection(door.heading.getOpposite()).multiply(7).add(door.p2), false));
-        room.add(drawVolume(0, 0, SOUTH, 4, UP, 6, EAST, 4, x + 4, y + 1, x + 4, false));
+        room.add(drawVolume(0, 0, SOUTH, 4, UP, 6, EAST, 4, x + 4, y + 1, z + 4, false));
         this.placeRoom();
     }
 }
