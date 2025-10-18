15e77e47437d6a16dd2550d5ff9af2d1a05ee726
Oly <33404595+Olypolyu@users.noreply.github.com>
2025-10-17 22:14:14 -0300

    fix hostile paradise with BTA-approved jank!

--- src/main/java/teamport/aether/mixin/achievement/HostileParadiseMixin.java
+++ src/main/java/teamport/aether/mixin/achievement/HostileParadiseMixin.java
@@ -1,20 +1,30 @@
 package teamport.aether.mixin.achievement;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.util.helper.DyeColor;
+import net.minecraft.core.entity.Mob;
+import net.minecraft.core.entity.player.Player;
+import net.minecraft.core.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import teamport.aether.achievements.AetherAchievements;
 import teamport.aether.world.AetherDimension;
 
-@Mixin(value = Minecraft.class, remap = false)
-public abstract class HostileParadiseMixin {
+@Mixin(value = Player.class, remap = false)
+public abstract class HostileParadiseMixin extends Mob {
+    public HostileParadiseMixin(@Nullable World world) {
+        super(world);
+    }
 
-    @Inject(method = "usePortal", at = @At(value = "HEAD"))
-    public void grantHostileParadise(int dim, DyeColor portalColor, CallbackInfo ci) {
-        if (dim == AetherDimension.AetherDimensionID) {
+    @Shadow
+    public int dimension;
+
+    @Inject(method = "tick", at = @At(value = "HEAD"))
+    public void grantHostileParadise(CallbackInfo ci) {
+        if (this.world.dimension.id == AetherDimension.AetherDimensionID) {
             Minecraft.getMinecraft().thePlayer.addStat(AetherAchievements.HOSTILE_PARADISE, 1);
         }
     }
