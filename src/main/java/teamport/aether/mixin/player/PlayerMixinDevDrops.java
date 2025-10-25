8316131d6004293b73c6d0e3e6fdfc9eb963ebe0
Redart15 <redart15@users.noreply.github.com>
2025-10-25 18:55:59 +0200

    Redart15 no longer drops the locked version of the carved light stone

--- src/main/java/teamport/aether/mixin/player/PlayerMixinDevDrops.java
+++ src/main/java/teamport/aether/mixin/player/PlayerMixinDevDrops.java
@@ -5,14 +5,12 @@ import net.minecraft.core.entity.player.Player;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import teamport.aether.AetherGlobals;
 import teamport.aether.blocks.AetherBlocks;
 import teamport.aether.items.AetherItems;
-
-import static teamport.aether.AetherMod.*;
 
 @Mixin(value = Player.class, remap = false)
 public class PlayerMixinDevDrops {
@@ -22,16 +20,16 @@ public class PlayerMixinDevDrops {
         Player asThis = (Player) (Object) this;
         String uuid = asThis.uuid.toString();
         switch (uuid) {
-            case UUID_LUKEISSTUFF: // LukeisStuff
+            case AetherGlobals.UUID_LUKEISSTUFF: // LukeisStuff
                 asThis.dropPlayerItemWithRandomChoice(new ItemStack(AetherItems.AMMO_WINDBALL, 1), true);
                 break;
-            case UUID_OLYPOLYU: // Olypolyu / Kheprep
+            case AetherGlobals.UUID_OLYPOLYU: // Olypolyu / Kheprep
                 asThis.dropPlayerItemWithRandomChoice(new ItemStack(AetherBlocks.BLOCK_GRAVITITE, 1), true);
                 break;
-            case UUID_TOCININ: // Tocinin
+            case AetherGlobals.UUID_TOCININ: // Tocinin
                 asThis.dropPlayerItemWithRandomChoice(new ItemStack(Items.FOOD_PORKCHOP_RAW, 1), true);
                 break;
-            case UUID_REDART15: // Redart15
+            case AetherGlobals.UUID_REDART15: // Redart15
                 asThis.dropPlayerItemWithRandomChoice(new ItemStack(AetherBlocks.CARVED_STONE_LIGHT, 1), true);
                 break;
             default:
