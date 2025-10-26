a4d72c87c48823ea2bbff6b0b4ea52612c633474
Redart15 <redart15@users.noreply.github.com>
2025-10-26 20:51:29 +0100

    extra health is now preserved on death.

---
+++ src/main/java/teamport/aether/mixin/item/PlayerListMixinRespawnWithExtraHealth.java
@@ -0,0 +1,20 @@
+package teamport.aether.mixin.item;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.server.entity.player.PlayerServer;
+import net.minecraft.server.net.PlayerList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import sunsetsatellite.catalyst.effects.helper.HealthHelper;
+
+@Mixin(value = PlayerList.class, remap = false)
+public class PlayerListMixinRespawnWithExtraHealth {
+
+    @Inject(method = "recreatePlayerEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/player/inventory/container/ContainerInventory;transferAllContents(Lnet/minecraft/core/player/inventory/container/ContainerInventory;)V"), remap = false)
+    public void keepExtraHealthServer(PlayerServer previousPlayer, int dimension, CallbackInfoReturnable<PlayerServer> cir, @Local(name = "newPlayer") final PlayerServer newPlayer) {
+        HealthHelper.setExtraHealth(newPlayer, HealthHelper.getExtraHealth(previousPlayer));
+    }
+
+}
