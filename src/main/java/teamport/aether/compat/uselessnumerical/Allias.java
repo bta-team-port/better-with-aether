c038816b9fea8da84056e04216788ead2c7e3667
Oly <33404595+Olypolyu@users.noreply.github.com>
2025-10-26 20:57:14 -0300

    fix typo

---
+++ src/main/java/teamport/aether/compat/uselessnumerical/Allias.java
@@ -0,0 +1,15 @@
+package teamport.aether.compat.uselessnumerical;
+
+import gungun974.uselessnumerical.UselessNumericalEntrypoint;
+import net.minecraft.core.util.collection.NamespaceID;
+import teamport.aether.AetherMod;
+
+import java.util.function.BiConsumer;
+
+public class Allias implements UselessNumericalEntrypoint {
+    @Override
+    public void defineAlias(BiConsumer<NamespaceID, NamespaceID> alias) {
+        alias.accept(NamespaceID.getPermanent(AetherMod.MOD_ID, "block/planks_skyroot.painted"), NamespaceID.getPermanent(AetherMod.MOD_ID, "block/planks_skyroot_painted"));
+        alias.accept(NamespaceID.getPermanent(AetherMod.MOD_ID, "item/sign.skyroot.painted"), NamespaceID.getPermanent(AetherMod.MOD_ID, "item/sign_skyroot_painted"));
+    }
+}
