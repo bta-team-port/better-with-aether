19da754fbec82ef848782d4ea1ff917d22b1aea4
LukeisStuff <lukeisawesome03@gmail.com>
2025-11-22 17:33:36 -0500

    make it protected

--- src/main/java/teamport/aether/entity/tile/AetherTileEntityMachine.java
+++ src/main/java/teamport/aether/entity/tile/AetherTileEntityMachine.java
@@ -7,7 +7,6 @@ import net.minecraft.core.block.entity.TileEntity;
 import net.minecraft.core.block.motion.CarriedBlock;
 import net.minecraft.core.entity.Entity;
 import net.minecraft.core.entity.player.Player;
-import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.net.packet.Packet;
 import net.minecraft.core.player.inventory.container.Container;
@@ -19,7 +18,7 @@ import java.util.Random;
 
 public class AetherTileEntityMachine extends TileEntity implements Container {
     public final Random random = new Random();
-    public ItemStack item;
+    protected ItemStack item;
 
     protected ItemStack[] containerItemStacks;
     private int maxEnergyTime = 0;       // maxBurnTime
