84b92652e00fca29f297483bc3432df0df1f677f
Redart15 <redart15@users.noreply.github.com>
2025-11-23 02:31:04 +0100

    works with valkyrie

--- src/main/java/teamport/aether/items/item_tool/item_tool_valkyrie/ItemToolPickaxeValkyrie.java
+++ src/main/java/teamport/aether/items/item_tool/item_tool_valkyrie/ItemToolPickaxeValkyrie.java
@@ -3,11 +3,18 @@ package teamport.aether.items.item_tool.item_tool_valkyrie;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.tag.BlockTags;
 import net.minecraft.core.entity.Mob;
+import net.minecraft.core.entity.player.Player;
+import net.minecraft.core.enums.EnumDropCause;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.item.material.ToolMaterial;
 import net.minecraft.core.util.helper.DamageType;
+import net.minecraft.core.util.helper.Side;
+import net.minecraft.core.world.World;
+import redart15.commandly.veincapitator.VeinMining;
 import teamport.aether.AetherMod;
 import teamport.aether.blocks.AetherBlockTags;
+import teamport.aether.compat.commandly.AetherCommandlyRules;
+import teamport.aether.helper.AetherUtil;
 import teamport.aether.items.AetherHasCustomDamageType;
 import teamport.aether.items.item_tool.ItemToolPickaxeAether;
 
@@ -29,6 +36,18 @@ public class ItemToolPickaxeValkyrie extends ItemToolPickaxeAether implements Ae
     @Override
     public float getStrVsBlock(ItemStack itemstack, Block<?> block) {
         return block.hasTag(AetherBlockTags.MINEABLE_BY_AETHER_PICKAXE) || block.hasTag(BlockTags.MINEABLE_BY_PICKAXE) ? this.material.getEfficiency(false) : 1.0F;
+    }
+
+    @Override
+    public boolean beforeDestroyBlock(World world, ItemStack itemStack, int blockId, int x, int y, int z, Side side, Player player) {
+        if (!world.isClientSide && AetherCommandlyRules.canVeinMine(world) && !player.isSneaking()) {
+            return !VeinMining
+                .veinMining(world, itemStack, x, y, z, player)
+                .setDropCause(AetherUtil.isSilkTouch(player) ? EnumDropCause.SILK_TOUCH : EnumDropCause.PROPER_TOOL)
+                .setMiningTags(AetherBlockTags.MINEABLE_BY_AETHER_PICKAXE, BlockTags.MINEABLE_BY_PICKAXE)
+                .mine(blockId, side);
+        }
+        return true;
     }
 
     @Override
