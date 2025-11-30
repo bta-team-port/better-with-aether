ac6b6054492e02fce873bf6a816a3b77a43ccbff
Redart15 <redart15@users.noreply.github.com>
2025-12-01 00:02:52 +0100

    added more stuff to plugin

--- src/main/java/teamport/aether/compat/AetherPlugin.java
+++
@@ -1,8 +0,0 @@
-package teamport.aether.compat;
-
-import teamport.aether.world.feature.dungeon.bronze.WorldFeatureAetherBronzeDungeon;
-
-public interface AetherPlugin {
-    void registerBronzeDungeonRoom(WorldFeatureAetherBronzeDungeon.RoomManager roomManager);
-    void registerDungeonType();
-}
