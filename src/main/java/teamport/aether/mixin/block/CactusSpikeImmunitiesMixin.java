188dc9ba135650565810ac51da5cb38475e7f0a4
Redart15 <redart15@users.noreply.github.com>
2025-11-30 22:04:07 +0100

    renamed interface

--- src/main/java/teamport/aether/mixin/block/CactusSpikeImmunitiesMixin.java
+++ src/main/java/teamport/aether/mixin/block/CactusSpikeImmunitiesMixin.java
@@ -12,7 +12,7 @@ import net.minecraft.core.util.helper.DamageType;
 import net.minecraft.core.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import teamport.aether.entity.AetherMobImmuneToSpikes;
+import teamport.aether.entity.AetherMobOtherImmunities;
 
 @Mixin(value = BlockLogicCactus.class, remap = false)
 public abstract class CactusSpikeImmunitiesMixin {
@@ -20,12 +20,12 @@ public abstract class CactusSpikeImmunitiesMixin {
     @Expression("? instanceof Mob")
     @ModifyExpressionValue(method = "onEntityCollidedWithBlock", at = @At("MIXINEXTRAS:EXPRESSION"))
     private boolean monsterImmuneToSpikeOne(boolean original, World world, int x, int y, int z, Entity entity) {
-        return original && entity instanceof AetherMobImmuneToSpikes;
+        return original && entity instanceof AetherMobOtherImmunities;
     }
     @WrapOperation(method = "onEntityCollidedWithBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/entity/Entity;hurt(Lnet/minecraft/core/entity/Entity;ILnet/minecraft/core/util/helper/DamageType;)Z"))
     private boolean monsterImmuneToSpikes(Entity instance, Entity attacker, int baseDamage, DamageType type, Operation<Boolean> original) {
-        if (!(instance instanceof AetherMobImmuneToSpikes)) return original.call(instance, attacker, baseDamage, type);
-        AetherMobImmuneToSpikes immune = (AetherMobImmuneToSpikes) instance;
+        if (!(instance instanceof AetherMobOtherImmunities)) return original.call(instance, attacker, baseDamage, type);
+        AetherMobOtherImmunities immune = (AetherMobOtherImmunities) instance;
         if (immune.canTakeDamageFromCactus()) return original.call(instance, attacker, baseDamage, type);
         return false;
     }
