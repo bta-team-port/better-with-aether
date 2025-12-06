4764fabc1c7c99ea1230ce7bc4a764508334a2c8
LukeisStuff <lukeisawesome03@gmail.com>
2025-12-06 05:20:42 -0500

    wip block color and colorizer stuff, added missing blocks to map colors and instrument list, cleanup

--- src/main/java/teamport/aether/models/BlockModelGrassAether.java
+++ src/main/java/teamport/aether/models/BlockModelGrassAether.java
@@ -2,7 +2,10 @@ package teamport.aether.models;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.render.RenderBlocks;
+import net.minecraft.client.render.block.model.BlockModelGrass;
 import net.minecraft.client.render.block.model.BlockModelStandard;
+import net.minecraft.client.render.tessellator.Tessellator;
 import net.minecraft.client.render.texture.stitcher.IconCoordinate;
 import net.minecraft.client.render.texture.stitcher.TextureRegistry;
 import net.minecraft.core.block.Block;
@@ -19,17 +22,65 @@ public class BlockModelGrassAether<T extends BlockLogic> extends BlockModelStand
     private final IconCoordinate snowSide = TextureRegistry.getTexture(MOD_ID + ":block/grass_aether/snowy_side");
     private final IconCoordinate snowSideRetro = TextureRegistry.getTexture(MOD_ID + ":block/grass_aether/snowy_side_retro");
 
+    private static final IconCoordinate[] overlayIndices = new IconCoordinate[]{
+        null, null,
+        TextureRegistry.getTexture(MOD_ID + ":block/grass_aether/side_overlay"),
+        TextureRegistry.getTexture(MOD_ID + ":block/grass_aether/side_overlay"),
+        TextureRegistry.getTexture(MOD_ID + ":block/grass_aether/side_overlay"),
+        TextureRegistry.getTexture(MOD_ID + ":block/grass_aether/side_overlay"),
+    };
+
     public BlockModelGrassAether(Block<T> block) {
         super(block);
     }
 
     @Override
-    public IconCoordinate getBlockTexture(WorldSource blockAccess, int x, int y, int z, Side side) {
-        Material material = blockAccess.getBlockMaterial(x, y + 1, z);
-        boolean isSnow = (material == Material.topSnow || material == Material.snow) && side.getAxis() != Axis.Y;
-        if (isRetro()) {
-            return isSnow ? this.snowSideRetro : super.getBlockTexture(blockAccess, x, y, z, side);
+    public boolean render(Tessellator tessellator, int x, int y, int z) {
+        boolean rendered = super.render(tessellator, x, y, z);
+
+        if (RenderBlocks.fancyGrass && !this.isRetro()) {
+            BlockModelGrass.useOverlay = true;
+            rendered |= super.render(tessellator, x, y, z);
+            BlockModelGrass.useOverlay = false;
         }
-        return isSnow ? this.snowSide : super.getBlockTexture(blockAccess, x, y, z, side);
+
+        return rendered;
+    }
+
+    @Override
+    public IconCoordinate getBlockTexture(WorldSource world, int x, int y, int z, Side side) {
+        Material above = world.getBlockMaterial(x, y + 1, z);
+        boolean snowy = (above == Material.topSnow || above == Material.snow) && side.getAxis() != Axis.Y;
+
+        if (snowy) {
+            return isRetro() ? snowSideRetro : snowSide;
+        }
+
+        return super.getBlockTexture(world, x, y, z, side);
+    }
+
+    @Override
+    public IconCoordinate getBlockTextureFromSideAndMetadata(Side side, int meta) {
+        return BlockModelGrass.useOverlay ? overlayIndices[side.getId()] : super.getBlockTextureFromSideAndMetadata(side, meta);
+    }
+
+    @Override
+    public boolean shouldSideBeColored(WorldSource world, int x, int y, int z, int sideId, int meta) {
+        Material above = world.getBlockMaterial(x, y + 1, z);
+        if (above == Material.topSnow || above == Material.snow) {
+            return false;
+        }
+        return BlockModelGrass.useOverlay || sideId == Side.TOP.getId();
+    }
+
+    @Override
+    public void renderBlockOnInventory(Tessellator tessellator, int metadata, float brightness, float alpha, Integer lightmapCoordinate) {
+        super.renderBlockOnInventory(tessellator, metadata, brightness, alpha, lightmapCoordinate);
+
+        if (RenderBlocks.fancyGrass && !this.isRetro()) {
+            BlockModelGrass.useOverlay = true;
+            super.renderBlockOnInventory(tessellator, metadata, brightness, alpha, lightmapCoordinate);
+            BlockModelGrass.useOverlay = false;
+        }
     }
 }
