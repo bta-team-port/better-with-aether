a0552cd4c92f79766635ceaebbc483e2eac6ef5b
Redart15 <redart15@users.noreply.github.com>
2025-12-07 16:03:44 +0100

    spawns particles when blocks cannot be placed, slab cannot be placed anymore

--- src/main/java/teamport/aether/mixin/item/ItemBlockSlabBlacklistMixin.java
+++ src/main/java/teamport/aether/mixin/item/ItemBlockSlabBlacklistMixin.java
@@ -11,35 +11,38 @@ import net.minecraft.core.block.Block;
 import net.minecraft.core.entity.player.Player;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.item.block.ItemBlock;
+import net.minecraft.core.item.block.ItemBlockSlab;
 import net.minecraft.core.util.helper.Side;
 import net.minecraft.core.world.World;
-import org.jspecify.annotations.NonNull;
+import org.jetbrains.annotations.NotNull;
 import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import teamport.aether.helper.MixinHelper;
 import teamport.aether.helper.ParticleMaker;
 import teamport.aether.world.AetherDimension;
 
-@Mixin(value = ItemBlock.class, remap = false)
-public abstract class ItemBlockBlacklistMixin {
-    @Shadow
-    @NonNull
-    protected Block<?> block;
+@Mixin(value = ItemBlockSlab.class, remap = false)
+public abstract class ItemBlockSlabBlacklistMixin extends ItemBlock {
+
+    private ItemBlockSlabBlacklistMixin(@NotNull Block block) {
+        super(block);
+    }
+
     @Definition(id = "stackSize", field = "Lnet/minecraft/core/item/ItemStack;stackSize:I")
     @Expression("?.stackSize <= 0")
     @ModifyExpressionValue(method = "onUseItemOnBlock", at = @At("MIXINEXTRAS:EXPRESSION"))
     private boolean banBlocksFromDimensionsOne(boolean original, ItemStack stack, @Nullable Player player, World world, int x, int y, int z, Side side, double xPlaced, double yPlaced) {
         return original || world.dimension != AetherDimension.getAether() && AetherDimension.getDimensionBlacklist(world.dimension).contains(block.id());
     }
-    @Definition(id = "canPlaceInsideBlock", method = "Lnet/minecraft/core/world/World;canPlaceInsideBlock(III)Z")
-    @Expression("?.canPlaceInsideBlock(?, ?, ?) == false")
+
+    @Expression("? != 0")
     @ModifyExpressionValue(method = "onUseItemOnBlock", at = @At("MIXINEXTRAS:EXPRESSION"))
     private boolean banBlocksFromDimensionsTwo(boolean original, ItemStack stack, @Nullable Player player, World world, int x, int y, int z, Side side, double xPlaced, double yPlaced, @Share("replacementId") LocalIntRef replacementId) {
         replacementId.set(AetherDimension.getDimensionBlacklist(world.dimension).contains(block.id()) ? MixinHelper.BLOCK_TO_BECOME.getOrDefault(block.id(), -2) : -1);
         return original;
     }
+
     @WrapOperation(method = "onUseItemOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/world/World;canBlockBePlacedAt(IIIIZLnet/minecraft/core/util/helper/Side;)Z"))
     private boolean banBlocksFromDimensionsThree(World instance, int blockId, int x, int y, int z, boolean flag, Side side, Operation<Boolean> original, @Share("replacementId") LocalIntRef replacementId) {
         int id = replacementId.get();
@@ -49,6 +52,7 @@ public abstract class ItemBlockBlacklistMixin {
         }
         return original.call(instance, id == -1 ? blockId : id, x, y, z, flag, side);
     }
+
     @WrapOperation(method = "onUseItemOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/world/World;setBlockAndMetadataWithNotify(IIIII)Z"))
     private boolean banBlocksFromDimensionsFour(World instance, int x, int y, int z, int id, int meta, Operation<Boolean> original, @Share("replacementId") LocalIntRef replacementId) {
         int theReplacementId = replacementId.get();
@@ -56,4 +60,5 @@ public abstract class ItemBlockBlacklistMixin {
         if (theReplacementId != -1 && condition) ParticleMaker.spawnReplacementEffects(instance, x, y, z);
         return condition;
     }
+
 }
