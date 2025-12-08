d7cdff0386fde8e50ea473b116850430404e5a90
Redart15 <redart15@users.noreply.github.com>
2025-12-09 00:33:36 +0100

    adjusted damageItem where items would not break upon reaching zero durability

--- src/main/java/teamport/aether/entity/player/PlayerUntil.java
+++ src/main/java/teamport/aether/entity/player/PlayerUntil.java
@@ -1,6 +1,7 @@
 package teamport.aether.entity.player;
 
 import net.minecraft.core.entity.player.Player;
+import net.minecraft.core.item.ItemFood;
 import net.minecraft.core.item.ItemStack;
 import teamport.aether.item.AetherItems;
 import turniplabs.halplibe.helper.EnvironmentHelper;
@@ -11,6 +12,9 @@ import static teamport.aether.item.accessory.SlotAccessory.TRINKET_2_SLOT;
 public class PlayerUntil {
     private PlayerUntil(){/* no need to initiate*/}
 
+    public enum InventoryType{
+        HOLD, MAIN, ARMOR
+    }
 
     public static boolean isSilkTouch(Player player) {
         ItemStack trinketOne = player.inventory.armorInventory[TRINKET_1_SLOT];
@@ -33,5 +37,67 @@ public class PlayerUntil {
             return player.y;
         }
         return player.y - player.bbHeight;
+    }
+
+    /// The normal damageItem does not destroy the item if the item durability hits zero.
+    public static void damageItem(Player player, ItemStack stack, int itemDamage, InventoryType type, int index){
+        stack.damageItem(itemDamage, player);
+        if(stack.stackSize <= 0){
+            switch (type){
+                case HOLD:
+                    player.destroyCurrentEquippedItem(); return;
+                case MAIN:
+                    player.inventory.mainInventory[index] = null; return;
+                case ARMOR:
+                    player.inventory.armorInventory[index] = null; return;
+                default:
+                    --stack.stackSize;
+                    return;
+            }
+        }
+    }
+
+
+
+    /// The normal damageItem does not destroy the item if the item durability hits zero. This target an item to destroy
+    /// in the player main inventory at an index.
+    public static void damageItemMain(Player player, int itemDamage, ItemStack stack, int index){
+        stack.damageItem(itemDamage, player);
+        if(stack.stackSize <= 0){
+            player.inventory.mainInventory[index] = null;
+        }
+    }
+
+    ///  Tool taking only 1 damage is quite common.
+    public static void damageItemMain(Player player, ItemStack stack, int index){
+        damageItemMain(player, 1, stack, index);
+    }
+
+    /// The normal damageItem does not destroy the item if the item durability hits zero. This target an item to destroy
+    /// in the player armor inventory at an index.
+    public static void damageItemArmor(Player player, int itemDamage, ItemStack stack,  int index){
+        stack.damageItem(itemDamage, player);
+        if(stack.stackSize <= 0){
+            player.inventory.armorInventory[index] = null;
+        }
+    }
+
+    ///  Tool taking only 1 damage is quite common.
+    public static void damageItemArmor(Player player, ItemStack stack, int index){
+        damageItemArmor(player, 1, stack, index);
+    }
+
+    /// The normal damageItem does not destroy the item if the item durability hits zero.
+    /// This method is the simplest but relays on a the fact that negative itemstacks are deleted by default.
+     public static void damageItem(Player player, int itemDamage, ItemStack stack){
+        stack.damageItem(itemDamage, player);
+        if(stack.stackSize <= 0){
+            --stack.stackSize;
+        }
+    }
+
+    ///  Tool taking only 1 damage is quite common.
+    public static void damageItem(Player player, ItemStack stack){
+        damageItem(player, 1, stack);
     }
 }
