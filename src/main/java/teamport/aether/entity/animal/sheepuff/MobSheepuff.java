9333e409d0d87173f4c5ac47af7a1fee801d05ed
Redart15 <redart15@users.noreply.github.com>
2025-12-15 00:03:01 +0100

    fixed the sheepuff spawning color

--- src/main/java/teamport/aether/entity/animal/sheepuff/MobSheepuff.java
+++ src/main/java/teamport/aether/entity/animal/sheepuff/MobSheepuff.java
@@ -308,17 +308,16 @@ public class MobSheepuff extends MobAetherAnimal {
 
     public static DyeColor getRandomFleeceColor(Random random) {
         int i = random.nextInt(100);
-        if (i < 5) {
+        if(i < 5){
             return DyeColor.LIGHT_BLUE;
-        } else if (i < 10) {
+        }else if(i < 10){
             return DyeColor.CYAN;
         } else if (i < 15) {
             return DyeColor.LIME;
         } else if (i < 18) {
             return DyeColor.PINK;
-        } else {
-            return random.nextInt(500) != 0 ? DyeColor.WHITE : DyeColor.PURPLE;
         }
+        return random.nextInt(500) == 0 ? DyeColor.PURPLE : DyeColor.WHITE;
     }
 
     public int getTimeSheepEating() {
