ef533fbb8a619d9e931939863cf78b8cd4362146
Redart15 <redart15@users.noreply.github.com>
2025-12-23 01:27:12 +0100

    count now only is available in dev env.

--- src/main/java/teamport/aether/command/AetherCommand.java
+++ src/main/java/teamport/aether/command/AetherCommand.java
@@ -1,15 +1,15 @@
 package teamport.aether.command;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.net.command.CommandManager;
 
-// TODO figure out how server-side commands are registered
-// TODO do the same optimization as in AetherItems.init()
 public class AetherCommand {
 
     public static void registerClientCommands() {
-        CommandManager.registerCommand(new CommandCount());
+        if(FabricLoader.getInstance().isDevelopmentEnvironment()){
+            CommandManager.registerCommand(new CommandCount());
+        }
     }
 
-    // I am unsure if this is a correct way of doing this, because I could not start server to check
     public static void registerServerCommands() {}
 }
