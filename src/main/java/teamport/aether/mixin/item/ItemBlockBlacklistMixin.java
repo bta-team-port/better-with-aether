0778fa307f13613196431f64325b5493c4e2f0e3
Your Name <33404595+Olypolyu@users.noreply.github.com>
2025-12-30 10:33:56 -0300

    fix igneous shit, readd ban list.

--- src/main/java/teamport/aether/mixin/item/ItemBlockBlacklistMixin.java
+++ src/main/java/teamport/aether/mixin/item/ItemBlockBlacklistMixin.java
@@ -8,6 +8,7 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
 import net.minecraft.core.block.Block;
+import net.minecraft.core.block.Blocks;
 import net.minecraft.core.entity.player.Player;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.item.block.ItemBlock;
@@ -22,10 +23,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import teamport.aether.helper.MixinHelper;
 import teamport.aether.helper.ParticleMaker;
 import teamport.aether.world.AetherDimension;
+import teamport.aether.world.SunSpiritDeath;
 import turniplabs.halplibe.helper.EnvironmentHelper;
+
+import java.util.List;
 
 @Mixin(value = ItemBlock.class, remap = false)
 public abstract class ItemBlockBlacklistMixin {
+
+    private static final int BANNED_BLOCK = -1;
+    private static final int REPLACED_BLOCK = -2;
+
     @Shadow
     @NonNull
     protected Block<?> block;
@@ -41,25 +49,35 @@ public abstract class ItemBlockBlacklistMixin {
     @Expression("?.canPlaceInsideBlock(?, ?, ?) == false")
     @ModifyExpressionValue(method = "onUseItemOnBlock", at = @At("MIXINEXTRAS:EXPRESSION"))
     private boolean banBlocksFromDimensionsTwo(boolean original, ItemStack stack, @Nullable Player player, World world, int x, int y, int z, Side side, double xPlaced, double yPlaced, @Share("replacementId") LocalIntRef replacementId) {
-        replacementId.set(AetherDimension.getDimensionBlacklist(world.dimension).contains(block.id()) ? MixinHelper.BLOCK_TO_BECOME.getOrDefault(block.id(), -2) : -1);
+        List<Integer> dimensionBlackList = AetherDimension.getDimensionBlacklist(world.dimension);
+
+        if (dimensionBlackList.contains(block.id())) {
+            // This is a hack.
+            // if we want to expand it later better make a proper interface for it,
+            // blocks that should be banned up to until the sun spirit dies and then be replaced on placement.
+            if (block == Blocks.COBBLE_NETHERRACK_IGNEOUS && !SunSpiritDeath.isDead()) replacementId.set(REPLACED_BLOCK);
+            else replacementId.set(MixinHelper.BLOCK_TO_BECOME.getOrDefault(block.id(), REPLACED_BLOCK));
+        }
+
+        else replacementId.set(BANNED_BLOCK);
         return original;
     }
 
     @WrapOperation(method = "onUseItemOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/world/World;canBlockBePlacedAt(IIIIZLnet/minecraft/core/util/helper/Side;)Z"))
     private boolean banBlocksFromDimensionsThree(World instance, int blockId, int x, int y, int z, boolean flag, Side side, Operation<Boolean> original, @Share("replacementId") LocalIntRef replacementId) {
         int id = replacementId.get();
-        if (id == -2) {
+        if (id == REPLACED_BLOCK) {
             ParticleMaker.spawnBlockBreakParticles(instance, x, y, z, blockId);
             return false;
         }
-        return original.call(instance, id == -1 ? blockId : id, x, y, z, flag, side);
+        return original.call(instance, id == BANNED_BLOCK ? blockId : id, x, y, z, flag, side);
     }
 
     @WrapOperation(method = "onUseItemOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/world/World;setBlockAndMetadataWithNotify(IIIII)Z"))
     private boolean banBlocksFromDimensionsFour(World instance, int x, int y, int z, int id, int meta, Operation<Boolean> original, @Share("replacementId") LocalIntRef replacementId) {
         int theReplacementId = replacementId.get();
-        boolean condition = original.call(instance, x, y, z, theReplacementId == -1 ? id : theReplacementId, meta);
-        if (theReplacementId != -1 && condition) {
+        boolean condition = original.call(instance, x, y, z, theReplacementId == BANNED_BLOCK ? id : theReplacementId, meta);
+        if (theReplacementId != BANNED_BLOCK && condition) {
             ParticleMaker.spawnReplacementEffects(instance, x, y, z);
             if (!EnvironmentHelper.isClientWorld()) {
                 instance.playSoundEffect(null, SoundCategory.WORLD_SOUNDS, x + 0.5, y + 0.5, z + 0.5, "fire.ignite", 1.0F, instance.rand.nextFloat() * 0.4F + 0.8F);
