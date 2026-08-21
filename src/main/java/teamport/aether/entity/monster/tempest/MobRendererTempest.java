1ab5b23424f73c2e8dfd4ed9f73d49be8111eb87
LukeisStuff <lukeisawesome03@gmail.com>
2026-08-20 20:45:43 -0400

    tempest variations and glowing eyes

--- src/main/java/teamport/aether/entity/monster/tempest/MobRendererTempest.java
+++ src/main/java/teamport/aether/entity/monster/tempest/MobRendererTempest.java
@@ -2,14 +2,15 @@ package teamport.aether.entity.monster.tempest;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.core.util.helper.MathHelper;
+import net.minecraft.client.render.entity.MobRenderer;
+import net.minecraft.client.render.renderer.BlendFactor;
 import net.minecraft.client.render.renderer.GLRenderer;
 import net.minecraft.client.render.renderer.State;
+import net.minecraft.core.util.helper.MathHelper;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 import org.useless.dragonfly.models.entity.BoneTransform;
 import org.useless.dragonfly.models.entity.StaticEntityModel;
-import net.minecraft.client.render.entity.MobRenderer;
 
 @Environment(EnvType.CLIENT)
 public class MobRendererTempest extends MobRenderer<MobTempest> {
@@ -20,27 +21,23 @@ public class MobRendererTempest extends MobRenderer<MobTempest> {
     @Override
     protected @Nullable StaticEntityModel getAndSetupModelForLayer(@NonNull MobTempest tempest, float brightness, float partialTick, int layer) {
         StaticEntityModel model;
-        if (layer == 1) {
+        if (layer == 2) {
+            this.bindTexture("/assets/aether/textures/entity/tempest/eyes/" + tempest.getTextureReference() + ".png");
+            GLRenderer.setLightmapCoord2i(15, 15);
+            GLRenderer.enableState(State.BLEND);
+            GLRenderer.setBlendFunc(BlendFactor.SRC_ALPHA, BlendFactor.ONE_MINUS_SRC_ALPHA);
+        } else if (layer == 1) {
             model = this.getModel("wind");
             model.resetBones();
-        } else if (layer == 0) {
-            model = this.getModel("main");
-            model.resetBones();
-        } else {
-            model = null;
-        }
 
-        if (layer == 1) {
             this.bindTexture("/assets/aether/textures/entity/tempest/wind.png");
 
             float time = tempest.tickCount + partialTick;
-            float scroll = time * 0.1F;
             float spinSpeed = 0.375F;
             float wobbleSpeed = 0.9F;
             float wobbleStrength = 0.12F;
             float wobble = MathHelper.sin(time * wobbleSpeed) * wobbleStrength;
 
-            GLRenderer.textureM4f().identity().translate(-scroll, 0.0F, 0.0F);
             GLRenderer.enableState(State.BLEND);
 
             BoneTransform wind = model.getTransform("wind");
@@ -63,20 +60,23 @@ public class MobRendererTempest extends MobRenderer<MobTempest> {
             wind5.rotY = wind3.rotY;
             wind5.rotX = wobble * 0.35F;
 
-        } else {
-            float bodyYaw = this.getBodyYaw(tempest, partialTick);
-            float headYaw = this.getHeadYaw(tempest, partialTick) - bodyYaw;
-            float headPitch = this.getHeadPitch(tempest, partialTick);
-            BoneTransform head = model.getTransform("head");
-            head.rotY = headYaw;
-            head.rotX = headPitch;
+            return model;
         }
+        model = this.getModel("main");
+        model.resetBones();
+
+        float bodyYaw = this.getBodyYaw(tempest, partialTick);
+        float headYaw = this.getHeadYaw(tempest, partialTick) - bodyYaw;
+        float headPitch = this.getHeadPitch(tempest, partialTick);
+        BoneTransform head = model.getTransform("head");
+        head.rotY = headYaw;
+        head.rotX = headPitch;
 
         return model;
     }
 
     @Override
     protected int maxRenderLayer(@NonNull MobTempest entity) {
-        return 1;
+        return 2;
     }
 }
